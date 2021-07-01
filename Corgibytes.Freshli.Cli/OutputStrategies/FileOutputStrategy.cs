ff6e2cea462c9a88d2e45cb7f3c3016fe747c539
Maira <mferrari@gmail.com>
2021-06-30 22:24:22 -0300

    Add logging interceptor

--- Corgibytes.Freshli.Cli/OutputStrategies/FileOutputStrategy.cs
+++ Corgibytes.Freshli.Cli/OutputStrategies/FileOutputStrategy.cs
@@ -1,4 +1,6 @@
-﻿using Corgibytes.Freshli.Cli.Formatters;
+﻿using Autofac.Extras.DynamicProxy;
+using Corgibytes.Freshli.Cli.Formatters;
+using Corgibytes.Freshli.Cli.IoC.Interceptors;
 using Corgibytes.Freshli.Cli.Options;
 using Freshli;
 using System;
@@ -7,11 +9,12 @@ using System.IO;
 
 namespace Corgibytes.Freshli.Cli.OutputStrategies
 {
+    [Intercept(typeof(LoggerInterceptor))]
     public class FileOutputStrategy : IOutputStrategy
     {
         public OutputStrategyType Type => OutputStrategyType.file;
 
-        public void Send( IList<MetricsResult> results, IOutputFormatter formatter, ScanOptions options )
+        public virtual void Send( IList<MetricsResult> results, IOutputFormatter formatter, ScanOptions options )
         {            
             var path = Path.Combine(options.Path, $"freshli-scan-{DateTime.Now:yyyyMMddTHHmmss}.{options.Format}");
             var file = File.CreateText(path);
--- Corgibytes.Freshli.Cli/Runners/ScanCommandRunner.cs
+++ Corgibytes.Freshli.Cli/Runners/ScanCommandRunner.cs
@@ -1,13 +1,15 @@
-﻿using Corgibytes.Freshli.Cli.Formatters;
+﻿using Autofac.Extras.DynamicProxy;
+using Corgibytes.Freshli.Cli.Formatters;
+using Corgibytes.Freshli.Cli.IoC.Interceptors;
 using Corgibytes.Freshli.Cli.Options;
 using Corgibytes.Freshli.Cli.OutputStrategies;
 using Freshli;
 using NLog;
-using System;
 using System.Collections.Generic;
 
 namespace Corgibytes.Freshli.Cli.Runners
 {
+    [Intercept(typeof(LoggerInterceptor))]
     public class ScanCommandRunner : ICommandRunner<ScanOptions>
     {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
@@ -24,32 +26,16 @@ namespace Corgibytes.Freshli.Cli.Runners
             this.Runner = runner;
         }
 
-        public int Run( ScanOptions options )
+        public virtual int Run( ScanOptions options )
         {
-            try
+            var results = this.Runner.Run(options.Path);
+
+            foreach(IOutputStrategy output in this.OutputStrategies)
             {
-
-                var path = options.Path;
-                logger.Debug($"Collecting data for path = { options.Path }, format = { options.Format }, output = { string.Join(",", options.Output) }");
-                var results = this.Runner.Run(options.Path);
-
-                foreach(IOutputStrategy output in this.OutputStrategies)
-                {
-                    output.Send(results, this.OutputFormatter, options);
-                }
-
-                return 0;
+                output.Send(results, this.OutputFormatter, options);
             }
-            catch(Exception e)
-            {
-                Console.WriteLine($"Exception executing Freshli for args path = { options.Path }, format = { options.Format }, output = { options.Output } : { e.Message} : {e.StackTrace}");
-                logger.Error(
-                  e,
-                  $"Exception executing Freshli for args path={options.Path}, format={options.Format}, output={options.Output} : {e.Message}"
-                );
-                logger.Trace(e, e.StackTrace);
-                throw;
-            }
+
+            return 0;
         }
     }
 }
