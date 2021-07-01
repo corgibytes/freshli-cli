6d54c79b553918b790ccb1cf4b4defbeaca33d0b
Maira <mferrari@gmail.com>
2021-06-30 22:24:22 -0300

    Add logging interceptor

--- Corgibytes.Freshli.Cli/IoC/FreshliContainerBuilder.cs
+++ Corgibytes.Freshli.Cli/IoC/FreshliContainerBuilder.cs
@@ -1,12 +1,5 @@
 ﻿using Autofac;
-using Corgibytes.Freshli.Cli.Formatters;
 using Corgibytes.Freshli.Cli.IoC.Modules;
-using Corgibytes.Freshli.Cli.Options;
-using Corgibytes.Freshli.Cli.OutputStrategies;
-using Corgibytes.Freshli.Cli.Runners;
-using Freshli;
-using System.Collections.Generic;
-
 
 namespace Corgibytes.Freshli.Cli.IoC
 {
@@ -16,10 +9,11 @@ namespace Corgibytes.Freshli.Cli.IoC
         {
             var builder = new ContainerBuilder();
 
-            builder.RegisterModule(new MiddlewareModule());
             builder.RegisterModule(new BaseCommandModule());
             builder.RegisterModule(new AuthCommandModule());
             builder.RegisterModule(new ScanCommandModule());
+
+            builder.RegisterModule(new InterceptorsModule());
 
             return builder.Build();
         }
