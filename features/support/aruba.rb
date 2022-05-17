5f1543443ad6fd50d28a788dc0b92a67129bef90
Jason C. McDonald <codemouse92@outlook.com>
2022-05-17 11:53:28 -0500

    test: testing 'cache prepare' produces valid database
    Ensures that 'cache prepare' creates the expected default folder,
    and generates an SQLite database file that passes SQLite's
    "PRAGMA integrity_check".
    
    NOTE: It is not feasible to test for pending migrations with cucumber.

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -1,1 +1,10 @@
 require 'aruba/cucumber'
+
+Aruba.configure do |config|
+  # Use aruba working directory
+  config.home_directory = File.join(config.root_directory, config.working_directory)
+end
+
+Aruba.configure do |config|
+  puts %(The default value is "#{config.home_directory}")
+end
