673b2ccfcd7ce46ff2ed102b3c52785f1f26489a
Jason C. McDonald <codemouse92@outlook.com>
2022-05-17 09:50:28 -0500

    test: 'cache' and 'cache prepare' commands
    Confirms the following:
    * 'cache' is not directly runnable (no handler)
    * 'cache prepare' is runnable (has handler)

---
+++ Corgibytes.Freshli.Cli.Test/Commands/CacheCommandTest.cs
@@ -0,0 +1,33 @@
+using System.CommandLine.Builder;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+using Corgibytes.Freshli.Cli.Commands;
+using Corgibytes.Freshli.Cli.Test.Common;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Corgibytes.Freshli.Cli.Test.Commands
+{
+    public class CacheCommandTest : FreshliTest
+    {
+        private readonly TestConsole _console = new();
+
+        public CacheCommandTest(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void Verify_no_cache_handler_configuration()
+        {
+            CacheCommand cacheCommand = new();
+            cacheCommand.Handler.Should().BeNull();
+        }
+
+        [Fact]
+        public void Verify_prepare_handler_configuration()
+        {
+            CachePrepareCommand cachePrepareCommand = new();
+            cachePrepareCommand.Handler.Should().NotBeNull();
+        }
+    }
+}
