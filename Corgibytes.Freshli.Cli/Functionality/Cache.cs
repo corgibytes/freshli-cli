f2117be28e177920ff28e9ee7985c784f7a09865
Jason C. McDonald <codemouse92@outlook.com>
2022-05-18 14:51:34 -0500

    fix: eliminate use of environment variable for --cache-dir
    Environment variables on Linux do not persist between process
    executions, so this was not actually enabling the 'dotnet ef'
    tool to work with non-standard locations. Removing all use
    of the environment variable to simplify code.

--- Corgibytes.Freshli.Cli/Functionality/Cache.cs
+++ Corgibytes.Freshli.Cli/Functionality/Cache.cs
@@ -16,28 +16,17 @@ namespace Corgibytes.Freshli.Cli.Functionality
 
     public class CacheContext : DbContext
     {
-        public static readonly string CacheDirEnvVariable = "FRESHLI_CACHE_DIR";
-        public static readonly DirectoryInfo CacheDirDefault = new DirectoryInfo(Environment.GetEnvironmentVariable("HOME") + "/.freshli");
+        public static readonly DirectoryInfo DefaultCacheDir = new DirectoryInfo(Environment.GetEnvironmentVariable("HOME") + "/.freshli");
+        public static DirectoryInfo CacheDir = DefaultCacheDir;
+
         private static readonly string cacheDbName = "freshli.db";
         public string DbPath { get; }
-
-        public static string CacheDir
-        {
-            get
-            {
-                /* If the user passes --cache-dir, the environment variable is set. This design enables 'dotnet ef'
-                 * to keep working, even after a call to `cache prepare --cache-dir [some directory]`.
-                 * Otherwise, the default is used. */
-                string cacheDir = Environment.GetEnvironmentVariable(CacheDirEnvVariable);
-                return cacheDir.IsNullOrEmpty() ? CacheDirDefault.ToString() : cacheDir;
-            }
-        }
 
         public DbSet<CachedProperty> CachedProperties { get; set; }
 
         public CacheContext()
         {
-            DbPath = Path.Join(CacheDir, cacheDbName);
+            DbPath = Path.Join(CacheDir.ToString(), cacheDbName);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
@@ -58,18 +47,14 @@ namespace Corgibytes.Freshli.Cli.Functionality
         }
         public static bool Prepare(DirectoryInfo cacheDir)
         {
+            CacheContext.CacheDir = cacheDir;
+            Console.Out.WriteLine($"Preparing cache at {cacheDir}");
+
             // Create the directory if it doesn't already exist
             if (cacheDir.Exists == false)
             {
                 cacheDir.Create();
             }
-
-            /* Store the expected cache directory in an environment variable so it can be used by the database model,
-             * even between calls in this terminal session.
-             */
-            Environment.SetEnvironmentVariable(CacheContext.CacheDirEnvVariable, cacheDir.ToString());
-
-            Console.Out.WriteLine($"Preparing cache at {CacheContext.CacheDir}");
 
             using var db = new CacheContext();
             try
