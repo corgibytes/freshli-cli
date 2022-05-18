a7b6ff35deb15b0974c5dfde7490fc1966636443
M. Scott Ford <scott@corgibytes.com>
2022-05-18 14:52:09 -0400

    Removes unused using statements

--- Corgibytes.Freshli.Cli.Test/Extensions.cs
+++ Corgibytes.Freshli.Cli.Test/Extensions.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Parsing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Corgibytes.Freshli.Cli.Test
 {
