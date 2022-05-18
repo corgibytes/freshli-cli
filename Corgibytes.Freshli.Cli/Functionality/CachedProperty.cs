d191dc762b32d13de9c81420e7da1953da99b137
Jason C. McDonald <codemouse92@outlook.com>
2022-05-18 15:02:27 -0500

    style: split classes out into dedicated files
    per PR request

---
+++ Corgibytes.Freshli.Cli/Functionality/CachedProperty.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Corgibytes.Freshli.Cli.Functionality
+{
+    [Index(nameof(Key), IsUnique = true)]
+    public class CachedProperty
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
