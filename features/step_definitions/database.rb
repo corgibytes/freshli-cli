57c64e3434d12cdf87a610d96b6e4276ae114ef0
Jason C. McDonald <codemouse92@outlook.com>
2022-05-19 11:50:57 -0500

    test: 'cache destroy'
    Add unit and acceptance tests for 'cache destroy' command.

--- features/step_definitions/database.rb
+++ features/step_definitions/database.rb
@@ -1,5 +1,29 @@
+require 'fileutils'
 require 'aruba/cucumber'
 require 'sqlite3'
+
+Given('that I ran {string}') do |command|
+    system command
+end
+
+Given('a blank file named {string} exists') do |filename|
+    Aruba.configure do |config|
+        if filename.include? "~"
+            filename["~"] = config.home_directory
+        end
+        FileUtils.touch filename
+    end
+end
+
+Given('a directory named {string} exists') do |dirname|
+    Aruba.configure do |config|
+        if dirname.include? "~"
+            dirname["~"] = config.home_directory
+        end
+        Dir.mkdir dirname
+    end
+end
+
 
 Then('we can open a SQLite connection to {string}') do |database|
     Aruba.configure do |config|
