53b1253a062b88189e58c3061820c0ef41467cf4
Jason C. McDonald <codemouse92@outlook.com>
2022-05-23 12:04:47 -0500

    feat: Translation strings for all interface strings
    Adds translation strings for all help and message strings
    defined in our code for the user interface. However, this
    does not include command, option, or argument names/aliases.

--- Corgibytes.Freshli.Cli/Commands/MainCommand.cs
+++ Corgibytes.Freshli.Cli/Commands/MainCommand.cs
@@ -1,16 +1,17 @@
 ﻿using System.CommandLine;
 using System.IO;
 using Corgibytes.Freshli.Cli.Functionality;
+using Corgibytes.Freshli.Cli.Resources;
 
 namespace Corgibytes.Freshli.Cli.Commands;
 
 public class MainCommand : RootCommand
 {
-    public MainCommand() : base("Root Command")
+    public MainCommand() : base($"{CliOutput.Help_MainCommand_Description}")
     {
         Option<DirectoryInfo> cacheDirOption = new(
             new[] { "--cache-dir" },
-            description: "The location for storing temporary files",
+            description: $"{CliOutput.Help_Option_CacheDir_Description}",
             getDefaultValue: () => CacheContext.DefaultCacheDir)
         {
             Arity = ArgumentArity.ExactlyOne
