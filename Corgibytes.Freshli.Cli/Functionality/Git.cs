150bd89872061807520b03df720c1658609ec544
Jason C. McDonald <codemouse92@outlook.com>
2022-05-24 16:37:48 -0500

    feat: implement 'git clone' command
    Implements #51. This command clones a repository in the cache
    directory and (optionally) checks out a branch. The repository URL
    and branch name are also stored in the cache database. If the
    repository and branch are already checked out, the latest version
    is pulled instead. Also supports overriding the Git binary used
    with the '--git-path' optional argument.

---
+++ Corgibytes.Freshli.Cli/Functionality/Git.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Corgibytes.Freshli.Cli.Functionality;
+
+[Serializable]
+public class GitException : Exception
+{
+    public GitException(string message, Exception innerException) : base(message, innerException) {}
+    public GitException(string message) : base(message) {}
+    protected GitException(SerializationInfo info, StreamingContext context) : base(info, context) {}
+}
+
+public class GitRepository
+{
+    public string Hash { get; }
+    private string Url { get; }
+    private string Branch { get; }
+    private DirectoryInfo Directory { get; }
+
+    private bool Cloned
+    {
+        get => Directory.GetFiles().Any() || Directory.GetDirectories().Any();
+    }
+
+    private bool BranchDefined
+    {
+        get => !string.IsNullOrEmpty(Branch);
+    }
+
+    public GitRepository(string hash, DirectoryInfo cacheDir)
+    {
+        // Ensure the cache directory is ready for use.
+        Cache.Prepare(cacheDir);
+
+        Hash = hash;
+
+        // Get existing entry via provided hash
+        using var db = new CacheContext();
+        var entry = db.CachedGitRepos.Find(Hash);
+        if (entry == null)
+        {
+            throw new CacheException("No repository with this hash exists in cache.");
+        }
+
+        Url = entry.Url;
+        Branch = entry.Branch;
+
+        // Ensure the directory exists in the cache for cloning the repository.
+        Directory = Cache.GetDirectoryInCache(cacheDir, new[] {"repositories", Hash});
+    }
+    public GitRepository(string url, string branch, DirectoryInfo cacheDir)
+    {
+        // Ensure the cache directory is ready for use.
+        Cache.Prepare(cacheDir);
+
+        Url = url;
+        Branch = branch;
+
+        // Generate a unique hash for the repository based on its URL and branch.
+        using SHA256 sha256 = SHA256.Create();
+        byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(Url + Branch));
+        var stringBuilder = new StringBuilder();
+        foreach (byte hashByte in hashBytes)
+        {
+            stringBuilder.Append(hashByte.ToString("x2"));
+        }
+
+        Hash = stringBuilder.ToString();
+
+        // Ensure the directory exists in the cache for cloning the repository.
+        Directory = Cache.GetDirectoryInCache(cacheDir, new[] {"repositories", Hash});
+
+        // Store ID, URL, branch, and folder path in the cache DB, if it doesn't already exist
+        using var db = new CacheContext();
+        if (db.CachedGitRepos.Find(Hash) != null) { return; }
+
+        var entry = new CachedGitRepo() {Id = Hash, Url = Url, Branch = Branch, LocalPath = Directory.FullName};
+        db.CachedGitRepos.Add(entry);
+        db.SaveChanges();
+    }
+
+    private void Delete()
+    {
+        using var db = new CacheContext();
+        var entry = db.CachedGitRepos.Find(Hash);
+        db.CachedGitRepos.Remove(entry!);
+
+        Directory.Delete(true);
+    }
+
+    private void Clone(string gitPath)
+    {
+        var cloneProcess = new Process();
+        cloneProcess.StartInfo = new ProcessStartInfo()
+        {
+            FileName = gitPath,
+            WorkingDirectory = Directory.FullName,
+            Arguments = $"clone {Url} .",  // clone directly in the working directory
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        };
+        cloneProcess.Start();
+        cloneProcess.WaitForExit();
+
+        if (cloneProcess.ExitCode != 0)
+        {
+            Delete();
+            throw new GitException($"Git encountered an error:\n{cloneProcess.StandardError.ReadToEnd()}");
+        }
+    }
+
+    private void Checkout(string gitPath)
+    {
+        var checkoutProcess = new Process();
+        checkoutProcess.StartInfo = new ProcessStartInfo()
+        {
+            FileName = gitPath,
+            WorkingDirectory = Directory.FullName,
+            Arguments = $"checkout {Branch}",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        };
+        checkoutProcess.Start();
+        checkoutProcess.WaitForExit();
+
+        if (checkoutProcess.ExitCode != 0)
+        {
+            Delete();
+            throw new GitException($"Git encountered an error:\n{checkoutProcess.StandardError.ReadToEnd()}");
+        }
+    }
+
+    private void Pull(string gitPath)
+    {
+        var pullProcess = new Process();
+        pullProcess.StartInfo = new ProcessStartInfo()
+        {
+            FileName = gitPath,
+            WorkingDirectory = Directory.FullName,
+            Arguments = $"pull origin {Branch}",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
+        };
+        pullProcess.Start();
+        pullProcess.WaitForExit();
+
+        if (pullProcess.ExitCode != 0)
+        {
+            throw new GitException($"Git encountered an error:\n{pullProcess.StandardError.ReadToEnd()}");
+        }
+    }
+
+    public void CloneOrPull(string gitPath)
+    {
+        // If already cloned, pull instead.
+        if (Cloned)
+        {
+            Pull(gitPath);
+            return;
+        }
+
+        // If not yet cloned, clone from URL.
+        Clone(gitPath);
+
+        // If a branch is defined, checkout branch
+        if (BranchDefined)
+        {
+            Checkout(gitPath);
+        }
+    }
+}
