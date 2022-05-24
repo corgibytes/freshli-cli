e7e09d0408c4295eca1565a36664a13ec4f386d0
Jason C. McDonald <codemouse92@outlook.com>
2022-05-24 16:34:21 -0500

    feat: add GitRepository table to cache db
    Adds a table to the cache database to track the repositories
    that are checked out by Freshli.

--- Corgibytes.Freshli.Cli/Migrations/20220524202324_GitRepository.Designer.cs
+++ Corgibytes.Freshli.Cli/Migrations/20220524202324_GitRepository.Designer.cs
@@ -2,6 +2,7 @@
 using Corgibytes.Freshli.Cli.Functionality;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Corgibytes.Freshli.Cli.Migrations
 {
     [DbContext(typeof(CacheContext))]
-    partial class CacheContextModelSnapshot : ModelSnapshot
+    [Migration("20220524202324_GitRepository")]
+    partial class GitRepository
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0-preview.4.22229.2");
