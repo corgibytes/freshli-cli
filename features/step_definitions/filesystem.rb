69893d502903228c5d0d4e89dae0fe8f1c6763bb
Jason C. McDonald <codemouse92@outlook.com>
2022-06-06 10:57:43 -0500

    refactor/test: resolve Aruba paths to simulated home directory
    Move the function for resolving paths in Aruba in our own step
    definitions into a dedicated support function.
    Both relative paths and paths starting with ~ should be resolved
    to the simulated and temporary home directory created by Aruba.
    This seems to be how the built-in step definitions from Aruba/Cucumber
    in Ruby seem to be interpreting paths anyway.

---
+++ features/step_definitions/filesystem.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+require 'aruba/cucumber'
+
+Given('a blank file named {string} exists') do |filename|
+  filename = resolve_path filename
+  FileUtils.touch filename
+end
+
+Given('a directory named {string} exists') do |dirname|
+  dirname = resolve_path dirname
+  Dir.mkdir dirname
+end
