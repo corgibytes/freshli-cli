736d38c4f0fa58723a904d186b56e576ddfa14a0
M. Scott Ford <scott@corgibytes.com>
2022-06-09 16:41:38 -0400

    Adds support for running other linters
    Add ability to run `rubocop`, `dotnet format`, and `codeclimate`. Also adds a helper function in the `bin/support` directory that knows how to stream the contents of stdout and stderr from child processes. The built-in ruby `system` function only redirects stdout. Also enables colorized output for the commands that provide a mechanism for doing so.

--- bin/lint.rb
+++ bin/lint.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
+require 'English'
 require 'optparse'
 
+require_relative './support/execute'
+
+enable_dotnet_command_colors
+
 perform_eclint = true
+perform_rubocop = true
+perform_dotnet_format = true
+perform_codeclimate = true
 
 parser = OptionParser.new do |options|
   options.banner = <<~BANNER
     Description:
         Linter Runner
-    
+
     Usage:
         lint.rb [options]
 
@@ -19,6 +28,18 @@ parser = OptionParser.new do |options|
     perform_eclint = false
   end
 
+  options.on('--skip-rubocop', 'Does not run the Rubocop linter') do
+    perform_rubocop = false
+  end
+
+  options.on('--skip-dotnet-format', 'Does not run the dotnet format linter') do
+    perform_dotnet_format = false
+  end
+
+  options.on('--skip-codeclimate', 'Does not run the codeclimate linter') do
+    perform_codeclimate = false
+  end
+
   options.on('-h', '--help', 'Show help and usage information') do
     puts options
     exit
@@ -27,22 +48,54 @@ end
 
 begin
   parser.parse!
-rescue OptionParser::InvalidOption => error
-  puts error
+rescue OptionParser::InvalidOption => e
+  puts e
   puts parser
-  exit -1
+  exit(-1)
 end
 
 linter_failed = false
 
 if perform_eclint
-  system("eclint")
+  status = execute('eclint')
 
-  linter_failed = !$?.success?
+  linter_failed = !status.success?
+end
+
+if perform_rubocop
+  status = execute('bundle check > /dev/null')
+  status = execute('bundle install') unless status.success?
+
+  status = execute('bundle exec rubocop --color') if status.success?
+
+  linter_failed = !status.success?
+end
+
+if perform_dotnet_format
+  status = execute('dotnet format --verify-no-changes --severity info')
+
+  linter_failed = !status.success?
+end
+
+if perform_codeclimate
+  if ENV['DEVCONTAINER'] == 'true'
+    status = execute(<<~COMMAND)
+      sudo docker run \
+        --rm \
+        --env CODECLIMATE_CODE="#{ENV.fetch('CODE_FOLDER')}" \
+        --volume "#{ENV.fetch('CODE_FOLDER')}":/code \
+        --volume /var/run/docker.sock:/var/run/docker.sock \
+        --volume /tmp/cc:/tmp/cc \
+        codeclimate/codeclimate analyze
+    COMMAND
+  else
+    status = execute('codeclimate analyze')
+  end
+  linter_failed = !status.success?
 end
 
 if linter_failed
-  puts "At least one linter encountered errors"
-  exit -1
+  puts 'At least one linter encountered errors'
+  exit(-1)
 end
-exit 0
+exit(0)
