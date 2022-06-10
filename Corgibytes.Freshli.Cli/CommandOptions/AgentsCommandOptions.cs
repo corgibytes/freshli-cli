b1de2b16f6b4e492bd7828c69e187f40bcffb74c
Dona Pop <dona@corgibytes.com>
2022-06-10 11:33:47 -0400

    added agents detect command

---
+++ Corgibytes.Freshli.Cli/CommandOptions/AgentsCommandOptions.cs
@@ -0,0 +1,10 @@
+namespace Corgibytes.Freshli.Cli.CommandOptions
+{
+    public class AgentsCommandOptions : CommandOptions
+    {
+    }
+
+    public class AgentsDetectCommandOptions : CommandOptions
+    {
+    }
+}
