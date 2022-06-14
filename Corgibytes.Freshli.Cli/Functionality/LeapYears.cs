3c15f31686bd8b20e482417b7d9b5876a48d25f2
Edwin Kortman <edwin.kortman@pm.me>
2022-06-14 15:19:23 +0200

    Calculate the libyear for two given dates
    A few notes:
    - This willingly ignores that the newest version of a package may have
      been released before the current version. This is ignored by using
    just the absolute number. I did this because I see libyear more as the
    'time you had to react to an update counted from'. So when the newer
    package was released the clock started ticking, and you didn't do
    something for x time.
    
    - It knows what leap years are! There's a surprisingly big difference if
      you ignore leap years.

---
+++ Corgibytes.Freshli.Cli/Functionality/LeapYears.cs
@@ -0,0 +1,21 @@
+
+using System;
+
+namespace Corgibytes.Freshli.Cli.Functionality
+{
+    public static class LeapYears
+    {
+        public static int NumberOfLeapYearsBetween(int start, int end)
+        {
+            return Math.Abs(Before(end) - Before(start + 1));
+        }
+
+        // Solution shamelessly borrowed from StackOverflow: https://stackoverflow.com/questions/4587513/how-to-calculate-number-of-leap-years-between-two-years-in-c-sharp
+        // Explanation: You can count it using analytic approach. A year is a leap year if it can be divided by 4, but can't be divided by 100, except of case when it can be divided by 400. Assuming that you can count such number by following code:
+        private static int Before(int year)
+        {
+            year--;
+            return (year / 4) - (year / 100) + (year / 400);
+        }
+    }
+}
