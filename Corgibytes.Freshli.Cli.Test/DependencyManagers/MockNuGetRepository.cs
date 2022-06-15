578a4144c16c29b8c1115f651ded7424c3597c5e
Edwin Kortman <edwin.kortman@pm.me>
2022-06-15 14:22:02 +0200

    Calculate libyear given two Package URLs
    This service uses the name and version to fetch the release date from a
    repository. With these releases date we can calculate libyear

---
+++ Corgibytes.Freshli.Cli.Test/DependencyManagers/MockNuGetRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using Corgibytes.Freshli.Cli.DependencyManagers;
+
+namespace Corgibytes.Freshli.Cli.Test.DependencyManagers;
+
+public class MockNuGetRepository: IRepository
+{
+    public DateTime GetReleaseDate(string name, string version)
+    {
+        if (name == "Newtonsoft.Json" && version == "3.22.2021")
+        {
+            return new DateTime(2021, 3, 22);
+        }
+
+        if (name == "Newtonsoft.Json" && version == "8.3.2014")
+        {
+            return new DateTime(2014, 8, 3);
+        }
+
+        throw new ArgumentException("Mock date could not be returned. Forgot to add it?");
+    }
+
+    public SupportedDependencyManagers Supports() => SupportedDependencyManagers.NuGet();
+}
+
