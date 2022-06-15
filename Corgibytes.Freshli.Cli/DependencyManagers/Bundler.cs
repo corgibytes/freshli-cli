22c12b333c364c07791c6cc2f4864ac07d932dfb
Edwin Kortman <edwin.kortman@pm.me>
2022-06-15 19:00:58 +0200

    Code restyling according to code format specs

--- Corgibytes.Freshli.Cli/DependencyManagers/Bundler.cs
+++ Corgibytes.Freshli.Cli/DependencyManagers/Bundler.cs
@@ -1,12 +1,11 @@
 using System;
 
-namespace Corgibytes.Freshli.Cli.DependencyManagers
-{
-    public class Bundler: IDependencyManagerRepository
-    {
-        public DateTime GetReleaseDate(string name, string version) => throw new NotImplementedException();
+namespace Corgibytes.Freshli.Cli.DependencyManagers;
 
-        public SupportedDependencyManagers Supports() => SupportedDependencyManagers.Bundler();
-    }
+public class Bundler : IDependencyManagerRepository
+{
+    public DateTime GetReleaseDate(string name, string version) => throw new NotImplementedException();
+
+    public SupportedDependencyManagers Supports() => SupportedDependencyManagers.Bundler();
 }
 
