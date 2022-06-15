8271ca0a63d4a8c89171d51c3b5ef7640b9b902f
Jason C. McDonald <codemouse92@outlook.com>
2022-06-15 16:07:04 -0500

    refactor: address linter issues

--- Corgibytes.Freshli.Cli/CommandRunners/ICommandRunner.cs
+++ Corgibytes.Freshli.Cli/CommandRunners/ICommandRunner.cs
@@ -2,7 +2,7 @@
 
 namespace Corgibytes.Freshli.Cli.CommandRunners;
 
-public interface ICommandRunner<T> where T : CommandOptions.CommandOptions
+public interface ICommandRunner<in T> where T : CommandOptions.CommandOptions
 {
     public int Run(T options, InvocationContext context);
 }
