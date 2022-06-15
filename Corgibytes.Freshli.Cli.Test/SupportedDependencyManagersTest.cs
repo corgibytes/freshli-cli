e226c52a94e1d00deb2b5d7bc4376a085d1ff486
Edwin Kortman <edwin.kortman@pm.me>
2022-06-15 13:32:57 +0200

    Instead of having constants, use an object.
    This way we don't have to pass around strings or verify strings. And
    it's nicer to read. :D

---
+++ Corgibytes.Freshli.Cli.Test/SupportedDependencyManagersTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Corgibytes.Freshli.Cli.DependencyManagers;
+using Corgibytes.Freshli.Cli.Test.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Corgibytes.Freshli.Cli.Test;
+
+public class SupportedDependencyManagersTest : FreshliTest
+{
+    public SupportedDependencyManagersTest(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void It_can_not_instantiate_with_invalid_dependency_manager()
+    {
+        ArgumentException caughtException =
+            Assert.Throws<ArgumentException>(() =>
+                SupportedDependencyManagers.FromString("prettysurethiscanneverwork"));
+
+        Assert.Equal("Invalid dependency manager given 'prettysurethiscanneverwork'", caughtException.Message);
+    }
+}
+
