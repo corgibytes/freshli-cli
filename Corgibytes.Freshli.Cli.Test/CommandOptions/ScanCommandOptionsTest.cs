ef752ab0138fe6e2683400a36e1a8bc639d91c2f
Jason C. McDonald <codemouse92@outlook.com>
2022-06-15 16:10:27 -0500

    refactor: address linter issues

--- Corgibytes.Freshli.Cli.Test/CommandOptions/ScanCommandOptionsTest.cs
+++ Corgibytes.Freshli.Cli.Test/CommandOptions/ScanCommandOptionsTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.CommandLine;
-using System.CommandLine.Builder;
+﻿using System.Collections.Generic;
 using System.CommandLine.Parsing;
 using System.IO;
 using System.Linq;
