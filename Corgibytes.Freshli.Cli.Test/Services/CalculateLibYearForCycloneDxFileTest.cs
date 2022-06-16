0083ccef46ee55f72a80bb288e2954f70cd8281a
Edwin Kortman <edwin.kortman@pm.me>
2022-06-16 16:06:19 +0200

    Calculate libyear from a given cycloneDx file in JSON format
    Given a CycloneDX file in JSON. Use the dependency manager repositories
    to figure out what the latest version is. With this latest version we
    can request the release date for both versions and calculate the
    libyear.
    
    We need to do this for all packages, and yes that could mean a lot of
    calls to the repositories. The idea is that their implementation will
    use a cache to fetch info for a package once (like tags and releases
    dates).

---
+++ Corgibytes.Freshli.Cli.Test/Services/CalculateLibYearForCycloneDxFileTest.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using Corgibytes.Freshli.Cli.DependencyManagers;
+using Corgibytes.Freshli.Cli.Functionality;
+using Corgibytes.Freshli.Cli.Services;
+using Corgibytes.Freshli.Cli.Test.Common;
+using Corgibytes.Freshli.Cli.Test.DependencyManagers;
+using Corgibytes.Freshli.Cli.Test.Functionality;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Corgibytes.Freshli.Cli.Test.Services;
+
+public class CalculateLibYearForCycloneDxFileTest : FreshliTest
+{
+    private readonly MockReadFile _readFileService;
+    private readonly CalculateLibYearFromCycloneDxFile _calculateLibYearFromCycloneDxFile;
+
+    public CalculateLibYearForCycloneDxFileTest(ITestOutputHelper output) : base(output)
+    {
+        var repositories = new List<IDependencyManagerRepository>() { new MockNuGetDependencyManagerRepository() };
+
+        _readFileService = new();
+        _calculateLibYearFromCycloneDxFile = new(
+            new(_readFileService),
+            repositories,
+            new(repositories)
+        );
+    }
+
+    [Fact]
+    public void Verify_it_can_process_an_entire_file()
+    {
+        var fileContents =
+            @"{
+            'bomFormat': 'CycloneDX',
+            'specVersion': '1.4',
+            'serialNumber': 'urn:uuid:3e671687-395b-41f5-a30f-a58921a69b79',
+            'version': 1,
+            'components': [
+                {
+                    'type': 'library',
+                    'name': 'calculatron',
+                    'version': '14.6',
+                    'purl': 'pkg:nuget/org.corgibytes.calculatron/calculatron@14.6'
+                },
+                {
+                    'type': 'library',
+                    'name': 'flyswatter',
+                    'version': '1.1.0',
+                    'purl': 'pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.1.0'
+                },
+                {
+                    'type': 'library',
+                    'name': 'auto-cup-of-tea',
+                    'version': '112.0',
+                    'purl': 'pkg:nuget/org.corgibytes.tea/auto-cup-of-tea@112.0'
+                }
+            ]
+        }";
+
+        _readFileService.FeedJson(fileContents);
+
+        Assert.Equal(9.42415, _calculateLibYearFromCycloneDxFile.AsDecimalNumber("this/is/a/file/path", 5));
+    }
+}
+
