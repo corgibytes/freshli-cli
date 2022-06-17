357d5e6240a087f049a30c7bf91e15995b7d3f37
Edwin Kortman <edwin.kortman@pm.me>
2022-06-17 16:45:30 +0200

    Reduce lines in 'file' to only lines that are relevant

--- Corgibytes.Freshli.Cli.Test/Functionality/ReadCycloneDxFileTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/ReadCycloneDxFileTest.cs
@@ -23,36 +23,18 @@ public class ReadCycloneDxFileTest : FreshliTest
     [Fact]
     public void Verify_it_can_create_a_json_object()
     {
+        // File was shortened the only show relevant information for this test
         var fileContents =
         @"{
-            'bomFormat': 'CycloneDX',
-            'specVersion': '1.4',
-            'serialNumber': 'urn:uuid:3e671687-395b-41f5-a30f-a58921a69b79',
-            'version': 1,
             'components': [
                 {
-                    'type': 'library',
-                    'name': 'calculatron',
-                    'version': '14.6',
                     'purl': 'pkg:nuget/org.corgibytes.calculatron/calculatron@14.6'
                 },
                 {
-                    'type': 'library',
-                    'name': 'flyswatter',
-                    'version': '1.1.0',
                     'purl': 'pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.1.0'
                 },
                 {
-                    'type': 'library',
-                    'name': 'auto-cup-of-tea',
-                    'version': '112.0',
                     'purl': 'pkg:composer/org.corgibytes.tea/auto-cup-of-tea@112.0'
-                },
-                {
-                    'type': 'library',
-                    'name': 'all-green-no-failing-tests',
-                    'version': '42.1.0',
-                    'purl': 'pkg:composer/org.corgibytes.test/all-green-no-failing-tests@42.1.0'
                 }
             ]
         }";
