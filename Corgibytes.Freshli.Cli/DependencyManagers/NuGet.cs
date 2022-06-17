549d94d90f904722253aaece3d173513a84c2010
Edwin Kortman <edwin.kortman@pm.me>
2022-06-17 16:49:48 +0200

    Make the function shorter by using a switch expression

--- Corgibytes.Freshli.Cli/DependencyManagers/NuGet.cs
+++ Corgibytes.Freshli.Cli/DependencyManagers/NuGet.cs
@@ -4,35 +4,15 @@ namespace Corgibytes.Freshli.Cli.DependencyManagers;
 
 public class NuGet : IDependencyManagerRepository
 {
-    public DateTime GetReleaseDate(string name, string version)
+    public DateTime GetReleaseDate(string name, string version) => (name, version) switch
     {
-        if (name == "calculatron" && version == "21.3")
-        {
-            return new(2022, 6, 16);
-        }
-
-        if (name == "calculatron" && version == "14.6")
-        {
-            return new(2019, 12, 31);
-        }
-
-        if (name == "flyswatter" && version == "1.1.0")
-        {
-            return new(1990, 1, 29);
-        }
-
-        if (name == "auto-cup-of-tea" && version == "112.0")
-        {
-            return new(2006, 11, 11);
-        }
-
-        if (name == "auto-cup-of-tea" && version == "256.0")
-        {
-            return new(2011, 10, 26);
-        }
-
-        throw new ArgumentException("Mock date could not be returned. Forgot to add it?");
-    }
+        ("calculatron", "21.3") => new(2022, 6, 16),
+        ("calculatron", "14.6") => new(2019, 12, 31),
+        ("flyswatter", "1.1.0") => new(1990, 1, 29),
+        ("auto-cup-of-tea", "112.0") => new(2004, 11, 11),
+        ("auto-cup-of-tea", "256.0") => new(2011, 10, 26),
+        _ => throw new ArgumentException("Mock date could not be returned. Forgot to add it?")
+    };
 
     public string GetLatestVersion(string name) => name switch
     {
