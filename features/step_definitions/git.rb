1de6ff8c37446bf39db90f4ce8f53330898191a0
Jason C. McDonald <codemouse92@outlook.com>
2022-06-27 16:10:02 -0500

    test: git clone --branch acceptance test

--- features/step_definitions/git.rb
+++ features/step_definitions/git.rb
@@ -3,7 +3,13 @@ Then('a Git repository exists at {string} with a Git SHA {string} checked out') 
   expect(repo.nil?).to match(false)
   git_index = repo + '/.git/index'
   expect(File.file?(git_index)).to match(true)
-  Dir.chdir(repo)
-  actual_sha = `git show-ref HEAD`.split[0]
+  actual_sha = run_from(repo, "git show-ref HEAD").split[0]
   expect(actual_sha).to match(sha)
+end
+
+Then('a Git repository exists at {string} with a branch {string} checked out') do |path, branch|
+  repo = resolve_path path
+  expect(repo.nil?).to match(false)
+  current_branch = run_from repo, "git branch --show-current"
+  expect(current_branch).to match(branch)
 end
