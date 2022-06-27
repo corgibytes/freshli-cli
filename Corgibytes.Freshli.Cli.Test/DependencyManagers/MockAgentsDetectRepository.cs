7585e8dfbf3b385731d342a0bf4482219b57a6d4
Edwin Kortman <edwin.kortman@pm.me>
2022-06-27 14:54:44 +0200

    Let the repository use a PackageURL instead

--- Corgibytes.Freshli.Cli.Test/DependencyManagers/MockAgentsDetectRepository.cs
+++ Corgibytes.Freshli.Cli.Test/DependencyManagers/MockAgentsDetectRepository.cs
@@ -1,21 +1,22 @@
 using System;
 using Corgibytes.Freshli.Cli.DependencyManagers;
+using PackageUrl;
 
 namespace Corgibytes.Freshli.Cli.Test.DependencyManagers;
 
 public class MockAgentsDetectDependencyManagerRepository : IDependencyManagerRepository
 {
-    public DateTimeOffset GetReleaseDate(string name, string version) => (name, version) switch
+    public DateTimeOffset GetReleaseDate(PackageURL packageUrl) => (packageUrl.Name + " " + packageUrl.Version) switch
     {
-        ("Newtonsoft.Json", "3.22.2021") => new(2021, 3, 22, 0, 0, 0, TimeSpan.Zero),
-        ("Newtonsoft.Json", "8.3.2014") => new(2014, 8, 3, 0, 0, 0, TimeSpan.Zero),
-        ("DifferentTimezone", "3.22.2021") => new(2021, 3, 22, 0, 0, 0, TimeSpan.FromHours(-10)),
-        ("DifferentTimezone", "8.3.2014") => new(2014, 8, 3, 0, 0, 0, TimeSpan.FromHours(-10)),
-        ("calculatron", "21.3") => new(2022, 6, 16, 0, 0, 0, TimeSpan.Zero),
-        ("calculatron", "14.6") => new(2019, 12, 31, 0, 0, 0, TimeSpan.Zero),
-        ("flyswatter", "1.1.0") => new(1990, 1, 29, 0, 0, 0, TimeSpan.Zero),
-        ("auto-cup-of-tea", "112.0") => new(2004, 11, 11, 0, 0, 0, TimeSpan.Zero),
-        ("auto-cup-of-tea", "256.0") => new(2011, 10, 26, 0, 0, 0, TimeSpan.Zero),
+        ("Newtonsoft.Json 3.22.2021") => new(2021, 3, 22, 0, 0, 0, TimeSpan.Zero),
+        ("Newtonsoft.Json 8.3.2014") => new(2014, 8, 3, 0, 0, 0, TimeSpan.Zero),
+        ("DifferentTimezone 3.22.2021") => new(2021, 3, 22, 0, 0, 0, TimeSpan.FromHours(-10)),
+        ("DifferentTimezone 8.3.2014") => new(2014, 8, 3, 0, 0, 0, TimeSpan.FromHours(-10)),
+        ("calculatron 21.3") => new(2022, 6, 16, 0, 0, 0, TimeSpan.Zero),
+        ("calculatron 14.6") => new(2019, 12, 31, 0, 0, 0, TimeSpan.Zero),
+        ("flyswatter 1.1.0") => new(1990, 1, 29, 0, 0, 0, TimeSpan.Zero),
+        ("auto-cup-of-tea 112.0") => new(2004, 11, 11, 0, 0, 0, TimeSpan.Zero),
+        ("auto-cup-of-tea 256.0") => new(2011, 10, 26, 0, 0, 0, TimeSpan.Zero),
         _ => throw new ArgumentException("Mock date could not be returned. Forgot to add it?")
     };
 
--- Corgibytes.Freshli.Cli/DependencyManagers/AgentsRepository.cs
+++ Corgibytes.Freshli.Cli/DependencyManagers/AgentsRepository.cs
@@ -1,16 +1,17 @@
 using System;
+using PackageUrl;
 
 namespace Corgibytes.Freshli.Cli.DependencyManagers;
 
 public class AgentsRepository : IDependencyManagerRepository
 {
-    public DateTimeOffset GetReleaseDate(string name, string version) => (name, version) switch
+    public DateTimeOffset GetReleaseDate(PackageURL packageUrl) => (packageUrl.Name + " " + packageUrl.Version) switch
     {
-        ("calculatron", "21.3") => new(new(2022, 6, 16)),
-        ("calculatron", "14.6") => new(new(2019, 12, 31)),
-        ("flyswatter", "1.1.0") => new(new(1990, 1, 29)),
-        ("auto-cup-of-tea", "112.0") => new(new(2004, 11, 11)),
-        ("auto-cup-of-tea", "256.0") => new(new(2011, 10, 26)),
+        ("calculatron 1.3") => new(new(2022, 6, 16)),
+        ("calculatron 14.6") => new(new(2019, 12, 31)),
+        ("flyswatter 1.1.0") => new(new(1990, 1, 29)),
+        ("auto-cup-of-tea 112.0") => new(new(2004, 11, 11)),
+        ("auto-cup-of-tea 256.0") => new(new(2011, 10, 26)),
         _ => throw new ArgumentException("Mock date could not be returned. Forgot to add it?")
     };
 
