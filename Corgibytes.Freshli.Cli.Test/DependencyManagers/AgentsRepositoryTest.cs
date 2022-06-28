4e44aee8d6682dc3e9e4573514c67fa93fabf55c
Edwin Kortman <edwin.kortman@pm.me>
2022-06-28 11:36:19 +0200

    Add a mocked method to fetch the latest version

--- Corgibytes.Freshli.Cli.Test/DependencyManagers/AgentsRepositoryTest.cs
+++ Corgibytes.Freshli.Cli.Test/DependencyManagers/AgentsRepositoryTest.cs
@@ -26,14 +26,33 @@ public class AgentsRepositoryTest : FreshliTest
         Assert.Equal(new(1990, 1, 29, 0, 0, 0, TimeSpan.Zero), _repository.GetReleaseDate(givenPackageUrl));
     }
 
+    [Fact]
+    public void It_is_able_to_fetch_the_latest_release_date()
+    {
+        var givenPackageUrl = new PackageURL("pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.1.0");
+
+        Assert.Equivalent(new PackageURL("pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.3.0"), _repository.GetLatestVersion(givenPackageUrl));
+    }
+
     [Theory]
     [InlineData("pkg:this.cant.be.real/org.corgibytes.random_package/randompackage@1.1.0", "None of the agents returned results for this package url: pkg:this.cant.be.real/org.corgibytes.random_package/randompackage@1.1.0")]
     [InlineData("pkg:ruby/org.corgibytes.no_release_date/no_release_date@1.1.0", "The returned list did not contain a release date for this package url: pkg:ruby/org.corgibytes.no_release_date/no_release_date@1.1.0")]
-    public void It_errors_when_it_cant_process_the_package_url(string packageUrl, string expectedErrorMessage)
+    public void It_errors_when_it_can_not_find_the_release_date(string packageUrl, string expectedErrorMessage)
     {
         var givenPackageUrl = new PackageURL(packageUrl);
 
         var caughtException = Assert.Throws<ReleaseDateNotFoundException>(() => _repository.GetReleaseDate(givenPackageUrl));
+
+        Assert.Equal(expectedErrorMessage, caughtException.Message);
+    }
+
+    [Theory]
+    [InlineData("pkg:this.cant.be.real/org.corgibytes.random_package/randompackage@1.1.0", "Latest version could not be found in list for this package url: pkg:this.cant.be.real/org.corgibytes.random_package/randompackage@1.1.0")]
+    public void It_errors_when_it_can_not_find_the_latest_version(string packageUrl, string expectedErrorMessage)
+    {
+        var givenPackageUrl = new PackageURL(packageUrl);
+
+        var caughtException = Assert.Throws<LatestVersionNotFoundException>(() => _repository.GetLatestVersion(givenPackageUrl));
 
         Assert.Equal(expectedErrorMessage, caughtException.Message);
     }
