06526eeaae16e18739b5d7865ab7289da734cef8
Edwin Kortman <edwin.kortman@pm.me>
2022-06-30 14:00:47 +0200

    Verify that the git checkout-history command has all it's expected options

---
+++ Corgibytes.Freshli.Cli.Test/Commands/Git/CheckoutHistoryCommandTest.cs
@@ -0,0 +1,46 @@
+using System.CommandLine;
+using System.Linq;
+using Corgibytes.Freshli.Cli.Commands.Git;
+using Corgibytes.Freshli.Cli.Test.Common;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.DependencyInjection;
+
+namespace Corgibytes.Freshli.Cli.Test.Commands.Git;
+
+public class CheckoutHistoryCommandTest : FreshliTest
+{
+    public CheckoutHistoryCommandTest(ITestOutputHelper output) : base(output) { }
+
+    [Fact]
+    public void Verify_git_path_option_configuration()
+    {
+        CheckoutHistoryCommand checkoutHistoryCommand = new();
+        var option = checkoutHistoryCommand.Options.ElementAt(0);
+
+        option.Name.Should().Be("--git-path");
+        option.Arity.Should().BeEquivalentTo(ArgumentArity.ExactlyOne);
+    }
+
+    [Theory]
+    [MethodData(nameof(DataForVerifyArgumentConfiguration))]
+    public void Verify_argument_configuration(string alias, ArgumentArity arity, int elementIndex)
+    {
+        CheckoutHistoryCommand checkoutHistoryCommand = new();
+        var argument = checkoutHistoryCommand.Arguments.ElementAt(elementIndex);
+
+        argument.Name.Should().Be(alias);
+        argument.Arity.Should().BeEquivalentTo(arity);
+    }
+
+    private static TheoryData<string, ArgumentArity, int> DataForVerifyArgumentConfiguration()
+    {
+        return new()
+        {
+            {"repository-id", ArgumentArity.ExactlyOne, 0},
+            {"sha", ArgumentArity.ExactlyOne, 1}
+        };
+    }
+}
+
