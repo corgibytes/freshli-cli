2f060b200ec2fb4df1e3ad8b2eb5dbb09f06a7f5
Edwin Kortman <edwin.kortman@pm.me>
2022-06-30 12:02:16 +0200

    Separate git clone commands and it's options from the git command

---
+++ Corgibytes.Freshli.Cli/CommandOptions/GitCloneCommandOptions.cs
@@ -0,0 +1,9 @@
+namespace Corgibytes.Freshli.Cli.CommandOptions;
+
+public class GitCloneCommandOptions : CommandOptions
+{
+    public string GitPath { get; set; }
+    public string Branch { get; set; }
+    public string RepoUrl { get; set; }
+}
+
---
+++ Corgibytes.Freshli.Cli/CommandRunners/Git/GitCloneCommandRunner.cs
@@ -0,0 +1,36 @@
+using System;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using Corgibytes.Freshli.Cli.CommandOptions;
+using Corgibytes.Freshli.Cli.Extensions;
+using Corgibytes.Freshli.Cli.Functionality;
+using Corgibytes.Freshli.Lib;
+
+namespace Corgibytes.Freshli.Cli.CommandRunners.Git;
+
+public class GitCloneCommandRunner : CommandRunner<GitCloneCommandOptions>
+{
+    public GitCloneCommandRunner(IServiceProvider serviceProvider, Runner runner)
+        : base(serviceProvider, runner)
+    { }
+
+    public override int Run(GitCloneCommandOptions options, InvocationContext context)
+    {
+        // Clone or pull the given repository and branch.
+        var gitRepository = new GitRepository(options.RepoUrl, options.Branch, options.CacheDir);
+        try
+        {
+            gitRepository.CloneOrPull(options.GitPath);
+        }
+        catch (GitException e)
+        {
+            context.Console.Error.WriteLine(e.Message);
+            return false.ToExitCode();
+        }
+
+        // Output the hash to the command line for use by the caller.
+        context.Console.Out.WriteLine(gitRepository.Hash);
+        return true.ToExitCode();
+    }
+}
+
