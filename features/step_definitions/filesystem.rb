b8ae13f19c575404e0b6d34906c4249b612c2d64
Edwin Kortman <edwin.kortman@pm.me>
2022-06-30 13:11:54 +0200

    Checks if a specific directory is not empty (in the tests)
    This will make the test fail because we haven't added the behaviour yet

--- features/step_definitions/filesystem.rb
+++ features/step_definitions/filesystem.rb
@@ -11,3 +11,8 @@ Given('a directory named {string} exists') do |dirname|
   dirname = resolve_path dirname
   Dir.mkdir dirname
 end
+
+Then('a directory named {string} is not empty') do |dirname|
+  dirname = resolve_path dirname
+  expect(Dir.empty?(dirname)).to match(false)
+end
