e3eaa2f7b9c0c48baaea743438614d0193446f4b
Edwin Kortman <edwin.kortman@pm.me>
2022-06-30 14:38:18 +0200

    Adds a git commit identifier object that's able to verify if a given sha is correct

---
+++ Corgibytes.Freshli.Cli/Functionality/GitCommitIdentifier.cs
@@ -0,0 +1,17 @@
+namespace Corgibytes.Freshli.Cli.Functionality;
+
+public class GitCommitIdentifier
+{
+    public string CommitId { get; }
+
+    public GitCommitIdentifier(string commitId)
+    {
+        if (commitId.Length > 40 || commitId.Length < 7)
+        {
+            throw new GitException($"Git commit id ({commitId}) not valid. It should be between 7 and 40 characters long. Given string is {commitId.Length} characters long");
+        }
+
+        CommitId = commitId;
+    }
+}
+
