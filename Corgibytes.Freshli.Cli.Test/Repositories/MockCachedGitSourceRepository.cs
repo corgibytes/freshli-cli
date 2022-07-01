8bd793daa1adc2783480fe42c3c4484ee55792be
Edwin Kortman <edwin.kortman@pm.me>
2022-07-01 14:35:06 +0200

    Adds a cached git source repository

---
+++ Corgibytes.Freshli.Cli.Test/Repositories/MockCachedGitSourceRepository.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.IO;
+using Corgibytes.Freshli.Cli.Functionality;
+using Corgibytes.Freshli.Cli.Repositories;
+
+namespace Corgibytes.Freshli.Cli.Test.Repositories;
+
+public class MockCachedGitSourceRepository : ICachedGitSourceRepository
+{
+    private List<CachedGitSource> _list;
+
+    public MockCachedGitSourceRepository() => _list = new();
+
+    public void addToList(CachedGitSource cachedGitSource)
+    {
+        _list.Add(cachedGitSource);
+    }
+
+    public CachedGitSource FindOneByHash(string hash, DirectoryInfo cacheDir)
+    {
+        return _list.Find(gitSource => gitSource.Id == hash);
+    }
+}
+
