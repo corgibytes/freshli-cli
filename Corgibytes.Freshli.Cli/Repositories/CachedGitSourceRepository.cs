840e2beca26c1320b4dad2abba17e906bc575af8
Edwin Kortman <edwin.kortman@pm.me>
2022-07-01 14:35:06 +0200

    Adds a cached git source repository

---
+++ Corgibytes.Freshli.Cli/Repositories/CachedGitSourceRepository.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using Corgibytes.Freshli.Cli.Functionality;
+
+namespace Corgibytes.Freshli.Cli.Repositories;
+
+public class CachedGitSourceRepository : ICachedGitSourceRepository
+{
+    public CachedGitSource FindOneByHash(string hash, DirectoryInfo cacheDir)
+    {
+        using var db = new CacheContext(cacheDir);
+        var entry = db.CachedGitRepos.Find(hash);
+        if (entry == null)
+        {
+            throw new CacheException("No repository with this hash exists in cache.");
+        }
+
+        return entry;
+    }
+}
+
