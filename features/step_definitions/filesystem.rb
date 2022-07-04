b89f6478cc6571e9d637cded1edb38ba3a35e314
Edwin Kortman <edwin.kortman@pm.me>
2022-07-04 16:14:40 +0200

    Rephrase a 'Given a directory named "something" exists'
    When writing Cucumber scenario's the phrasing appears to favor tests
    from an I do something perspective. We also had a conflict because for
    the "Then a directory named something exists" it made more sense to have
    this kind of phrasing.

--- features/step_definitions/filesystem.rb
+++ features/step_definitions/filesystem.rb
@@ -7,9 +7,14 @@ Given('a blank file named {string} exists') do |filename|
   FileUtils.touch filename
 end
 
-Given('a directory named {string} exists') do |dirname|
+Given('I create a directory named {string}') do |dirname|
   dirname = resolve_path dirname
   Dir.mkdir dirname
+end
+
+Then('a directory named {string} exists') do |dirname|
+  dirname = resolve_path dirname
+  expect(Dir.exists?(dirname)).to match(true)
 end
 
 Then('a directory named {string} is not empty') do |dirname|
