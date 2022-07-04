9406542bb539ae997c2b48605ae31ff1073f9c50
Edwin Kortman <edwin.kortman@pm.me>
2022-07-04 16:17:35 +0200

    Write the created path to the output

--- Corgibytes.Freshli.Cli/CommandRunners/Git/CheckoutHistoryCommandRunner.cs
+++ Corgibytes.Freshli.Cli/CommandRunners/Git/CheckoutHistoryCommandRunner.cs
@@ -1,18 +1,32 @@
 using System;
 using System.CommandLine.Invocation;
+using System.CommandLine.IO;
 using Corgibytes.Freshli.Cli.CommandOptions;
+using Corgibytes.Freshli.Cli.Functionality.Git;
 using Corgibytes.Freshli.Lib;
 
 namespace Corgibytes.Freshli.Cli.CommandRunners.Git;
 
 public class CheckoutHistoryCommandRunner : CommandRunner<CheckoutHistoryCommandOptions>
 {
-    public CheckoutHistoryCommandRunner(IServiceProvider serviceProvider, Runner runner) : base(serviceProvider, runner)
+    private readonly GitArchive _gitArchive;
+
+    public CheckoutHistoryCommandRunner(IServiceProvider serviceProvider, Runner runner, GitArchive gitArchive) : base(serviceProvider, runner)
     {
+        _gitArchive = gitArchive;
     }
 
     public override int Run(CheckoutHistoryCommandOptions options, InvocationContext context)
     {
+        context.Console.Out.WriteLine(
+            _gitArchive.CreateArchive(
+                options.RepositoryId,
+                options.CacheDir,
+                new(options.Sha),
+                options.GitPath
+            )
+        );
+
         return 0;
     }
 }
