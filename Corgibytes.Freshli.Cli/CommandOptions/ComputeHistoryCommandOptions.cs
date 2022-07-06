ffa5503db021d32e52f8261394954de978d830e1
Edwin Kortman <edwin@corgibytes.com>
2022-07-06 17:58:12 +0200

    Adds git path as a command option
    If the git path differs from the default value `git` then it needs to be
    specified

--- Corgibytes.Freshli.Cli/CommandOptions/ComputeHistoryCommandOptions.cs
+++ Corgibytes.Freshli.Cli/CommandOptions/ComputeHistoryCommandOptions.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace Corgibytes.Freshli.Cli.CommandOptions;
 
 public class ComputeHistoryCommandOptions : CommandOptions
@@ -5,5 +7,6 @@ public class ComputeHistoryCommandOptions : CommandOptions
     public string RepositoryId { get; set; }
     public bool CommitHistory { get; set; }
     public string HistoryInterval { get; set; }
+    public FileInfo GitPath { get; set; }
 }
 
