9ac8a7c20a0b70cabe15d5452fbc31642ff9d196
Edwin Kortman <edwin.kortman@pm.me>
2022-07-06 14:08:50 +0200

    Adds blank command, options and runner for compute history

---
+++ Corgibytes.Freshli.Cli/Commands/Git/ComputeHistoryCommand.cs
@@ -0,0 +1,39 @@
+using System.CommandLine;
+using Corgibytes.Freshli.Cli.CommandOptions;
+
+namespace Corgibytes.Freshli.Cli.Commands.Git;
+
+public class ComputeHistoryCommand : RunnableCommand<ComputeHistoryCommand, ComputeHistoryCommandOptions>
+{
+    public ComputeHistoryCommand() : base("compute-history", "Used to examine the history of the specified repository and determine the sha hashes that will need to be checked out to complete a historical analysis at the intervals specified.")
+    {
+        Argument<string> repositoryIdentifier = new("repository-id", "The ID of the repository to compute history for")
+        {
+            Arity = ArgumentArity.ExactlyOne
+        };
+
+        AddArgument(repositoryIdentifier);
+
+        Option<bool> commitHistory = new(
+            "--commit-history",
+            description: "Analyzes only the points in time when files have changed in the commit history"
+        )
+        {
+            AllowMultipleArgumentsPerToken = false,
+            Arity = ArgumentArity.ZeroOrOne
+        };
+        AddOption(commitHistory);
+
+        Option<string> historyInterval = new(
+            "--history-interval",
+            description: "As the analyze command moves backwards in time through the history of the project, what time interval should it use to determine the points in time that are sampled",
+            getDefaultValue: () => "m"
+        )
+        {
+            AllowMultipleArgumentsPerToken = false,
+            Arity = ArgumentArity.ZeroOrOne
+        };
+        AddOption(historyInterval);
+    }
+}
+
