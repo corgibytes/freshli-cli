185c7752c93d3d5c753f34fe58e7de0f381ac337
Edwin Kortman <edwin@corgibytes.com>
2022-07-07 17:13:17 +0200

    Address linter errors for ruby files

--- features/step_definitions/git.rb
+++ features/step_definitions/git.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Then('a Git repository exists at {string} with a Git SHA {string} checked out') do |path, sha|
   repo = resolve_path path
   expect(repo.nil?).to match(false)
-  git_index = repo + '/.git/index'
+  git_index = "#{repo}/.git/index"
   expect(File.file?(git_index)).to match(true)
   actual_sha = run_from(repo, 'git show-ref HEAD').split[0]
   expect(actual_sha).to match(sha)
