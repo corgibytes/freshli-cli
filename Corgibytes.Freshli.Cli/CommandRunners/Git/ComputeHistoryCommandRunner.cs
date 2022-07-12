288b9b5eb70b5e991aa9d5d025b427fe113e5150
Edwin Kortman <edwin@corgibytes.com>
2022-07-12 12:53:52 +0200

    refactor: Let the command runner use the compute history service to actually output a list of commit identifiers, and dates.
    Before it was just returning a default value.

--- Corgibytes.Freshli.Cli/CommandRunners/Git/ComputeHistoryCommandRunner.cs
+++ Corgibytes.Freshli.Cli/CommandRunners/Git/ComputeHistoryCommandRunner.cs
@@ -1,16 +1,33 @@
 using System;
+using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Globalization;
 using Corgibytes.Freshli.Cli.CommandOptions;
 using Corgibytes.Freshli.Cli.Commands.Git;
+using Corgibytes.Freshli.Cli.Functionality.Git;
 using Corgibytes.Freshli.Lib;
 
 namespace Corgibytes.Freshli.Cli.CommandRunners.Git;
 
 public class ComputeHistoryCommandRunner : CommandRunner<ComputeHistoryCommand, ComputeHistoryCommandOptions>
 {
-    public ComputeHistoryCommandRunner(IServiceProvider serviceProvider, Runner runner) : base(serviceProvider, runner)
+    private readonly ComputeHistory _computeHistory;
+
+    public ComputeHistoryCommandRunner(IServiceProvider serviceProvider, Runner runner, ComputeHistory computeHistory) : base(serviceProvider, runner)
     {
+        _computeHistory = computeHistory;
     }
 
-    public override int Run(ComputeHistoryCommandOptions options, InvocationContext context) => 0;
+    public override int Run(ComputeHistoryCommandOptions options, InvocationContext context)
+    {
+        foreach (var historyIntervalStop in
+                 _computeHistory.ComputeWithHistoryInterval(options.RepositoryId, options.GitPath.FullName, options.HistoryInterval))
+        {
+            context.Console.WriteLine(
+                historyIntervalStop.CommittedAt.ToString(CultureInfo.InvariantCulture) + " " + historyIntervalStop.GitCommitIdentifier
+            );
+        }
+
+        return 0;
+    }
 }
