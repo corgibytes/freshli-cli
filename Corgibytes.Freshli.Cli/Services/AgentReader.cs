102289a6f871f28f393f59bec461329349de4535
Edwin Kortman <edwin@corgibytes.com>
2022-07-13 14:15:41 +0200

    refactor: Let the agents reader, accept freshli-agent-test as well
    This is done, so that we can let the test generate this file. This will make it work as the Agents Reader is just returning static data.

--- Corgibytes.Freshli.Cli/Services/AgentReader.cs
+++ Corgibytes.Freshli.Cli/Services/AgentReader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Corgibytes.Freshli.Cli.Functionality;
 using PackageUrl;
 
@@ -11,25 +12,29 @@ public class AgentReader : IAgentReader
 {
     public List<Package> ListValidPackageUrls(string agentExecutable, PackageURL packageUrl) => agentExecutable switch
     {
-        "/usr/local/bin/freshli-agent-csharp" => packageUrl.Name switch
-        {
-            "flyswatter" => new()
-            {
-                new(new("pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.1.0"),
-                    new(1990, 11, 29, 0, 0, 0, TimeSpan.Zero))
-            },
-            "calculatron" => new()
-            {
-                new(new("pkg:nuget/org.corgibytes.calculatron/calculatron@21.3"),
-                    new(2022, 10, 16, 0, 0, 0, TimeSpan.Zero)),
-                new(new("pkg:nuget/org.corgibytes.calculatron/calculatron@14.6"),
-                    new(2019, 12, 31, 0, 0, 0, TimeSpan.Zero))
-            },
-            _ => new()
-        },
+        "/usr/local/bin/freshli-agent-csharp" => ReturnListOfPackages(packageUrl),
+        _ when agentExecutable.Contains("freshli-agent-test") => ReturnListOfPackages(packageUrl),
         "/usr/local/bin/freshli-agent-javascript" => new(),
         "/usr/local/agents/bin/freshli-agent-csharp" => new(),
         "/home/freshli-user/bin/agents/freshli-agent-ruby" => new(),
+        _ => new()
+    };
+
+    private static List<Package> ReturnListOfPackages(PackageURL packageUrl) =>
+    packageUrl.Name switch
+    {
+        "flyswatter" => new()
+        {
+            new(new("pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.1.0"),
+                new(1990, 11, 29, 0, 0, 0, TimeSpan.Zero))
+        },
+        "calculatron" => new()
+        {
+            new(new("pkg:nuget/org.corgibytes.calculatron/calculatron@21.3"),
+                new(2022, 10, 16, 0, 0, 0, TimeSpan.Zero)),
+            new(new("pkg:nuget/org.corgibytes.calculatron/calculatron@14.6"),
+                new(2019, 12, 31, 0, 0, 0, TimeSpan.Zero))
+        },
         _ => new()
     };
 }
