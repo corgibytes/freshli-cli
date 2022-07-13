fb3738df9ade648c0fa404da38b6179984585d3a
Jason C. McDonald <codemouse92@outlook.com>
2022-07-13 13:42:23 -0500

    refactor: work nullability into libyear

--- Corgibytes.Freshli.Cli/Functionality/JsonCycloneDX.cs
+++ Corgibytes.Freshli.Cli/Functionality/JsonCycloneDX.cs
@@ -8,9 +8,11 @@ namespace Corgibytes.Freshli.Cli.Functionality;
 
 public class JsonCycloneDx
 {
-    public IList<Component> Components { get; set; }
+    public JsonCycloneDx(IList<Component> components) => Components = components;
 
-    public static JsonCycloneDx FromJson(string json)
+    public IList<Component>? Components { get; set; }
+
+    public static JsonCycloneDx? FromJson(string json)
     {
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         options.Converters.Add(new JsonStringEnumConverter());
@@ -20,6 +22,6 @@ public class JsonCycloneDx
 
     public class Component
     {
-        public string Purl { get; set; }
+        public string? Purl { get; set; }
     }
 }
