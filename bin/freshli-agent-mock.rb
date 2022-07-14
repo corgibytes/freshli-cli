ed3dd168706c9ca0d7914f41527a363a5fc925bc
donabp <dona@corgibytes.com>
2022-07-14 10:01:11 -0400

    test: Finishes basic functionality
    This version intentionally ignores any input parameters to the
    commands.
    
    Co-Authored-By: Jason C. McDonald <jasonm@corgibytes.com>
    Co-Authored-By: M. Scott Ford <scott@corgibytes.com>

--- bin/freshli-agent-mock.rb
+++ bin/freshli-agent-mock.rb
@@ -6,17 +6,24 @@ require 'securerandom'
 
 case ARGV[0]
 when 'validating-package-urls'
-  puts "pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.1.0"
+  puts "pkg:nuget/org.corgibytes.flyswatter/flyswatter"
 when 'retrieve-release-history'
-  puts "pkg:nuget/org.corgibytes.flyswatter/flyswatter@1.1.0\t1990-01-29T12:15:25Z"
+  puts "1.1.0\t1990-01-29T12:15:25Z"
+  puts "1.2.0\t1990-04-17T13:14:45Z"
 when 'validating-repositories'
   puts "https://github.com/corgibytes/freshli-fixture-ruby-nokotest"
 when 'process-manifests'
-  puts ARGV[0]
+  puts "freshli-fixture-ruby-nokotest/Gemfile\tfreshli-fixture-ruby-nokotest/Gemfile-pinned\tfreshli-fixture-ruby-nokotest/Gemfile-pinned.bom"
+  puts "freshli-fixture-ruby-nokotest/Gemfile.lock\tfreshli-fixture-ruby-nokotest/Gemfile.lock\tfreshli-fixture-ruby-nokotest/Gemfile.lock.bom"
+
 when 'detect-manifests'
   manifest_id = Digest::SHA1.hexdigest SecureRandom.uuid
   repo_id = Digest::SHA256.hexdigest "https://github.com/corgibytes/freshli-fixture-ruby-nokotestmaster"
-  puts "#{repo_id}\t#{manifest_id}"
+
+  manifest_hash = "2ec0807d95c45cdd0a5579589caa4f0bd0dac425d6447c02bfc85d182c189817"
+  manifest_path = "freshli-fixture-ruby-nokotest/Gemfile.lock"
+
+  puts "#{repo_id}\t#{manifest_id}\t#{manifest_hash}\t#{manifest_path}"
 end
 
 
