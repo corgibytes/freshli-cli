7ade58641b223ab72c2e3e8027bd7037c0e03ae9
Edwin Kortman <edwin@corgibytes.com>
2022-07-14 10:20:53 +0200

    refactor: set default value for the git path on the command line

--- Corgibytes.Freshli.Cli/CommandOptions/Git/CheckoutHistoryCommandOptions.cs
+++ Corgibytes.Freshli.Cli/CommandOptions/Git/CheckoutHistoryCommandOptions.cs
@@ -4,5 +4,5 @@ public class CheckoutHistoryCommandOptions : CommandOptions
 {
     public string RepositoryId { get; set; }
     public string Sha { get; set; }
-    public string GitPath { get; set; } = "git";
+    public string GitPath { get; set; }
 }
