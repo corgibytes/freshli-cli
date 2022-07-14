c37ca2bdcd42c4b63e8ce53d504317fc7e0815af
Edwin Kortman <edwin@corgibytes.com>
2022-07-14 11:19:19 +0200

    refactor: adds translatable strings

--- Corgibytes.Freshli.Cli/Resources/CliOutput.Designer.cs
+++ Corgibytes.Freshli.Cli/Resources/CliOutput.Designer.cs
@@ -9,21 +9,21 @@
 
 namespace Corgibytes.Freshli.Cli.Resources {
     using System;
-
-
+    
+    
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class CliOutput {
-
+        
         private static System.Resources.ResourceManager resourceMan;
-
+        
         private static System.Globalization.CultureInfo resourceCulture;
-
+        
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal CliOutput() {
         }
-
+        
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
@@ -34,7 +34,7 @@ namespace Corgibytes.Freshli.Cli.Resources {
                 return resourceMan;
             }
         }
-
+        
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Globalization.CultureInfo Culture {
             get {
@@ -44,94 +44,106 @@ namespace Corgibytes.Freshli.Cli.Resources {
                 resourceCulture = value;
             }
         }
-
+        
         internal static string Output_Date {
             get {
                 return ResourceManager.GetString("Output_Date", resourceCulture);
             }
         }
-
+        
         internal static string Output_LibYear {
             get {
                 return ResourceManager.GetString("Output_LibYear", resourceCulture);
             }
         }
-
+        
         internal static string Output_UpgradesAvailable {
             get {
                 return ResourceManager.GetString("Output_UpgradesAvailable", resourceCulture);
             }
         }
-
+        
         internal static string Output_Skipped {
             get {
                 return ResourceManager.GetString("Output_Skipped", resourceCulture);
             }
         }
-
+        
         internal static string ScanCommand_ScanCommand_Executing_scan_command_handler {
             get {
                 return ResourceManager.GetString("ScanCommand_ScanCommand_Executing_scan_command_handler", resourceCulture);
             }
         }
-
+        
         internal static string MainCommand_MainCommand_Root_Command_Executed {
             get {
                 return ResourceManager.GetString("MainCommand_MainCommand_Root_Command_Executed", resourceCulture);
             }
         }
-
+        
         internal static string AuthCommand_AuthCommand_Executing_auth_command_handler {
             get {
                 return ResourceManager.GetString("AuthCommand_AuthCommand_Executing_auth_command_handler", resourceCulture);
             }
         }
-
+        
         internal static string ScanCommandRunner_Run_Path_should_not_be_null_or_empty {
             get {
                 return ResourceManager.GetString("ScanCommandRunner_Run_Path_should_not_be_null_or_empty", resourceCulture);
             }
         }
-
+        
         internal static string ComputeLibYearCommandRunner_Run_FilePath_should_not_be_null_or_empty {
             get {
                 return ResourceManager.GetString("ComputeLibYearCommandRunner_Run_FilePath_should_not_be_null_or_empty", resourceCulture);
             }
         }
-
+        
         internal static string AgentsDetectCommandRunner_Run_No_detected_agents_found {
             get {
                 return ResourceManager.GetString("AgentsDetectCommandRunner_Run_No_detected_agents_found", resourceCulture);
             }
         }
-
+        
         internal static string ComputeLibYearCommandRunner_Table_Header_Package {
             get {
                 return ResourceManager.GetString("ComputeLibYearCommandRunner_Table_Header_Package", resourceCulture);
             }
         }
-
+        
         internal static string ComputeLibYearCommandRunner_Table_Header_Currently_Installed {
             get {
                 return ResourceManager.GetString("ComputeLibYearCommandRunner_Table_Header_Currently_Installed", resourceCulture);
             }
         }
-
+        
         internal static string ComputeLibYearCommandRunner_Table_Header_Released_at {
             get {
                 return ResourceManager.GetString("ComputeLibYearCommandRunner_Table_Header_Released_at", resourceCulture);
             }
         }
-
+        
         internal static string ComputeLibYearCommandRunner_Table_Header_Latest_Available {
             get {
                 return ResourceManager.GetString("ComputeLibYearCommandRunner_Table_Header_Latest_Available", resourceCulture);
             }
         }
-
+        
         internal static string ReadCycloneDxFile_Exception_Can_Not_Read_File {
             get {
                 return ResourceManager.GetString("ReadCycloneDxFile_Exception_Can_Not_Read_File", resourceCulture);
+            }
+        }
+        
+        internal static string CachedGitSourceRepository_No_Repository_Found_In_Cache {
+            get {
+                return ResourceManager.GetString("CachedGitSourceRepository_No_Repository_Found_In_Cache", resourceCulture);
+            }
+        }
+        
+        internal static string GitArchive_Git_Exception {
+            get {
+                return ResourceManager.GetString("GitArchive_Git_Exception", resourceCulture);
             }
         }
     }
