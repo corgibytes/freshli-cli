73fbc30e160fa5ac632e76b054b383ea73bf14e1
Jason C. McDonald <codemouse92@outlook.com>
2022-07-14 11:38:11 -0500

    fix: raise exception if file has no path
    This scenario can only happen if there's a bug in
    AgentsDetector.Detect.

--- Corgibytes.Freshli.Cli/CommandRunners/AgentsCommandRunner.cs
+++ Corgibytes.Freshli.Cli/CommandRunners/AgentsCommandRunner.cs
@@ -35,7 +35,9 @@ public class AgentsDetectCommandRunner : CommandRunner<AgentsDetectCommand, Empt
         // Path.GetFileName returns string?, but ToDictionary needs string. We resolve this with the following, and
         // then tell the compiler to stop complaining that GetFileName(x) is never null; this is about the return type.
         // ReSharper disable once ConstantNullCoalescingCondition
-        var agentsAndLocations = agents.ToDictionary(x => Path.GetFileName(x) ?? "");
+        var agentsAndLocations = agents.ToDictionary(
+            x => Path.GetFileName(x) ?? throw new ArgumentException("No file name for given path.")
+        );
 
         var basicTable = new TextTable(2);
         basicTable.AddCell("Agent file");
