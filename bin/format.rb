1e710580d9a0345001f9dba0aa0044bd8e1132fc
Jason C. McDonald <codemouse92@outlook.com>
2022-07-19 14:34:15 -0500

    build: reverse the order of formatters
    The order in which we run formatters in bin/format.rb should be
    the same as the order in which we run the associated linters in
    bin/lint.rb. This ensures consistency in precedence, and allows
    dotnet format (which was flagging the change made by jb cleanupcode)
    the opportunity to reverse the unwanted change.
    
    However, this is only a workaround. We still need to determine
    the conflicting rules in `.editorconfig` that is causing this
    formatting change in MainCommand.rb to thrash.

--- bin/format.rb
+++ bin/format.rb
@@ -7,14 +7,14 @@ enable_dotnet_command_colors
 
 eclint_status = execute('eclint -fix')
 
-dotnet_format_status = execute('dotnet format --severity info')
-
 status = execute('bundle check > /dev/null')
 status = execute('bundle install') unless status.success?
 
-rubocop_status = execute('bundle exec rubocop --autocorrect --color') if status.success?
-
 resharper_status = execute('dotnet jb cleanupcode freshli-cli.sln') if status.success?
+
+dotnet_format_status = execute('dotnet format --severity info') if status.success?
+
+rubocop_status = execute('bundle exec rubocop --autocorrect --color') if status.success?
 
 composite_exitstatus =
   dotnet_format_status.exitstatus + rubocop_status.exitstatus + resharper_status.exitstatus + eclint_status.exitstatus
