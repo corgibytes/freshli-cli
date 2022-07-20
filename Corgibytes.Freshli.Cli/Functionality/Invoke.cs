702bb9a3ba637a16530004910acbac0831caa2b6
Jason C. McDonald <codemouse92@outlook.com>
2022-07-20 14:34:33 -0500

    refactor: abstract out external process calls
    Adds the Invoke.Command() and Invoke.Freshli() methods for
    executing external processes with CliWrap. This eliminates
    boilerplate from the rest of the project.
    
    Changes all prior direct invocations of processes with CliWrap
    to use Invoke.Command() instead.
    
    Invoke.Freshli() method will be needed in upcoming work. This just
    defines it, since it was written as part of a now-abandoned task
    and worth retaining.
    
    Co-Authored-By: Dona Pop <dona@corgibytes.com>

---
+++ Corgibytes.Freshli.Cli/Functionality/Invoke.cs
@@ -0,0 +1,53 @@
+using System.IO;
+using System.Reflection;
+using System.Text;
+using CliWrap;
+
+namespace Corgibytes.Freshli.Cli.Functionality;
+
+public static class Invoke
+{
+    public static string Command(string executable, string arguments)
+    {
+        var stdOutBuffer = new StringBuilder();
+        var stdErrBuffer = new StringBuilder();
+
+        var command = CliWrap.Cli.Wrap(executable).WithArguments(
+                args => args
+                    .Add(arguments.Split())
+            )
+            .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
+            .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer));
+
+        using var task = command.ExecuteAsync().Task;
+        task.Wait();
+
+        if (task.Result.ExitCode != 0)
+        {
+            throw new IOException(stdErrBuffer.ToString());
+        }
+
+        return stdOutBuffer.ToString();
+    }
+
+    // This will be needed in later work, but is being defined now, so we'll disable the warning about being unused.
+    // ReSharper disable once UnusedMember.Global
+    public static string Freshli(string arguments)
+    {
+        var executionLocation = new FileInfo(
+            Assembly.GetExecutingAssembly().Location
+        ).Directory!; // null is forgivable here, because if we get null, there are far weirder problems afoot
+        var executable = new FileInfo(executionLocation.FullName + "/freshli");
+
+        try
+        {
+            return Command(executable.FullName, arguments);
+        }
+        catch (IOException e)
+        {
+            throw new IOException(
+                $"Invoking 'freshli {arguments}' failed with the following output:\n{e.Message}"
+            );
+        }
+    }
+}
