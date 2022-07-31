8ef839ce283a76fff072a0a981d04c5f7c79bc79
donabp <dona@corgibytes.com>
2022-07-31 19:23:10 -0400

    Changed RunAgentsVerify to use Invoke.Commands

--- Corgibytes.Freshli.Cli/CommandRunners/AgentsCommandRunner.cs
+++ Corgibytes.Freshli.Cli/CommandRunners/AgentsCommandRunner.cs
@@ -50,9 +50,9 @@ public class AgentsDetectCommandRunner : CommandRunner<AgentsDetectCommand, Empt
 
 public class AgentsVerifyCommandRunner : CommandRunner<AgentsVerifyCommand, AgentsVerifyCommandOptions>
 {
-    private readonly AgentsDetector _agentsDetector;
+    private readonly IAgentsDetector _agentsDetector;
     public AgentsVerifier AgentsVerifier { get; }
-    public AgentsVerifyCommandRunner(IServiceProvider serviceProvider, Runner runner, AgentsVerifier agentsVerifier, AgentsDetector agentsDetector)
+    public AgentsVerifyCommandRunner(IServiceProvider serviceProvider, Runner runner, AgentsVerifier agentsVerifier, IAgentsDetector agentsDetector)
         : base(serviceProvider, runner)
     {
         _agentsDetector = agentsDetector;
@@ -60,67 +60,26 @@ public class AgentsVerifyCommandRunner : CommandRunner<AgentsVerifyCommand, Agen
     }
 
     public override int Run(AgentsVerifyCommandOptions options, InvocationContext context)
-    {
+    {   
         var agents = _agentsDetector.Detect();
 
-
         if (options.LanguageName == null)
-        {
-            
+        {   
             foreach (var agentsAndPath in agents)
             {
-             
               AgentsVerifier.RunAgentsVerify(agentsAndPath,"validating-repositories", options.CacheDir, "");
-            //  AgentsVerifier.RunProcessOutput(agentsAndPath,"detect-manifests");
-            //  AgentsVerifier.RunProcessOutput(agentsAndPath,"process-manifests");
             }
         } else
-        {
-            
+        {            
             foreach (var agentsAndPath in agents)
             {
                 if(agentsAndPath.ToLower().Contains("freshli-agent-"+options.LanguageName.ToLower())){
-                
                 AgentsVerifier.RunAgentsVerify(agentsAndPath,"validating-repositories", options.CacheDir, options.LanguageName);
-                // AgentsVerifier.RunProcessOutput(agentsAndPath,"detect-manifests");
-                // AgentsVerifier.RunProcessOutput(agentsAndPath,"process-manifests");
-
                 }
                 
             }
-            Console.WriteLine(options.LanguageName + " my word length");
         }
-        
-       
-        
 
-
-        // string executableFile = "/Users/dona/Desktop/projects/freshli-cli/agents_verify.sh";
-        // var processInfo = new ProcessStartInfo();
-        // processInfo.UseShellExecute = false;
-        // processInfo.FileName = executableFile;   // 'sh' for bash
-        //
-        // var stdErrBuffer = new StringBuilder();
-        // var stdOutBuffer = new StringBuilder();
-        // var command = CliWrap.Cli.Wrap(gitPath).WithArguments(
-        //         args => args
-        //             .Add("log")
-        //             // Commit hash, author date, strict ISO 8601 format
-        //             // Lists commits as '583d813db3e28b9b44a29db352e2f0e1b4c6e420 2021-05-19T15:24:24-04:00'
-        //             // Source: https://git-scm.com/docs/pretty-formats
-        //             .Add("--pretty=format:%H %aI")
-        //     )
-        //     .WithValidation(CommandResultValidation.None)
-        //     .WithWorkingDirectory(gitSource.Directory.FullName)
-        //     .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
-        //     .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer));
-        //
-        // using var task = command.ExecuteAsync().Task;
-        // task.Wait();
-
-        
-
-        context.Console.WriteLine(options.Workers + " Dona's workers found");
         return 0;
     }
 }
--- Corgibytes.Freshli.Cli/Commands/AgentsVerifier.cs
+++ Corgibytes.Freshli.Cli/Commands/AgentsVerifier.cs
@@ -17,56 +17,40 @@ public class AgentsVerifier
 
     public void RunAgentsVerify(string AgentFileAndPath, string Argument, DirectoryInfo CacheDir, String languageName ){
 
-        var processInfo = new ProcessStartInfo();
-        processInfo.UseShellExecute = false;
-        processInfo.FileName = AgentFileAndPath;   // 'sh' for bash 
-        processInfo.Arguments =  Argument;    // The Script name 
-        processInfo.RedirectStandardOutput = true;
-        Process process = Process.Start(processInfo);   // Start that process.
-            
+        languageName = String.IsNullOrEmpty(languageName) ? Path.DirectorySeparatorChar+"repositories" : Path.DirectorySeparatorChar+ languageName;
+        var validatingRepositoriesUrl = Invoke.Command(AgentFileAndPath,Argument,".").TrimEnd('\n','\r');
+
+        if(validatingRepositoriesUrl.Contains("\n")){
+            foreach(var url in validatingRepositoriesUrl){
+            try
+                {   
+                    Invoke.Command("git", $"-C {CacheDir.FullName}{languageName} clone {url}", CacheDir.FullName);    
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e.Message);
+                }
+                RunDetectManfiest(AgentFileAndPath,"detect-manifests", CacheDir.FullName);
+
+        }
+        }else{
+            try
+                {   
+                    Invoke.Command("git", $"-C {CacheDir.FullName}{languageName} clone {validatingRepositoriesUrl}", CacheDir.FullName);    
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e.Message);
+                }
+                // RunDetectManfiest(AgentFileAndPath,"detect-manifests",url, CacheDir.FullName);
+
+        }
         
-            while (!process.StandardOutput.EndOfStream)
-            {
-                string url = process.StandardOutput.ReadLine();
-                
-                var gitRepository = new GitRepository(url,"master", CacheDir, languageName);
-                try
-                {
-                    try
-                    {
-                        gitRepository.CloneOrPull("git");
-                    }
-                    catch(GitException e)
-                    {
-                        Console.Error.WriteLine(e.Message);
-                    }
-                    
-                    RunDetectManfiest(AgentFileAndPath,"detect-manifests",url,gitRepository.Hash, CacheDir.FullName, languageName);
-                }
-                catch (GitException e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                Console.WriteLine(gitRepository.Hash);
-
-
-
-            }
-            process.WaitForExit();
-
-
     }
-
-    public void RunDetectManfiest(string AgentFileAndPath, string Argument,string Url,string hash, string directory, string languageName){
-
-        List<string> DetectedManifestFiles = null;
-        var processInfo = new ProcessStartInfo();
-        processInfo.UseShellExecute = false;
-        processInfo.FileName = AgentFileAndPath;   // 'sh' for bash 
-        processInfo.Arguments =  Argument;    // The Script name 
-        processInfo.RedirectStandardOutput = true;
-        Process process = Process.Start(processInfo);   // Start that process.
-            
+    public void RunDetectManfiest(string AgentFileAndPath, string Argument, string directory){
+        
+        var detectManfiestOutput = Invoke.Command(AgentFileAndPath,Argument,".");
+        Console.WriteLine("Donas RunDetectManfiest:  " + info);
         
         while (!process.StandardOutput.EndOfStream)
         {
@@ -79,14 +63,14 @@ public class AgentsVerifier
         }
         process.WaitForExit();
 
-        if(DetectedManifestFiles.Count < 1)
-        {
-            Console.Error.WriteLine("There are no manifest files found");
-        }
-        else
-        {
-            RunProcessManfiest(AgentFileAndPath, "process-manifests",DetectedManifestFiles,Url);
-        }
+        // if(DetectedManifestFiles.Count < 1)
+        // {
+        //     Console.Error.WriteLine("There are no manifest files found");
+        // }
+        // else
+        // {
+        //     RunProcessManfiest(AgentFileAndPath, "process-manifests",DetectedManifestFiles,Url);
+        // }
 
         // Console.WriteLine("Dona are the files the same? : " + FileCompare(DetectedManifestFiles, directory+Path.DirectorySeparatorChar+languageName+Path.DirectorySeparatorChar+hash+Path.DirectorySeparatorChar+manifestFile.Substring(manifestFile.LastIndexOf(Path.DirectorySeparatorChar)+1)));
 
@@ -125,56 +109,6 @@ public class AgentsVerifier
         }
         process.WaitForExit();
 
-    }
-
-    private bool FileCompare(string file1, string file2)
-    {
-        int file1byte;
-        int file2byte;
-        FileStream fs1;
-        FileStream fs2;
-
-        // Determine if the same file was referenced two times.
-        if (file1 == file2)
-        {
-            // Return true to indicate that the files are the same.
-            return true;
-        }
-
-        // Open the two files.
-        fs1 = new FileStream(file1, FileMode.Open);
-        fs2 = new FileStream(file2, FileMode.Open);
-
-        // Check the file sizes. If they are not the same, the files
-        // are not the same.
-        if (fs1.Length != fs2.Length)
-        {
-            // Close the file
-            fs1.Close();
-            fs2.Close();
-
-            // Return false to indicate files are different
-            return false;
-        }
-
-        // Read and compare a byte from each file until either a
-        // non-matching set of bytes is found or until the end of
-        // file1 is reached.
-        do
-        {
-            // Read one byte from each file.
-            file1byte = fs1.ReadByte();
-            file2byte = fs2.ReadByte();
-        }
-        while ((file1byte == file2byte) && (file1byte != -1));
-
-        // Close the files.
-        fs1.Close();
-        fs2.Close();
-
-        // Return the success of the comparison. "file1byte" is
-        // equal to "file2byte" at this point only if the files are
-        // the same.
-        return ((file1byte - file2byte) == 0);
-    }
+    }        
+        
 }
