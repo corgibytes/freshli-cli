d1d1df32c28d664c634833019ea90033abc17268
M. Scott Ford <scott@corgibytes.com>
2022-08-02 15:44:20 -0400

    bug: fixes error when running tests without building first

--- bin/test.rb
+++ bin/test.rb
@@ -41,7 +41,7 @@ end
 
 status = execute("ruby #{File.dirname(__FILE__)}/build.rb") if perform_build
 
-if status.success?
+if status.nil? || status.success?
   status = execute('bundle check > /dev/null')
   status = execute('bundle install') unless status.success?
 
