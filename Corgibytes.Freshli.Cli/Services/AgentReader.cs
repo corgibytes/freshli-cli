2e277c31d986b835618f8adbc3b7867b1dcefe27
M. Scott Ford <scott@corgibytes.com>
2022-08-04 12:11:23 -0400

    refactor: applies formatting rules

--- Corgibytes.Freshli.Cli/Services/AgentReader.cs
+++ Corgibytes.Freshli.Cli/Services/AgentReader.cs
@@ -15,23 +15,25 @@ public class AgentReader : IAgentReader
         string packageUrlsWithDate;
         try
         {
-            packageUrlsWithDate = Invoke.Command(agentExecutable, $"retrieve-release-history {packageUrl.FormatWithoutVersion()}", ".");
+            packageUrlsWithDate = Invoke.Command(agentExecutable,
+                $"retrieve-release-history {packageUrl.FormatWithoutVersion()}", ".");
         }
         catch (IOException)
         {
             return packages;
         }
 
-        foreach (var packageUrlAndDate in packageUrlsWithDate.TrimEnd('\n','\r').Split("\n"))
+        foreach (var packageUrlAndDate in packageUrlsWithDate.TrimEnd('\n', '\r').Split("\n"))
         {
             var separated = packageUrlAndDate.Split("\t");
 
             packages.Add(
-                new
-                (new(packageUrl.Type, packageUrl.Namespace, packageUrl.Name, separated[0], null, null)
+                new Package(new PackageURL(packageUrl.Type, packageUrl.Namespace, packageUrl.Name, separated[0], null,
+                        null)
                     , DateTimeOffset.ParseExact(separated[1], "yyyy'-'MM'-'dd'T'HH':'mm':'ssK", null)
                 ));
         }
+
         return packages;
     }
 }
