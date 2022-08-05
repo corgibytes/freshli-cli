fb4516ad4ac40baf5af64424228eded1f5d39cec
Edwin Kortman <edwin@corgibytes.com>
2022-08-05 16:08:56 +0200

    refactor: Remove the cloned property
    It was used in one position, might as well make it just an if statement to keep GitSource clean

--- Corgibytes.Freshli.Cli/Functionality/Git/GitSource.cs
+++ Corgibytes.Freshli.Cli/Functionality/Git/GitSource.cs
@@ -86,6 +86,4 @@ public class GitSource
     public DirectoryInfo Directory { get; }
 
     public DirectoryInfo CacheDir { get; }
-
-    public bool Cloned => Directory.GetFiles().Any() || Directory.GetDirectories().Any();
 }
