97fa596140ee8b98516ac7472fecf51ed7a08079
Kimarie Hoot <kimarie@corgibytes.com>
2022-08-05 17:00:01 -0600

    Migrate `cache destroy` to application engine
    Follows pattern from `agents detect` migration
    
    This commit adds DestroyCacheActivity and CacheDestroyedEvent.

--- Corgibytes.Freshli.Cli/CommandRunners/Cache/DestroyCommandRunner.cs
+++ Corgibytes.Freshli.Cli/CommandRunners/Cache/DestroyCommandRunner.cs
@@ -1,46 +1,39 @@
 using System;
 using System.CommandLine.Invocation;
-using System.CommandLine.IO;
 using Corgibytes.Freshli.Cli.CommandOptions;
 using Corgibytes.Freshli.Cli.Commands;
-using Corgibytes.Freshli.Cli.Extensions;
-using Corgibytes.Freshli.Cli.Functionality;
-using Corgibytes.Freshli.Cli.Resources;
+using Corgibytes.Freshli.Cli.Commands.Cache;
+using Corgibytes.Freshli.Cli.Functionality.Engine;
 using Corgibytes.Freshli.Lib;
 
 namespace Corgibytes.Freshli.Cli.CommandRunners.Cache;
 
 public class CacheDestroyCommandRunner : CommandRunner<CacheCommand, CacheDestroyCommandOptions>
 {
-    public CacheDestroyCommandRunner(IServiceProvider serviceProvider, Runner runner)
+    public CacheDestroyCommandRunner(IServiceProvider serviceProvider, Runner runner,
+        IApplicationActivityEngine activityEngine, IApplicationEventEngine eventEngine)
         : base(serviceProvider, runner)
     {
+        ActivityEngine = activityEngine;
+        EventEngine = eventEngine;
     }
+
+    private IApplicationActivityEngine ActivityEngine { get; }
+    private IApplicationEventEngine EventEngine { get; }
 
     public override int Run(CacheDestroyCommandOptions options, InvocationContext context)
     {
-        // Unless the --force flag is passed, prompt the user whether they want to destroy the cache
-        if (!options.Force && !Confirm(
-                string.Format(CliOutput.CacheDestroyCommandRunner_Run_Prompt, options.CacheDir.FullName),
-                context
-            ))
-        {
-            context.Console.Out.WriteLine(CliOutput.CacheDestroyCommandRunner_Run_Abort);
-            return true.ToExitCode();
-        }
+        ActivityEngine.Dispatch(new DestroyCacheActivity(options, context));
 
-        // Destroy the cache
-        context.Console.Out.WriteLine(string.Format(CliOutput.CacheDestroyCommandRunner_Run_Destroying,
-            options.CacheDir));
-        try
+        EventEngine.On<CacheDestroyedEvent>(destroyEvent =>
         {
-            return Functionality.Cache.Destroy(options.CacheDir).ToExitCode();
-        }
-        // Catch errors
-        catch (CacheException error)
-        {
-            context.Console.Error.WriteLine(error.Message);
-            return error.IsWarning.ToExitCode();
-        }
+            // TODO
+            // Should anything happen here after the CacheDestroyedEvent is
+            // received?
+        });
+
+        ActivityEngine.Wait();
+
+        return 0;
     }
 }
