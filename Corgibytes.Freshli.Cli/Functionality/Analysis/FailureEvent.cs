765dfe237dac16176609085cde4fb9f5c086eaee
M. Scott Ford <scott@corgibytes.com>
2022-08-08 16:01:19 -0400

    feat: Adds required attributes to the CacheWasNotPreparedEvent

--- Corgibytes.Freshli.Cli/Functionality/Analysis/FailureEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Analysis/FailureEvent.cs
@@ -2,11 +2,6 @@ using Corgibytes.Freshli.Cli.Functionality.Engine;
 
 namespace Corgibytes.Freshli.Cli.Functionality.Analysis;
 
-public abstract class FailureEvent : IApplicationEvent
+public abstract class FailureEvent : ErrorEvent
 {
-    public string ErrorMessage { get; init; } = null!;
-
-    public virtual void Handle(IApplicationActivityEngine eventClient)
-    {
-    }
 }
