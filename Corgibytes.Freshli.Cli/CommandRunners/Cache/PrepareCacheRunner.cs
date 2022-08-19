d17db4e38322e0ef001a841dfb319db545474135
donabp <dona@corgibytes.com>
2022-08-19 18:17:29 -0400

    prepare cache refactor to CQRS architecture completed

--- Corgibytes.Freshli.Cli/CommandRunners/Cache/PrepareCacheRunner.cs
+++ Corgibytes.Freshli.Cli/CommandRunners/Cache/PrepareCacheRunner.cs
@@ -1,36 +1,34 @@
 using System;
 using System.CommandLine.Invocation;
-using System.CommandLine.IO;
+using Corgibytes.Freshli.Cli.Functionality.Engine;
 using Corgibytes.Freshli.Cli.CommandOptions;
 using Corgibytes.Freshli.Cli.Commands;
-using Corgibytes.Freshli.Cli.Extensions;
 using Corgibytes.Freshli.Cli.Functionality;
-using Corgibytes.Freshli.Cli.Resources;
 using Corgibytes.Freshli.Lib;
 
 namespace Corgibytes.Freshli.Cli.CommandRunners.Cache;
 
 public class CachePrepareCommandRunner : CommandRunner<CacheCommand, CachePrepareCommandOptions>
 {
-    public CachePrepareCommandRunner(IServiceProvider serviceProvider, ICacheManager cacheManager, Runner runner)
-        : base(serviceProvider, runner) =>
+    public CachePrepareCommandRunner(IServiceProvider serviceProvider, ICacheManager cacheManager, Runner runner,
+        IApplicationActivityEngine activityEngine, IApplicationEventEngine eventEngine)
+        : base(serviceProvider, runner)
+    {
         CacheManager = cacheManager;
+        ActivityEngine = activityEngine;
+        EventEngine = eventEngine;
+
+    }
 
     private ICacheManager CacheManager { get; }
+    private IApplicationActivityEngine ActivityEngine { get; }
+    private IApplicationEventEngine EventEngine { get; }
 
     public override int Run(CachePrepareCommandOptions options, InvocationContext context)
     {
-        context.Console.Out.WriteLine(
-            string.Format(CliOutput.CachePrepareCommandRunner_Run_Preparing_cache, options.CacheDir)
-        );
-        try
-        {
-            return CacheManager.Prepare(options.CacheDir).ToExitCode();
-        }
-        catch (CacheException e)
-        {
-            context.Console.Error.WriteLine(e.Message);
-            return false.ToExitCode();
-        }
+        ActivityEngine.Dispatch(new PrepareCacheActivity(options.CacheDir));
+        ActivityEngine.Wait();
+        return 0;
+
     }
 }
