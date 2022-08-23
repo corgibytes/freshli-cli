6d8d2281573d884ebe60a7235d5b62e0bd65c4bc
Kimarie Hoot <kimarie@corgibytes.com>
2022-08-23 15:52:11 -0600

    Adds git clone to analysis process

--- Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisStartedEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisStartedEvent.cs
@@ -1,13 +1,21 @@
 using System;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.Git;
 
 namespace Corgibytes.Freshli.Cli.Functionality.Analysis;
 
 public class AnalysisStartedEvent : IApplicationEvent
 {
+    private readonly IServiceProvider _serviceProvider = null!;
+
     public Guid AnalysisId { get; init; }
+
+    public string CacheDir { get; init; } = null!;
+
+    public string GitPath { get; init; } = null!;
 
     public void Handle(IApplicationActivityEngine eventClient)
     {
+        eventClient.Dispatch(new CloneGitRepositoryActivity(_serviceProvider, AnalysisId, CacheDir, GitPath));
     }
 }
