054b46957ca4c3bc310cded897ee7d6da1eb9177
Edwin Kortman <edwin@corgibytes.com>
2022-08-29 18:26:25 +0200

    Use the CacheDirectory from the CacheDb.cs
    The CacheDb.cs already knows where it lives, so just re-use that instead of saving it with the analysis.

--- Corgibytes.Freshli.Cli/Functionality/ICache.cs
+++ Corgibytes.Freshli.Cli/Functionality/ICache.cs
@@ -5,6 +5,7 @@ namespace Corgibytes.Freshli.Cli.Functionality;
 
 public interface ICacheDb
 {
+    public string CacheDir { get; }
     public Guid SaveAnalysis(CachedAnalysis analysis);
     public CachedAnalysis? RetrieveAnalysis(Guid id);
 }
