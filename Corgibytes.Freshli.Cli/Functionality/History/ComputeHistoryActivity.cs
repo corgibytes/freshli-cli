e460fb2275fc08f8e8bc7f3f3f7dd199d02e476f
Edwin Kortman <edwin@corgibytes.com>
2022-08-29 14:48:07 +0200

    Adds compute history activity
    The activity uses ComputeHistory.cs to determine the intervals it has to stop at, for each interval found it will fire the HistoryIntervalStopFoundEvent.cs

--- Corgibytes.Freshli.Cli/Functionality/History/ComputeHistoryActivity.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/ComputeHistoryActivity.cs
@@ -1,20 +1,45 @@
+using System;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
 using Corgibytes.Freshli.Cli.Functionality.Git;
+using Newtonsoft.Json;
 
 namespace Corgibytes.Freshli.Cli.Functionality.History;
 
 public class ComputeHistoryActivity : IApplicationActivity
 {
-    private readonly IComputeHistory _computeHistoryService;
+    [JsonProperty] private readonly ICacheDb _cacheDb;
+    [JsonProperty] private readonly IComputeHistory _computeHistoryService;
+    [JsonProperty] private readonly Guid _analysisId;
+    [JsonProperty] private readonly string _repositoryId;
 
-    public ComputeHistoryActivity(IComputeHistory computeHistoryService)
+    public ComputeHistoryActivity(ICacheDb cacheDb, IComputeHistory computeHistoryService, Guid analysisId, string repositoryId)
     {
+        _cacheDb = cacheDb;
         _computeHistoryService = computeHistoryService;
+        _analysisId = analysisId;
+        _repositoryId = repositoryId;
     }
 
     public void Handle(IApplicationEventEngine eventClient)
     {
+        var analysis = _cacheDb.RetrieveAnalysis(_analysisId);
+        if (analysis == null)
+        {
+            return;
+        }
 
+        var historyIntervalStops =
+            _computeHistoryService.
+                ComputeWithHistoryInterval(_repositoryId, analysis.GitPath, analysis.HistoryInterval, analysis.CacheDirectory);
+
+        foreach (var historyIntervalStop in historyIntervalStops)
+        {
+            eventClient.Fire(new HistoryIntervalStopFoundEvent()
+            {
+                GitCommitIdentifier = historyIntervalStop.GitCommitIdentifier,
+                RepositoryId = _repositoryId
+            });
+        }
     }
 }
 
