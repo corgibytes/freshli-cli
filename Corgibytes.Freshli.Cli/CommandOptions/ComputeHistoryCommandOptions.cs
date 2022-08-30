a21345955f2844be2bbe012e91e280a838594bb1
Edwin Kortman <edwin@corgibytes.com>
2022-08-30 12:27:32 +0200

    Adds an AnalysisLocation object
    This object is then used by the compute history command runner. The object is the combination of a repository id and the cache directory. Together they can form a path where the repository has been cloned to.

--- Corgibytes.Freshli.Cli/CommandOptions/ComputeHistoryCommandOptions.cs
+++ Corgibytes.Freshli.Cli/CommandOptions/ComputeHistoryCommandOptions.cs
@@ -1,13 +1,11 @@
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 // ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
 
-using Corgibytes.Freshli.Cli.Functionality.Analysis;
-
 namespace Corgibytes.Freshli.Cli.CommandOptions;
 
 public class ComputeHistoryCommandOptions : CommandOptions
 {
-    public IAnalysisLocation AnalysisLocation { get; set; } = null!;
+    public string RepositoryId { get; set; } = null!;
     public string HistoryInterval { get; set; } = null!;
     public string GitPath { get; set; } = null!;
 }
