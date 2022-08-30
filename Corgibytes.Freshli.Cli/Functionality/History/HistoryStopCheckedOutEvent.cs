3e955eadd6e26398592758bffcfde50957f4d21e
M. Scott Ford <scott@corgibytes.com>
2022-08-30 02:28:06 +0000

    Adds CheckoutHistoryActivity class

---
+++ Corgibytes.Freshli.Cli/Functionality/History/HistoryStopCheckedOutEvent.cs
@@ -0,0 +1,13 @@
+using Corgibytes.Freshli.Cli.Functionality.Analysis;
+using Corgibytes.Freshli.Cli.Functionality.Engine;
+
+namespace Corgibytes.Freshli.Cli.Functionality.History;
+
+public class HistoryStopCheckedOutEvent : IApplicationEvent
+{
+    public IAnalysisLocation AnalysisLocation { get; }
+
+    public HistoryStopCheckedOutEvent(IAnalysisLocation analysisLocation) => AnalysisLocation = analysisLocation;
+
+    public void Handle(IApplicationActivityEngine eventClient) => throw new System.NotImplementedException();
+}
