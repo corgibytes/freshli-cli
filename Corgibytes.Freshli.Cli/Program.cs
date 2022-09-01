5e918c7ae02bcc9fbeb1123d5ef51c9a86dd861f
M. Scott Ford <scott@corgibytes.com>
2022-09-01 12:06:39 -0400

    Cleans up result of previous merge
    * A method was defined twice. Those two methods have been correctly merged together
    * The `Main` method has been changed to take `params` to simplify `null` checks in the test
    * Nullability check issues were cleaned up
    * Warnings about async tasks not being waited for were cleaned up
    * Updated calls to System.CommandLine classes to reflect recent breaking changes that were introduced in the library
    
    Co-Authored-By: Kimarie Hoot <52430349+klhoot@users.noreply.github.com>
    Co-Authored-By: Edwin Kortman <7047894+edwinkortman@users.noreply.github.com>

--- Corgibytes.Freshli.Cli/Program.cs
+++ Corgibytes.Freshli.Cli/Program.cs
@@ -22,7 +22,7 @@ public static class Program
     private static readonly Logger s_logger = LogManager.GetCurrentClassLogger();
     private const string DefaultLogLayout = "${date}|${level:uppercase=true:padding=5}|${logger}:${callsite-linenumber}|${message} ${exception}";
 
-    public static async Task<int> Main(string[] args)
+    public static async Task<int> Main(params string[] args)
     {
         var cmdBuilder = CreateCommandLineBuilder();
         return await cmdBuilder.UseDefaults()
@@ -30,7 +30,7 @@ public static class Program
             .InvokeAsync(args);
     }
 
-    private static void ConfigureLogging(string consoleLogLevel, string logfile)
+    private static void ConfigureLogging(string? consoleLogLevel, string? logfile)
     {
         NLog.LogLevel desiredLevel = NLog.LogLevel.FromString(consoleLogLevel);
 
@@ -63,41 +63,47 @@ public static class Program
             })
             .UseNLog()
             .ConfigureServices((_, services) =>
-        {
-            new FreshliServiceBuilder(services).Register();
-        });
+            {
+                new FreshliServiceBuilder(services).Register();
+            });
+
 
     public static CommandLineBuilder CreateCommandLineBuilder()
     {
-        Option logLevelOption = new Option<string>(alias: "--loglevel", description: "the minimum log level to log to console",
+        Option<string> logLevelOption = new Option<string>("--loglevel", description: "the minimum log level to log to console",
             getDefaultValue: () => "Warn");
-        Option logFileOption = new Option<string?>("--logfile", "file for logs instead of logging to console");
+        Option<string> logFileOption = new Option<string>("--logfile", "file for logs instead of logging to console");
+
+        var command = new MainCommand
+        {
+            // Add commands here!
+            new ScanCommand(),
+            new CacheCommand(),
+            new AgentsCommand(),
+            new GitCommand(),
+            new ComputeLibYearCommand()
+        };
+
+        command.AddOption(logLevelOption);
+        command.AddOption(logFileOption);
 
         var builder = new CommandLineBuilder(command)
             .UseHost(CreateHostBuilder)
+            .AddMiddleware(async (context, next) =>
+            {
+                await Task.Run(() => ConfigureLogging(
+                    context.ParseResult.GetValueForOption<string>(logLevelOption),
+                    context.ParseResult.GetValueForOption<string>(logFileOption))
+                );
+
+                await next(context);
+            })
             .AddMiddleware(async (context, next) => { await LogExecution(context, next); })
             .UseExceptionHandler()
-            .UseHelp()
-            .AddOption(logLevelOption)
-            .AddOption(logFileOption);
-        builder.UseMiddleware(context =>
-        {
-            ConfigureLogging(context.ParseResult.ValueForOption<string>(logLevelOption),
-                context.ParseResult.ValueForOption<string>(logFileOption));
+            .UseHelp();
 
-        });
-
-        builder.UseMiddleware(async (context, next) =>
-        {
-            await LogExecution(context, next);
-        });
         return builder;
     }
-
-    private static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders())
-            .ConfigureServices((_, services) => { new FreshliServiceBuilder(services).Register(); });
 
     private static async Task LogExecution(InvocationContext context, Func<InvocationContext, Task> next)
     {
