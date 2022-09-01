95fc49274820794318a75f0656869e8adc4337bd
Edwin Kortman <edwin@corgibytes.com>
2022-09-01 12:24:06 +0200

    Adds the options for the analyze command

--- Corgibytes.Freshli.Cli/CommandOptions/AnalyzeCommandOptions.cs
+++ Corgibytes.Freshli.Cli/CommandOptions/AnalyzeCommandOptions.cs
@@ -1,12 +1,10 @@
-using System.IO;
-
 namespace Corgibytes.Freshli.Cli.CommandOptions;
 
 public class AnalyzeCommandOptions : CommandOptions
 {
     public string GitPath { get; set; } = null!;
-    // public string Branch { get; set; } = null!;
-    // public bool CommitHistory { get; set; }
-    // public string HistoryInterval { get; set; } = null!;
-    // public int? Workers { get; set; } = null!;
+    public string Branch { get; set; } = null!;
+    public bool CommitHistory { get; set; }
+    public string HistoryInterval { get; set; } = null!;
+    public int? Workers { get; set; } = null!;
 }
--- Corgibytes.Freshli.Cli/Commands/AnalyzeCommand.cs
+++ Corgibytes.Freshli.Cli/Commands/AnalyzeCommand.cs
@@ -1,4 +1,6 @@
+using System;
 using System.CommandLine;
+using System.Text.RegularExpressions;
 using Corgibytes.Freshli.Cli.CommandOptions;
 
 namespace Corgibytes.Freshli.Cli.Commands;
@@ -18,5 +20,51 @@ public class AnalyzeCommand : RunnableCommand<AnalyzeCommand, AnalyzeCommandOpti
             Arity = ArgumentArity.ExactlyOne
         };
         AddOption(gitPath);
+
+        var branch = new Option<string>(
+            "--branch",
+            description: "The branch to checkout after cloning the repository. If the option is not specified, then no checkout command will be issued. The remote server’s default branch will be used instead."
+        )
+        {
+            AllowMultipleArgumentsPerToken = false,
+            Arity = ArgumentArity.ExactlyOne
+        };
+        AddOption(branch);
+
+        var commitHistory = new Option<bool>("--commit-history",
+            "Analyzes only the points in time when files have changed in the commit history")
+        {
+            AllowMultipleArgumentsPerToken = false,
+            Arity = ArgumentArity.ZeroOrOne
+        };
+        AddOption(commitHistory);
+
+        var historyInterval = new Option<string>("--history-interval",
+            description:
+            "As the analyze command moves backwards in time through the history of the project, what time interval should it use to determine the points in time that are sampled. It's value should be a number followed by a suffix. Possible suffixes: y(ears), m(onths), w(eeks), d(ays)",
+            getDefaultValue: () => "1m")
+        {
+            AllowMultipleArgumentsPerToken = false,
+            Arity = ArgumentArity.ZeroOrOne
+        };
+
+        historyInterval.AddValidator(optionResult =>
+        {
+            var givenValue = optionResult.GetValueOrDefault<string>();
+            var regex = new Regex("/\\d+[ymwd]?/");
+            if (givenValue != null && !regex.IsMatch(givenValue))
+            {
+                optionResult.ErrorMessage = $"Option {givenValue} not valid. Valid values are a number followed by a suffix. For example: 2m";
+            }
+        });
+        AddOption(historyInterval);
+
+        var workers = new Option<int>("--workers",
+            "The number of worker processes that should be running at any given time. This defaults to twice the number of CPU cores.")
+        {
+            AllowMultipleArgumentsPerToken = false,
+            Arity = ArgumentArity.ZeroOrOne
+        };
+        AddOption(workers);
     }
 }
