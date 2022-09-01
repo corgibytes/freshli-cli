7c658cca143ee57681a1c640f1266ded8df269bb
Kimarie Hoot <kimarie@corgibytes.com>
2022-09-01 23:21:59 +0000

    Dispatches ComputeHistoryActivity from GitRepositoryClonedEvent

--- Corgibytes.Freshli.Cli/Functionality/Git/GitRepositoryClonedEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Git/GitRepositoryClonedEvent.cs
@@ -1,17 +1,27 @@
 using System;
+using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.History;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Corgibytes.Freshli.Cli.Functionality.Git;
 
 public class GitRepositoryClonedEvent : IApplicationEvent
 {
+    public IServiceProvider ServiceProvider = null!;
+
     public string GitRepositoryId { get; init; } = null!;
 
-    // ReSharper disable once UnusedAutoPropertyAccessor.Global
     public Guid AnalysisId { get; init; }
+
+    public string GitPath { get; init; } = null!;
 
     public void Handle(IApplicationActivityEngine eventClient)
     {
-        // TODO - Dispatch(ComputeHistoryActivity)
+        var cacheDb = ServiceProvider.GetRequiredService<ICacheDb>();
+        var computeHistoryService = ServiceProvider.GetRequiredService<IComputeHistory>();
+        var analysisLocation = ServiceProvider.GetRequiredService<IAnalysisLocation>();
+
+        eventClient.Dispatch(new ComputeHistoryActivity(GitPath, cacheDb, computeHistoryService, AnalysisId, analysisLocation));
     }
 }
