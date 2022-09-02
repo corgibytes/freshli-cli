431533601416297329a63c52efe6246a3aec294b
M. Scott Ford <scott@corgibytes.com>
2022-09-02 14:28:43 +0000

    Reduces duplication around console output setup

--- Corgibytes.Freshli.Cli.Test/ProgramTest.cs
+++ Corgibytes.Freshli.Cli.Test/ProgramTest.cs
@@ -10,64 +10,55 @@ namespace Corgibytes.Freshli.Cli.Test;
 
 public class ProgramTest : FreshliTest
 {
+    private StringWriter _consoleOutput = new StringWriter();
+
     public ProgramTest(ITestOutputHelper output) : base(output)
     {
+        Console.SetOut(_consoleOutput);
     }
 
     [Fact]
     public void Validate_Main_loglevel_debug()
     {
-        var stringWriter = new StringWriter();
-        Console.SetOut(stringWriter);
-
         var task = Task.Run(() => Program.Main("--loglevel", "Debug"));
         task.Wait();
 
-        stringWriter.ToString().Should().Contain("DEBUG|Microsoft.Extensions.Hosting.Internal.Host:0|Hosting stopping");
+        _consoleOutput.ToString().Should().Contain("DEBUG|Microsoft.Extensions.Hosting.Internal.Host:0|Hosting stopping");
     }
 
     [Fact]
     public void Validate_Main_loglevel_info()
     {
-        var stringWriter = new StringWriter();
-        Console.SetOut(stringWriter);
-
         var task = Task.Run(() => Program.Main("--loglevel", "Info"));
         task.Wait();
 
-        stringWriter.ToString().Should()
+        _consoleOutput.ToString().Should()
             .NotContain("DEBUG|Microsoft.Extensions.Hosting.Internal.Host:0|Hosting stopping");
-        stringWriter.ToString().Should().Contain("INFO|Microsoft.Hosting.Lifetime:0|Application is shutting down...");
+        _consoleOutput.ToString().Should().Contain("INFO|Microsoft.Hosting.Lifetime:0|Application is shutting down...");
     }
 
     [Fact]
     public void Validate_Main_loglevel_default()
     {
-        var stringWriter = new StringWriter();
-        Console.SetOut(stringWriter);
-
         var task = Task.Run(() => Program.Main());
         task.Wait();
 
-        stringWriter.ToString().Should()
+        _consoleOutput.ToString().Should()
             .NotContain("DEBUG|Microsoft.Extensions.Hosting.Internal.Host:0|Hosting stopping");
-        stringWriter.ToString().Should()
+        _consoleOutput.ToString().Should()
             .NotContain("INFO|Microsoft.Hosting.Lifetime:0|Application is shutting down...");
     }
 
     [Fact]
     public void Validate_Main_logfile()
     {
-        Console.Out.Write(Environment.CurrentDirectory);
         var testfile = "testlog.log";
-        var stringWriter = new StringWriter();
-        Console.SetOut(stringWriter);
 
         var task = Task.Run(() => Program.Main("--loglevel", "Info", "--logfile", testfile));
         task.Wait();
 
         var logFileContent = File.ReadAllText(testfile);
-        stringWriter.ToString().Should()
+        _consoleOutput.ToString().Should()
             .NotContain("INFO|Microsoft.Hosting.Lifetime:0|Application is shutting down...");
         logFileContent.Should().Contain("INFO|Microsoft.Hosting.Lifetime:0|Application is shutting down...");
     }
