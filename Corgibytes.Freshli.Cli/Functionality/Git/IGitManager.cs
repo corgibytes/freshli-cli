031ee33bda578998623790daf6607b9baf3499df
M. Scott Ford <scott@corgibytes.com>
2022-09-05 20:56:59 +0000

    Renames more instances of commitSha to commitId

--- Corgibytes.Freshli.Cli/Functionality/Git/IGitManager.cs
+++ Corgibytes.Freshli.Cli/Functionality/Git/IGitManager.cs
@@ -2,7 +2,7 @@ namespace Corgibytes.Freshli.Cli.Functionality.Git;
 
 public interface IGitManager
 {
-    public GitCommitIdentifier ParseCommitSha(string commitSha);
+    public GitCommitIdentifier ParseCommitId(string commitId);
 
     string CreateArchive(string repositoryId, string cacheDirectory, GitCommitIdentifier gitCommitIdentifier,
         string gitPath);
