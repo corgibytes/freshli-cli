ae4de1b15021b9b0a8c5ed30eaa4c543e68a6f81
Edwin Kortman <edwin@corgibytes.com>
2022-09-05 13:44:43 +0200

    Add the commit identifier to the analysis location
    A bit of information we want later on is the commit identifier. Since it's very likely we want the repository as well, add it to the analysis location. Our big identifier used throughout.

--- Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisLocation.cs
+++ Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisLocation.cs
@@ -1,14 +1,18 @@
+using Corgibytes.Freshli.Cli.Functionality.Git;
+
 namespace Corgibytes.Freshli.Cli.Functionality.Analysis;
 
 public class AnalysisLocation : IAnalysisLocation
 {
     private readonly string _repositoryId;
     private readonly string _cacheDirectory;
+    public readonly GitCommitIdentifier? GitCommitIdentifier;
 
-    public AnalysisLocation(string repositoryId, string cacheDirectory)
+    public AnalysisLocation(string repositoryId, string cacheDirectory, GitCommitIdentifier? gitCommitIdentifier = null)
     {
         _repositoryId = repositoryId;
         _cacheDirectory = cacheDirectory;
+        GitCommitIdentifier = gitCommitIdentifier;
     }
 
     public string Path => _cacheDirectory + "/repositories/" + _repositoryId;
