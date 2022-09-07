d218719ad5d25668dcfd385625c34336a51f0681
Edwin Kortman <edwin@corgibytes.com>
2022-09-07 12:35:10 +0200

    Add the ServiceProvider as public property of the ApplicationEngine
    This will allow us to access the ServiceProvider from an Activity without having to inject it anywhere. Other solution tested was to make it part of the Handle() which works as well but the concern then is if objects become really large when enqueued. By adding it to the application engine we are avoiding all of that.

--- Corgibytes.Freshli.Cli/Functionality/Engine/IApplicationEventEngine.cs
+++ Corgibytes.Freshli.Cli/Functionality/Engine/IApplicationEventEngine.cs
@@ -4,6 +4,8 @@ namespace Corgibytes.Freshli.Cli.Functionality.Engine;
 
 public interface IApplicationEventEngine
 {
+    public IServiceProvider ServiceProvider { get; }
+
     public void Fire(IApplicationEvent applicationEvent);
     public void On<TEvent>(Action<TEvent> eventHandler) where TEvent : IApplicationEvent;
 }
