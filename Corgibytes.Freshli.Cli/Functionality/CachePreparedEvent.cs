c5b1bba084ff88a9f7a5e912e5cf9ffae4c1771b
Edwin Kortman <edwin@corgibytes.com>
2022-09-07 15:58:27 +0200

    Instead of restart analysis when cache is not prepared, restart after cache is prepared
    If PrepareCacheActivity and RestartAnalysisActivity are both dispatched from the same event, the database is not yet initialized when the analysis is already restarting. So, instead let the analysis restart after the cache was prepared.

--- Corgibytes.Freshli.Cli/Functionality/CachePreparedEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/CachePreparedEvent.cs
@@ -1,10 +1,31 @@
+using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Corgibytes.Freshli.Cli.Functionality;
 
 public class CachePreparedEvent : IApplicationEvent
 {
+    public string GitPath { get; init; } = null!;
+    public string CacheDirectory { get; init; } = null!;
+    public string RepositoryUrl { get; init; } = null!;
+    public string? RepositoryBranch { get; init; }
+    public string HistoryInterval { get; init; } = null!;
+    public bool UseCommitHistory { get; init; } = false;
+
     public void Handle(IApplicationActivityEngine eventClient)
     {
+        eventClient.Dispatch(new RestartAnalysisActivity(
+            eventClient.ServiceProvider.GetRequiredService<ICacheManager>(),
+            eventClient.ServiceProvider.GetRequiredService<IHistoryIntervalParser>()
+        )
+        {
+            GitPath = GitPath,
+            CacheDirectory = CacheDirectory,
+            RepositoryUrl = RepositoryUrl,
+            RepositoryBranch = RepositoryBranch,
+            HistoryInterval = HistoryInterval,
+            UseCommitHistory = UseCommitHistory
+        });
     }
 }
