a878f8ae2316118606f1ff8e483549e11b7c6cbf
donabp <dona@corgibytes.com>
2022-09-08 05:42:07 -0400

    preparing the branch for updating it with main

--- Corgibytes.Freshli.Cli/Functionality/History/ComputeHistoryActivity.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/ComputeHistoryActivity.cs
@@ -2,6 +2,7 @@ using System;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
 using Corgibytes.Freshli.Cli.Functionality.Git;
+using Corgibytes.Freshli.Cli.Services;
 using Newtonsoft.Json;
 
 namespace Corgibytes.Freshli.Cli.Functionality.History;
@@ -12,11 +13,13 @@ public class ComputeHistoryActivity : IApplicationActivity
     [JsonProperty] private readonly IAnalysisLocation _analysisLocation;
     [JsonProperty] private readonly ICacheDb _cacheDb;
     [JsonProperty] private readonly IComputeHistory _computeHistoryService;
+    [JsonProperty] private readonly IAgentManager _agentManager;
     private readonly string _gitExecutablePath;
 
-    public ComputeHistoryActivity(string gitExecutablePath, ICacheDb cacheDb, IComputeHistory computeHistoryService,
+    public ComputeHistoryActivity(IAgentManager agentManager, string gitExecutablePath, ICacheDb cacheDb, IComputeHistory computeHistoryService,
         Guid analysisId, IAnalysisLocation analysisLocation)
     {
+        _agentManager = agentManager;
         _gitExecutablePath = gitExecutablePath;
         _cacheDb = cacheDb;
         _computeHistoryService = computeHistoryService;
@@ -26,15 +29,19 @@ public class ComputeHistoryActivity : IApplicationActivity
 
     public void Handle(IApplicationEventEngine eventClient)
     {
+
         var analysis = _cacheDb.RetrieveAnalysis(_analysisId);
         if (analysis == null)
         {
             return;
         }
 
+
+
         var historyIntervalStops =
             _computeHistoryService.ComputeWithHistoryInterval(_analysisLocation, _gitExecutablePath,
                 analysis.HistoryInterval);
+
 
         foreach (var historyIntervalStop in historyIntervalStops)
         {
