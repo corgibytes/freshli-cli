8755957e997fa314c8d6d7323082758bf47500be
Edwin Kortman <edwin@corgibytes.com>
2022-09-09 16:33:46 +0200

    At specified intervals, walk back through the commits and fetch the most recent commit per block

--- Corgibytes.Freshli.Cli.Test/Functionality/Git/ComputeHistoryTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/Git/ComputeHistoryTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Corgibytes.Freshli.Cli.Functionality;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Git;
 using Corgibytes.Freshli.Cli.Test.Common;
@@ -15,13 +16,21 @@ public class ComputeHistoryTest : FreshliTest
 {
     private readonly ComputeHistory _computeHistory;
     private readonly MockListCommits _listCommits;
-    private Mock<IAnalysisLocation> _analysisLocation;
+    private readonly Mock<IAnalysisLocation> _analysisLocation;
 
     public ComputeHistoryTest(ITestOutputHelper output) : base(output)
     {
         _listCommits = new MockListCommits();
-        _computeHistory = new ComputeHistory(_listCommits);
+        _computeHistory = new ComputeHistory(_listCommits, new HistoryIntervalParser());
         _analysisLocation = new Mock<IAnalysisLocation>();
+    }
+
+    [Fact]
+    public void Verify_it_can_deal_with_no_commits()
+    {
+        _listCommits.HasCommitsAvailable(new List<GitCommit>());
+        var expectedStops = new List<HistoryIntervalStop>();
+        Assert.Equivalent(expectedStops, _computeHistory.ComputeWithHistoryInterval(_analysisLocation.Object, "git", "1d"));
     }
 
     [Fact]
@@ -43,17 +52,13 @@ public class ComputeHistoryTest : FreshliTest
         var expectedStops = new List<HistoryIntervalStop>()
         {
             new(
-                "a4792063da2ebb7628b66b9f238cba300b18ab00",
-                new DateTimeOffset(2022, 1, 1, 15, 24, 24, TimeSpan.Zero)
-            ),
-            new(
                 "18e3e90c27f07e734b44bb0bf9c3a8676a11f4df",
                 new DateTimeOffset(2022, 1, 3, 15, 24, 24, TimeSpan.Zero)
             )
         };
 
         Assert.Equivalent(expectedStops,
-            _computeHistory.ComputeWithHistoryInterval(_analysisLocation.Object, "git", "week"));
+            _computeHistory.ComputeWithHistoryInterval(_analysisLocation.Object, "git", "1w"));
     }
 
     [Fact]
@@ -85,6 +90,7 @@ public class ComputeHistoryTest : FreshliTest
     [MethodData(nameof(ExpectedStopsForWeekIntervalForRealWorldScenario))]
     [MethodData(nameof(ExpectedStopsForMonthIntervalForRealWorldScenario))]
     [MethodData(nameof(ExpectedStopsForYearIntervalForRealWorldScenario))]
+    [MethodData(nameof(ExpectedStopsForTwoWeekInterval))]
     public void Verify_it_can_find_sha_identifiers_and_dates_for_interval(List<HistoryIntervalStop> expectedStops, string interval,
         List<GitCommit> availableCommits)
     {
@@ -93,48 +99,64 @@ public class ComputeHistoryTest : FreshliTest
             _computeHistory.ComputeWithHistoryInterval(_analysisLocation.Object, "git", interval));
     }
 
-    private static void CommitsAvailableCrossingYears(MockListCommits listCommits) =>
-        listCommits.HasCommitsAvailable(new List<GitCommit>
+    private static List<GitCommit> CommitsAvailableForTwoWeekInterval() =>
+        new()
         {
-            new("583d813db3e28b9b44a29db352e2f0e1b4c6e420",
-                // 2021/12/19 15:24:24
-                new DateTimeOffset(2021, 12, 19, 15, 24, 24, TimeSpan.Zero)),
-            new("75c7fcc7336ee718050c4a5c8dfb5598622787b2",
-                // 2021/12/20 12:31:34
-                new DateTimeOffset(2021, 12, 20, 12, 31, 34, TimeSpan.Zero)),
-            new("9cd8467fe93714da66bce9056d527d360c6389df",
-                // 2022/01/01 19:26:16
-                new DateTimeOffset(2021, 2, 1, 19, 26, 16, TimeSpan.Zero)),
-            new("a4792063da2ebb7628b66b9f238cba300b18ab00",
-                // 2022/01/01 19:27:42
-                new DateTimeOffset(2022, 1, 1, 19, 27, 42, TimeSpan.Zero)),
-            new("57e5112ae54b7bec8a5294b7cbba2fd9bbd0a75c",
-                // 2022/01/02 10:13:46
-                new DateTimeOffset(2022, 1, 2, 10, 13, 46, TimeSpan.Zero)),
-            new("b2bd95f16a8587dd0bd618ea3415fc8928832c91",
-                // 2022/02/02 13:17:05
-                new DateTimeOffset(2022, 2, 2, 13, 17, 05, TimeSpan.Zero))
-        });
+            new GitCommit("be2640b3a7e098ae7b32c5444502936efc4d0d1e",
+                new DateTimeOffset(2021, 5, 17, 15, 24, 24, TimeSpan.Zero)),
+            new GitCommit("2737b0313671f93e7b18a93614ce1e1a3b097e02",
+                new DateTimeOffset(2021, 5, 23, 01, 50, 59, TimeSpan.Zero)),
+            new GitCommit("9961845aea15e9ba5f057904d1db4bcd80c74f48",
+                new DateTimeOffset(2021, 5, 23, 22, 24, 24, TimeSpan.Zero)),
+            new GitCommit("825a824502e04da744671501958503cf457c128f",
+                new DateTimeOffset(2021, 5, 31, 15, 24, 24, TimeSpan.Zero)),
+            new GitCommit("e212cb02b6df68bfbad94c71228dce944500ea82",
+                new DateTimeOffset(2021, 6, 20, 15, 24, 24, TimeSpan.Zero))
+        };
+
+    private static TheoryData<List<HistoryIntervalStop>, string, List<GitCommit>> ExpectedStopsForTwoWeekInterval() =>
+        new()
+        {
+            {
+                new List<HistoryIntervalStop>
+                {
+                    // Third block is from 23/5 till 9/5
+                    // Youngest commit in this block is on
+                    new("be2640b3a7e098ae7b32c5444502936efc4d0d1e",
+                        new DateTimeOffset(2021, 5, 17, 15, 24, 24, TimeSpan.Zero)),
+                    // Second block is from 6/6 till 23/5
+                    // Youngest commit in this block is on 31/5
+                    new("825a824502e04da744671501958503cf457c128f",
+                        new DateTimeOffset(2021, 5, 31, 15, 24, 24, TimeSpan.Zero)),
+                    // Start point is 20/6, so our first block is from 20/6 till 6/6
+                    // Closest commit is on 20/6
+                    new("e212cb02b6df68bfbad94c71228dce944500ea82",
+                        new DateTimeOffset(2021, 6, 20, 15, 24, 24, TimeSpan.Zero))
+                },
+                "2w",
+                CommitsAvailableForTwoWeekInterval()
+            }
+        };
 
     private static List<GitCommit> CommitsAvailableFromRealWorldScenario() =>
         new()
         {
-            new("583d813db3e28b9b44a29db352e2f0e1b4c6e420",
+            new GitCommit("583d813db3e28b9b44a29db352e2f0e1b4c6e420",
                 // 2021/05/19 15:24:24
                 new DateTimeOffset(2021, 5, 19, 15, 24, 24, TimeSpan.Zero)),
-            new("75c7fcc7336ee718050c4a5c8dfb5598622787b2",
+            new GitCommit("75c7fcc7336ee718050c4a5c8dfb5598622787b2",
                 // 2021/02/20 12:31:34
                 new DateTimeOffset(2021, 2, 20, 12, 31, 34, TimeSpan.Zero)),
-            new("b2bd95f16a8587dd0bd618ea3415fc8928832c91",
+            new GitCommit("b2bd95f16a8587dd0bd618ea3415fc8928832c91",
                 // 2021/02/02 13:17:05
                 new DateTimeOffset(2021, 2, 2, 13, 17, 05, TimeSpan.Zero)),
-            new("57e5112ae54b7bec8a5294b7cbba2fd9bbd0a75c",
+            new GitCommit("57e5112ae54b7bec8a5294b7cbba2fd9bbd0a75c",
                 // 2021/02/02 10:13:46
                 new DateTimeOffset(2021, 2, 2, 10, 13, 46, TimeSpan.Zero)),
-            new("a4792063da2ebb7628b66b9f238cba300b18ab00",
+            new GitCommit("a4792063da2ebb7628b66b9f238cba300b18ab00",
                 // 2021/02/01 19:27:42
                 new DateTimeOffset(2021, 2, 1, 19, 27, 42, TimeSpan.Zero)),
-            new("9cd8467fe93714da66bce9056d527d360c6389df",
+            new GitCommit("9cd8467fe93714da66bce9056d527d360c6389df",
                 // 2021/02/01 19:26:16
                 new DateTimeOffset(2021, 2, 1, 19, 26, 16, TimeSpan.Zero))
         };
@@ -162,7 +184,7 @@ public class ComputeHistoryTest : FreshliTest
                         new DateTimeOffset(2021, 5, 19, 15, 24, 24, TimeSpan.Zero)
                     )
                 },
-                "day",
+                "1d",
                 CommitsAvailableFromRealWorldScenario()
             }
         };
@@ -186,7 +208,7 @@ public class ComputeHistoryTest : FreshliTest
                         new DateTimeOffset(2021, 5, 19, 15, 24, 24, TimeSpan.Zero)
                     )
                 },
-                "week",
+                "1w",
                 CommitsAvailableFromRealWorldScenario()
             }
         };
@@ -206,7 +228,7 @@ public class ComputeHistoryTest : FreshliTest
                         new DateTimeOffset(2021, 5, 19, 15, 24, 24, TimeSpan.Zero)
                     )
                 },
-                "month",
+                "1m",
                 CommitsAvailableFromRealWorldScenario()
             }
         };
@@ -222,7 +244,7 @@ public class ComputeHistoryTest : FreshliTest
                         new DateTimeOffset(2021, 5, 19, 15, 24, 24, TimeSpan.Zero)
                     )
                 },
-                "year",
+                "1y",
                 CommitsAvailableFromRealWorldScenario()
             }
         };
