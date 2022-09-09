1428be2e89fb689602637f8e69a426e05fa176b4
Edwin Kortman <edwin@corgibytes.com>
2022-09-09 16:33:46 +0200

    At specified intervals, walk back through the commits and fetch the most recent commit per block

--- Corgibytes.Freshli.Cli/Functionality/IHistoryIntervalParser.cs
+++ Corgibytes.Freshli.Cli/Functionality/IHistoryIntervalParser.cs
@@ -3,4 +3,6 @@ namespace Corgibytes.Freshli.Cli.Functionality;
 public interface IHistoryIntervalParser
 {
     public bool IsValid(string value);
+
+    public void Parse(string value, out int interval, out string? quantifier);
 }
