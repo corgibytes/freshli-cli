e3c67e6abffe9a13a325c43d0dfa3c9e5ec7da34
Kimarie Hoot <kimarie@corgibytes.com>
2022-09-12 11:37:51 -0600

    Refactors CloneGitRepositoryActivity to pass the AnalysisLocation
    This uses the analysis location to access the cache dir and the repository
    ID instead passing them as separate parameters.

--- Corgibytes.Freshli.Cli/Functionality/Git/GitRepositoryClonedEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Git/GitRepositoryClonedEvent.cs
@@ -7,17 +7,14 @@ namespace Corgibytes.Freshli.Cli.Functionality.Git;
 
 public class GitRepositoryClonedEvent : IApplicationEvent
 {
-    public string GitRepositoryId { get; init; } = null!;
-
     public Guid AnalysisId { get; init; }
 
     public string GitPath { get; init; } = null!;
 
-    public string CacheDir { get; init; } = null!;
+    public AnalysisLocation AnalysisLocation { get; init; } = null!;
 
     public void Handle(IApplicationActivityEngine eventClient)
     {
-        var analysisLocation = new AnalysisLocation(CacheDir, GitRepositoryId);
-        eventClient.Dispatch(new ComputeHistoryActivity(GitPath, CacheDir, AnalysisId, analysisLocation));
+        eventClient.Dispatch(new ComputeHistoryActivity(GitPath, AnalysisId, AnalysisLocation));
     }
 }
