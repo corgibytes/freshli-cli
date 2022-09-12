d4fd48b3bb1ddce27435a1671ae563aec900472e
donabp <dona@corgibytes.com>
2022-09-12 13:17:16 -0400

    changed from object GenerateBillOfMaterialsAcivity inside HistoryIntervalStopFoundEvent.Handle to CheckoutHistoryActivity

--- Corgibytes.Freshli.Cli/Functionality/History/HistoryIntervalStopFoundEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/HistoryIntervalStopFoundEvent.cs
@@ -1,36 +1,26 @@
+using System;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.BillOfMaterials;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.Git;
 using Corgibytes.Freshli.Cli.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Corgibytes.Freshli.Cli.Functionality.History;
 
 public class HistoryIntervalStopFoundEvent : IApplicationEvent
 {
-    private readonly IAgentManager _agentManager;
-    private readonly string _agentPath;
-    private readonly IAnalysisLocation _analysisLocation;
-    private readonly string _manifestPath;
-
-    public HistoryIntervalStopFoundEvent(
-        IAgentManager agentManager, IAnalysisLocation analysisLocation, string manifestPath, string agentPath
-    )
-    {
-        _agentManager = agentManager;
-        _analysisLocation = analysisLocation;
-        _manifestPath = manifestPath;
-        _agentPath = agentPath;
-    }
-
-    public HistoryIntervalStopFoundEvent()
-    {
-
-    }
     public string? GitCommitIdentifier { get; init; }
     public IAnalysisLocation? AnalysisLocation { get; init; }
+    public string? GitExecutablePath { get; init; }
 
     public void Handle(IApplicationActivityEngine eventClient)
     {
-        eventClient.Dispatch(new GenerateBillOfMaterialsActivity(_agentManager, _analysisLocation, _manifestPath, _agentPath));
+
+        if (AnalysisLocation != null)
+        {
+            eventClient.Dispatch(new CheckoutHistoryActivity(eventClient.ServiceProvider.GetRequiredService<IGitManager>(), GitExecutablePath,
+                ));
+        }
     }
 }
