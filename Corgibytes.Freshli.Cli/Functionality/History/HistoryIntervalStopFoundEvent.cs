6dfe00e5784e411e30227c5b851e2e8a2a9c81fe
donabp <dona@corgibytes.com>
2022-09-13 11:26:25 -0400

    fixed lint.rb errors

--- Corgibytes.Freshli.Cli/Functionality/History/HistoryIntervalStopFoundEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/HistoryIntervalStopFoundEvent.cs
@@ -7,15 +7,15 @@ namespace Corgibytes.Freshli.Cli.Functionality.History;
 
 public class HistoryIntervalStopFoundEvent : IApplicationEvent
 {
+
     public string? GitCommitIdentifier { get; init; }
     public IAnalysisLocation? AnalysisLocation { get; init; }
     public string? GitExecutablePath { get; init; }
 
     public void Handle(IApplicationActivityEngine eventClient)
     {
-        if (AnalysisLocation != null && GitExecutablePath != null && AnalysisLocation.CacheDirectory != null &&
-            AnalysisLocation.RepositoryId != null
-            && AnalysisLocation.CommitId != null)
+        if (AnalysisLocation != null && GitExecutablePath != null
+                                     && AnalysisLocation.CommitId != null)
         {
             eventClient.Dispatch(new CheckoutHistoryActivity(
                 eventClient.ServiceProvider.GetRequiredService<IGitManager>(), GitExecutablePath,
