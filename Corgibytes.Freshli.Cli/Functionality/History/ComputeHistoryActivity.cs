074f24e90f381715c8b366ef9ea02c114dca515b
donabp <dona@corgibytes.com>
2022-09-13 12:34:49 -0400

    fixed merge conflict

--- Corgibytes.Freshli.Cli/Functionality/History/ComputeHistoryActivity.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/ComputeHistoryActivity.cs
@@ -50,7 +50,7 @@ public class ComputeHistoryActivity : IApplicationActivity
         {
             eventClient.Fire(new HistoryIntervalStopFoundEvent
             {
-                GitExecutablePath = _gitExecutablePath,
+                GitExecutablePath = GitExecutablePath,
                 GitCommitIdentifier = historyIntervalStop.GitCommitIdentifier,
                 AnalysisLocation = AnalysisLocation
             });
