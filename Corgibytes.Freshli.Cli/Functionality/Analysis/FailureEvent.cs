785aa0049a54e106544de7554c59676462e44099
Edwin Kortman <edwin@corgibytes.com>
2022-09-19 13:54:37 +0200

    Adds FailureEvent
    FailureEvents are ErrorEvents the analysis cannot recover from. e.g. If we can't clone the git repository we are dead in the water.

--- Corgibytes.Freshli.Cli/Functionality/Analysis/FailureEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Analysis/FailureEvent.cs
@@ -1,5 +1,14 @@
+using Corgibytes.Freshli.Cli.Functionality.Engine;
+
 namespace Corgibytes.Freshli.Cli.Functionality.Analysis;
 
+/**
+ *  A failure is any failure we can't recover from during the analysis, blocking the analysis from continuing.
+ */
 public abstract class FailureEvent : ErrorEvent
 {
+    public override void Handle(IApplicationActivityEngine eventClient)
+    {
+        eventClient.Dispatch(new LogAnalysisFailureActivity(this));
+    }
 }
