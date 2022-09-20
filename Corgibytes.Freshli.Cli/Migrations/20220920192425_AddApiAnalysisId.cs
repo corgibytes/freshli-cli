c241754de0022fd1280bc726f9e2af83a15f2bc5
M. Scott Ford <scott@corgibytes.com>
2022-09-20 15:42:14 -0400

    Persists the API analysis id in the cache database

---
+++ Corgibytes.Freshli.Cli/Migrations/20220920192425_AddApiAnalysisId.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Corgibytes.Freshli.Cli.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApiAnalysisId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ApiAnalysisId",
+                table: "CachedAnalyses",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UseCommitHistory",
+                table: "CachedAnalyses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiAnalysisId",
+                table: "CachedAnalyses");
+
+            migrationBuilder.DropColumn(
+                name: "UseCommitHistory",
+                table: "CachedAnalyses");
+        }
+    }
+}
