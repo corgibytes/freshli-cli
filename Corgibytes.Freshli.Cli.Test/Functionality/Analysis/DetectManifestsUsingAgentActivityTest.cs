29331644244c8aaaab2f5bd79f669ae8b6bfd998
Kimarie Hoot <kimarie@corgibytes.com>
2022-09-21 12:59:24 -0600

    Gets AgentReader from IAgentManager via the eventClient.ServiceProvider
    This changes the chain of activities in the analyze command to
    pass the agentExecutablePath instead of serializing/deserializing the
    IAgentReader object.

--- Corgibytes.Freshli.Cli.Test/Functionality/Analysis/DetectManifestsUsingAgentActivityTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/Analysis/DetectManifestsUsingAgentActivityTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
@@ -24,20 +25,28 @@ public class DetectManifestsUsingAgentActivityTest
                 "/path/to/second/manifest"
             });
 
-        var activity = new DetectManifestsUsingAgentActivity(analysisLocation.Object, agentReader.Object);
+        const string agentExecutablePath = "/path/to/agent";
+        var agentManager = new Mock<IAgentManager>();
+        agentManager.Setup(mock => mock.GetReader(agentExecutablePath)).Returns(agentReader.Object);
+
+        var serviceProvider = new Mock<IServiceProvider>();
+        serviceProvider.Setup(mock => mock.GetService(typeof(IAgentManager))).Returns(agentManager.Object);
 
         var eventEngine = new Mock<IApplicationEventEngine>();
+        eventEngine.Setup(mock => mock.ServiceProvider).Returns(serviceProvider.Object);
+
+        var activity = new DetectManifestsUsingAgentActivity(analysisLocation.Object, agentExecutablePath);
 
         activity.Handle(eventEngine.Object);
 
         eventEngine.Verify(mock => mock.Fire(It.Is<ManifestDetectedEvent>(appEvent =>
             appEvent.AnalysisLocation == analysisLocation.Object &&
-            appEvent.AgentReader == agentReader.Object &&
+            appEvent.AgentExecutablePath == agentExecutablePath &&
             appEvent.ManifestPath == "/path/to/first/manifest")));
 
         eventEngine.Verify(mock => mock.Fire(It.Is<ManifestDetectedEvent>(appEvent =>
             appEvent.AnalysisLocation == analysisLocation.Object &&
-            appEvent.AgentReader == agentReader.Object &&
+            appEvent.AgentExecutablePath == agentExecutablePath &&
             appEvent.ManifestPath == "/path/to/second/manifest")));
     }
 }
--- Corgibytes.Freshli.Cli.Test/Functionality/Analysis/ManifestDetectedEventTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/Analysis/ManifestDetectedEventTest.cs
@@ -15,23 +15,19 @@ public class ManifestDetectedEventTest
     public void CorrectlyDispatchesGenerateBillOfMaterialsActivity()
     {
         var manifestPath = "/path/to/manifest";
-        var billOfMaterialsPath = "/path/to/bom";
         var analysisLocation = new AnalysisLocation("/cache/directory", "2dbc2fd2358e1ea1b7a6bc08ea647b9a337ac92d",
             "da39a3ee5e6b4b0d3255bfef95601890afd80709");
 
-        var javaAgentReader = new Mock<IAgentReader>();
-
-        javaAgentReader.Setup(mock => mock.ProcessManifest(manifestPath, It.IsAny<DateTime>())).Returns(billOfMaterialsPath);
-
         var engine = new Mock<IApplicationActivityEngine>();
 
-        var manifestEvent = new ManifestDetectedEvent(analysisLocation, javaAgentReader.Object, manifestPath);
+        const string agentExecutablePath = "/path/to/agent";
+        var manifestEvent = new ManifestDetectedEvent(analysisLocation, agentExecutablePath, manifestPath);
         manifestEvent.Handle(engine.Object);
 
         engine.Verify(mock => mock.Dispatch(It.Is<GenerateBillOfMaterialsActivity>(value =>
             value.AnalysisLocation == analysisLocation &&
             value.ManifestPath == manifestPath &&
-            value.AgentReader == javaAgentReader.Object
+            value.AgentExecutablePath == agentExecutablePath
         )));
     }
 }
