47d0aeca987cdcf3db778c41d1d51d2a2a813b8a
Edwin Kortman <edwin@corgibytes.com>
2022-09-22 17:25:44 +0200

    Adds LatestOnly property to CachedAnalysis

--- Corgibytes.Freshli.Cli/DataModel/CachedAnalysis.cs
+++ Corgibytes.Freshli.Cli/DataModel/CachedAnalysis.cs
@@ -11,12 +11,13 @@ namespace Corgibytes.Freshli.Cli.DataModel;
 public class CachedAnalysis
 {
     public CachedAnalysis(string repositoryUrl, string? repositoryBranch, string historyInterval,
-        CommitHistory useCommitHistory)
+        CommitHistory useCommitHistory, LatestOnly latestOnly)
     {
         RepositoryUrl = repositoryUrl;
         RepositoryBranch = repositoryBranch;
         HistoryInterval = historyInterval;
         UseCommitHistory = useCommitHistory;
+        LatestOnly = latestOnly;
     }
 
     [Required] public Guid Id { get; set; }
@@ -24,6 +25,7 @@ public class CachedAnalysis
 
     public string? RepositoryBranch { get; set; }
     public CommitHistory UseCommitHistory { get; set; }
+    public LatestOnly LatestOnly { get; set; }
 
     // TODO: Research how to use a value class here instead of a string
     [Required] public string HistoryInterval { get; set; }
