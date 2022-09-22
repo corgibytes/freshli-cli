fd68fc3dace348753a89129fe7991117a0dae2bc
M. Scott Ford <scott@corgibytes.com>
2022-09-22 15:34:01 -0400

    Adds diagram-generator CLI program
    This is intended for use by the development team, and will not be shipped as part of the application.
    
    This program generates a Mermaid.js diagram that represents the relationships between the classes that descend from `IApplicationActivity` and `IApplicationEvent`.
    
    An arrow with a solid line indicates that the source type creates an instance of the destination type. An arrow with a dotted line indicates that the source type is a descendant of the destination type.
    
    The goal is to run this program as part of CI to accurately generate accurate documentation about how the different activity and event classes work together.

---
+++ diagram-generator/Program.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+
+MSBuildLocator.RegisterDefaults();
+var workspace = MSBuildWorkspace.Create();
+
+var projectFilePath = "../../../../Corgibytes.Freshli.Cli/Corgibytes.Freshli.Cli.csproj";
+if (!File.Exists(projectFilePath))
+{
+    Console.WriteLine($"Couldn't find {Path.GetFullPath(projectFilePath)}");
+    return -1;
+}
+var project = await workspace.OpenProjectAsync(projectFilePath);
+
+var compilation = await project.GetCompilationAsync();
+
+Console.WriteLine("flowchart TD;");
+
+foreach (var tree in compilation!.SyntaxTrees)
+{
+    var root = await tree.GetRootAsync();
+    var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+    foreach (var classNode in classes)
+    {
+        var classModel = compilation.GetSemanticModel(classNode.SyntaxTree);
+        var classSymbol = classModel.GetDeclaredSymbol(classNode);
+
+        if (classSymbol!.HasInterfaceNamed("IApplicationActivity") || classSymbol!.HasInterfaceNamed("IApplicationEvent"))
+        {
+            List<ITypeSymbol> targetTypeSymbols = new();
+
+            var objectCreations = classNode.DescendantNodes().OfType<ObjectCreationExpressionSyntax>();
+            foreach (var objectCreation in objectCreations)
+            {
+                if (classModel.GetSymbolInfo(objectCreation.Type).Symbol is INamedTypeSymbol objectTypeSymbol)
+                {
+                    if (objectTypeSymbol.HasInterfaceNamed("IApplicationActivity") ||
+                        objectTypeSymbol.HasInterfaceNamed("IApplicationEvent"))
+                    {
+                        targetTypeSymbols.Add(objectTypeSymbol);
+                    }
+                }
+            }
+
+            var implicitObjectCreations = classNode.DescendantNodes().OfType<ImplicitObjectCreationExpressionSyntax>();
+            foreach (var objectCreation in implicitObjectCreations)
+            {
+                var operation = classModel.GetOperation(objectCreation);
+                if (operation!.Type != null)
+                {
+                    if (operation.Type.HasInterfaceNamed("IApplicationActivity") ||
+                        operation.Type.HasInterfaceNamed("IApplicationEvent"))
+                    {
+                        targetTypeSymbols.Add(operation.Type);
+                    }
+                }
+            }
+
+            if (classSymbol!.BaseType != null)
+            {
+                foreach (var typeParameterSymbol in classSymbol.BaseType.TypeArguments)
+                {
+                    if (typeParameterSymbol.HasInterfaceNamed("IApplicationActivity") ||
+                        typeParameterSymbol.HasInterfaceNamed("IApplicationEvent"))
+                    {
+                        targetTypeSymbols.Add(typeParameterSymbol);
+                    }
+                }
+
+                if (classSymbol.BaseType.HasInterfaceNamed("IApplicationActivity") ||
+                    classSymbol.BaseType.HasInterfaceNamed("IApplicationEvent"))
+                {
+                    Console.WriteLine($"    {classSymbol.Name} -.-> {classSymbol.BaseType.Name}");
+                }
+            }
+
+            targetTypeSymbols = targetTypeSymbols.Distinct().ToList();
+
+            foreach (var targetClassSymbol in targetTypeSymbols)
+            {
+                Console.WriteLine($"    {classSymbol.Name} --> {targetClassSymbol.Name}");
+            }
+
+            if (targetTypeSymbols.Count == 0)
+            {
+                Console.WriteLine($"    {classSymbol.Name}");
+            }
+        }
+    }
+}
+
+return 0;
+
+public static class TypeSymbolExtensions
+{
+    public static bool HasInterfaceNamed(this ITypeSymbol symbol, string interfaceName)
+    {
+        var interfaceNames = symbol.AllInterfaces.Select(i => i.Name);
+        return interfaceNames.Contains(interfaceName);
+    }
+}
+
+
+
+
+
