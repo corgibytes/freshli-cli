c08de29fd4b640070d1dccda9d467d7fa1c0a601
Edwin Kortman <edwin@corgibytes.com>
2022-09-22 17:25:26 +0200

    Adds LatestOnly to the CachedAnalysis table

---
+++ Corgibytes.Freshli.Cli/Migrations/20220922144650_AddLatestOnlyOptionToCachedAnalysis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Corgibytes.Freshli.Cli.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLatestOnlyOptionToCachedAnalysis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(name: "LatestOnly", table: "CachedAnalyses", type: "boolean", nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "LatestOnly", table: "CachedAnalyses");
+        }
+    }
+}
