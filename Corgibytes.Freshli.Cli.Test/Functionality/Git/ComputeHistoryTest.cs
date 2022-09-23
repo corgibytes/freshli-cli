52555d6e4b377dd4d5bc442c453a57d09545ffe1
Edwin Kortman <edwin@corgibytes.com>
2022-09-23 13:30:33 +0200

    Fetch the most recent commit
    It returns a list so whatever processing we do on it, we don't have to check if it's a list or not.

--- Corgibytes.Freshli.Cli.Test/Functionality/Git/ComputeHistoryTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/Git/ComputeHistoryTest.cs
@@ -56,6 +56,22 @@ public class ComputeHistoryTest : FreshliTest
         Assert.Equal(expectedStops.Count, actualStops.Count);
     }
 
+    [Fact]
+    public void Verify_it_can_fetch_latest_commit()
+    {
+        _listCommits.HasCommitsAvailable(AvailableCommits());
+        var expectedStops = new List<HistoryIntervalStop>
+        {
+            new ("edd01470c5fb4c5922db060f59bf0e0a5ddce6a5",
+                new DateTimeOffset(2021, 1, 29, 00, 00, 00, TimeSpan.Zero))
+        };
+
+        var actualStops = _computeHistory.ComputeLatestOnly(_analysisLocation.Object, "git").ToList();
+
+        Assert.NotStrictEqual(expectedStops, actualStops);
+        Assert.Equal(expectedStops.Count, actualStops.Count);
+    }
+
     [Theory]
     [MethodData(nameof(DataForTwoWeekInterval))]
     [MethodData(nameof(DataForOneDayInterval))]
--- Corgibytes.Freshli.Cli.Test/Functionality/Git/MockListCommits.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/Git/MockListCommits.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Git;
 
@@ -12,6 +13,12 @@ public class MockListCommits : IListCommits
 
     public IEnumerable<GitCommit> ForRepository(IAnalysisLocation analysisLocation, string gitPath) =>
         _availableCommits;
+
+    public GitCommit MostRecentCommit(IAnalysisLocation analysisLocation, string gitPath)
+    {
+        // Assuming _availableCommits is sorted with most recent first
+        return _availableCommits.First();
+    }
 
     public void HasCommitsAvailable(IEnumerable<GitCommit> availableGitCommits) =>
         _availableCommits = availableGitCommits;
