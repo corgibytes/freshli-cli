580a5356ac47fb011e8c7d5a4b98b2b0b26fc063
Edwin Kortman <edwin@corgibytes.com>
2022-09-23 13:31:19 +0200

    Compute history activity is able to show latest only

--- Corgibytes.Freshli.Cli.Test/Functionality/History/ComputeHistoryActivityTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/History/ComputeHistoryActivityTest.cs
@@ -134,4 +134,58 @@ public class ComputeHistoryActivityTest
             )
         );
     }
+
+    [Fact]
+    public void FiresHistoryIntervalStopFoundEventsForLatestOnly()
+    {
+        // Arrange
+        // Have an analysis available
+        var cachedAnalysis = new CachedAnalysis("https://lorem-ipsum.com", "main", "1m", CommitHistory.Full, LatestOnly.ShowLatestOnly);
+        _cacheDb.Setup(mock => mock.RetrieveAnalysis(It.IsAny<Guid>())).Returns(cachedAnalysis);
+
+        // Have interval stop available
+        var historyIntervalStops = new List<HistoryIntervalStop>
+        {
+            new(
+                "75c7fcc7336ee718050c4a5c8dfb5598622787b2",
+                new DateTimeOffset(2021, 2, 20, 12, 31, 34, TimeSpan.Zero)
+            )
+        };
+        _computeHistory.Setup(mock => mock.ComputeLatestOnly(
+                It.IsAny<IAnalysisLocation>(), It.IsAny<string>())
+            )
+            .Returns(historyIntervalStops);
+
+        var analysisLocation = new Mock<IAnalysisLocation>();
+
+        var serviceProvider = new Mock<IServiceProvider>();
+        _eventEngine.Setup(mock => mock.ServiceProvider).Returns(serviceProvider.Object);
+
+        var cacheManager = new Mock<ICacheManager>();
+        cacheManager.Setup(mock => mock.GetCacheDb(It.IsAny<string>())).Returns(_cacheDb.Object);
+        serviceProvider.Setup(mock => mock.GetService(typeof(ICacheManager))).Returns(cacheManager.Object);
+
+        serviceProvider.Setup(mock => mock.GetService(typeof(IComputeHistory))).Returns(_computeHistory.Object);
+
+        // Act
+        new ComputeHistoryActivity(
+            "git",
+            new Guid("cbc83480-ae47-46de-91df-60747ca8fb09"),
+            analysisLocation.Object
+        ).Handle(_eventEngine.Object);
+
+        // Assert
+        _eventEngine.Verify(
+            mock => mock.Fire(
+                It.Is<HistoryIntervalStopFoundEvent>(
+                    value =>
+                        value.GitExecutablePath == "git" &&
+                        value.AnalysisLocation != null &&
+                        value.AnalysisLocation.CommitId == "75c7fcc7336ee718050c4a5c8dfb5598622787b2"
+                )
+            )
+        );
+    }
+
+
 }
