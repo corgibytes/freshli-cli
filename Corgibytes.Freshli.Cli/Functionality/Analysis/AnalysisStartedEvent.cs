deee94d2ec54fd5ccf8b4a1a8a8bd23728f3bb52
M. Scott Ford <scott@corgibytes.com>
2022-09-24 22:12:23 -0400

    Routes to correct activity

--- Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisStartedEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisStartedEvent.cs
@@ -1,5 +1,6 @@
 using System;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.FreshliWeb;
 using Corgibytes.Freshli.Cli.Functionality.Git;
 
 namespace Corgibytes.Freshli.Cli.Functionality.Analysis;
@@ -13,5 +14,5 @@ public class AnalysisStartedEvent : IApplicationEvent
     public string GitPath { get; init; } = null!;
 
     public void Handle(IApplicationActivityEngine eventClient) =>
-        eventClient.Dispatch(new CloneGitRepositoryActivity(AnalysisId, CacheDir, GitPath));
+        eventClient.Dispatch(new CreateAnalysisApiActivity(AnalysisId, CacheDir, GitPath));
 }
