9452c26968cca98a710b1d4c94e835f47926cbfd
M. Scott Ford <scott@corgibytes.com>
2022-09-25 21:49:54 -0400

    Pulls services from service provider

--- Corgibytes.Freshli.Cli/Functionality/ComputeLibYearActivity.cs
+++ Corgibytes.Freshli.Cli/Functionality/ComputeLibYearActivity.cs
@@ -1,27 +1,26 @@
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
 using Corgibytes.Freshli.Cli.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 namespace Corgibytes.Freshli.Cli.Functionality;
 
 public class ComputeLibYearActivity : IApplicationActivity
 {
-    [JsonProperty] private readonly ICalculateLibYearFromFile _calculateLibYearFromFile;
     public readonly IAnalysisLocation AnalysisLocation;
     public readonly string PathToBoM;
 
-    public ComputeLibYearActivity(ICalculateLibYearFromFile calculateLibYearFromFile, string pathToBoM,
-        IAnalysisLocation analysisLocation)
+    public ComputeLibYearActivity(string pathToBoM, IAnalysisLocation analysisLocation)
     {
-        _calculateLibYearFromFile = calculateLibYearFromFile;
         PathToBoM = pathToBoM;
         AnalysisLocation = analysisLocation;
     }
 
     public void Handle(IApplicationEventEngine eventClient)
     {
-        var libYearPackages = _calculateLibYearFromFile.AsList(PathToBoM);
+        var calculateLibYearFromFile = eventClient.ServiceProvider.GetRequiredService<ICalculateLibYearFromFile>();
+        var libYearPackages = calculateLibYearFromFile.AsList(PathToBoM);
         eventClient.Fire(new LibYearComputedEvent
         {
             LibYearPackages = libYearPackages,
