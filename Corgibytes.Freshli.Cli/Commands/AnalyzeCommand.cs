92617f7e2be11601b47f1b15a093d2d1d1f9bb64
M. Scott Ford <scott@corgibytes.com>
2022-09-26 09:47:29 -0400

    Use IConfiguration for command parameter defaults

--- Corgibytes.Freshli.Cli/Commands/AnalyzeCommand.cs
+++ Corgibytes.Freshli.Cli/Commands/AnalyzeCommand.cs
@@ -6,13 +6,13 @@ namespace Corgibytes.Freshli.Cli.Commands;
 
 public class AnalyzeCommand : RunnableCommand<AnalyzeCommand, AnalyzeCommandOptions>
 {
-    public AnalyzeCommand() : base("analyze",
+    public AnalyzeCommand(IConfiguration configuration) : base("analyze",
         "The primary user-facing command. This command will delegate to other freshli activities to accomplish its work. It will manage work queues to enable parallelization.")
     {
         var gitPath = new Option<string>(
             "--git-path",
             description: "Path to the git binary. Default = 'git'",
-            getDefaultValue: () => "git"
+            getDefaultValue: () => configuration.GitPath
         )
         {
             AllowMultipleArgumentsPerToken = false,
