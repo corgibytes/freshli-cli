0d7696065d24953dc601b36c5b284beff66debb3
Kimarie Hoot <kimarie@corgibytes.com>
2022-09-28 09:38:02 -0600

    Adds database step to query test for history stop interval

--- features/step_definitions/database.rb
+++ features/step_definitions/database.rb
@@ -14,3 +14,10 @@ Then('the {channel} should contain the version of {string}') do |_channel, dll|
   matches = /ProductVersion: (.*)/.match command_output
   expect(last_command_started).to have_output output_string_eq matches[1]
 end
+
+Then('the {string} contains history interval stop at {string} {string}') do |database, commitDate, commitId|
+  database = resolve_path database
+  db = SQLite3::Database.open database
+  r = db.execute("SELECT GitCommitDate, GitCommitId FROM CachedHistoryIntervalStops WHERE GitCommitId='#{commitId}' AND GitCommitDate='#{commitDate}'")
+  expect(r[0]).to match([commitDate, commitId])
+end
