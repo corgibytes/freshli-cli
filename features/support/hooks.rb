68926655fdde8a79792985dfc06ed60b01e785b6
M. Scott Ford <scott@corgibytes.com>
2022-09-28 15:59:07 -0400

    Adds hooks for working with pact
    These are based on the hooks that are used for RSpec as found here: https://github.com/pact-foundation/pact-ruby/blob/a619deb7d6ea72989132f5fdb5aafa80ec63a92f/lib/pact/consumer/rspec.rb

---
+++ features/support/hooks.rb
@@ -0,0 +1,35 @@
+# Based on https://github.com/pact-foundation/pact-ruby/blob/a619deb7d6ea72989132f5fdb5aafa80ec63a92f/lib/pact/consumer/rspec.rb
+
+require 'pact/consumer'
+require 'pact/consumer/spec_hooks'
+require 'pact/rspec'
+require 'pact/helpers'
+
+module Pact
+  module Consumer
+    module Cucumber
+      include Pact::Consumer::ConsumerContractBuilders
+      include Pact::Helpers
+    end
+  end
+end
+
+World(Pact::Consumer::Cucumber)
+
+hooks = Pact::Consumer::SpecHooks.new
+
+BeforeAll do
+  hooks.before_all
+end
+
+Before do |scenario|
+  hooks.before_each scenario.name
+end
+
+After do |scenario|
+  hooks.after_each scenario.name
+end
+
+AfterAll do
+  hooks.after_suite
+end
