d5db0c42999a1f561fdfc79244c293c43791f0a2
Edwin Kortman <edwin@corgibytes.com>
2022-09-29 11:31:22 +0200

    Let AnalysisStartedEvent Dispatch either CloneGitRepositoryActivity or VerifyGitRepositoryInLocalDirectoryActivity
    The latter will test if it's a valid git repository we have or not.

--- Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisStartedEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisStartedEvent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
 using Corgibytes.Freshli.Cli.Functionality.Git;
 
@@ -7,7 +8,17 @@ namespace Corgibytes.Freshli.Cli.Functionality.Analysis;
 public class AnalysisStartedEvent : IApplicationEvent
 {
     public Guid AnalysisId { get; init; }
+    public string? RepositoryUrl { get; init; }
 
-    public void Handle(IApplicationActivityEngine eventClient) =>
-        eventClient.Dispatch(new CloneGitRepositoryActivity(AnalysisId));
+    public void Handle(IApplicationActivityEngine eventClient)
+    {
+        if (Directory.Exists(RepositoryUrl))
+        {
+            eventClient.Dispatch(new VerifyGitRepositoryInLocalDirectoryActivity{AnalysisId = AnalysisId});
+        }
+        else
+        {
+            eventClient.Dispatch(new CloneGitRepositoryActivity(AnalysisId));
+        }
+    }
 }
