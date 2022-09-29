8cc95a03c7a46d1f94f3567d8e4febb094e1a8ea
Edwin Kortman <edwin@corgibytes.com>
2022-09-29 15:50:57 +0200

    Dispatch ComputeHistory after local directory was verified

--- Corgibytes.Freshli.Cli/Functionality/Git/GitRepositoryInLocalDirectoryVerifiedEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/Git/GitRepositoryInLocalDirectoryVerifiedEvent.cs
@@ -1,6 +1,7 @@
 using System;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.History;
 
 namespace Corgibytes.Freshli.Cli.Functionality.Git;
 
@@ -9,5 +10,8 @@ public class GitRepositoryInLocalDirectoryVerifiedEvent : IApplicationEvent
     public Guid AnalysisId { get; init; }
     public AnalysisLocation AnalysisLocation { get; init; }
 
-    public void Handle(IApplicationActivityEngine eventClient) => throw new System.NotImplementedException();
+    public void Handle(IApplicationActivityEngine eventClient)
+    {
+        eventClient.Dispatch(new ComputeHistoryActivity(AnalysisId, AnalysisLocation));
+    }
 }
