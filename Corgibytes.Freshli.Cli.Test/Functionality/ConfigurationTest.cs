4a33b29f4a08d868847d36fa7d2ce7a7aced6bb6
M. Scott Ford <scott@corgibytes.com>
2022-09-29 15:49:25 -0400

    Adds test for Configuration setters

--- Corgibytes.Freshli.Cli.Test/Functionality/ConfigurationTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/ConfigurationTest.cs
@@ -20,5 +20,19 @@ public class ConfigurationTest
     public void DefaultGitPath() => Assert.Equal("git", _configuration.GitPath);
 
     [Fact]
+    public void ModifyingGitPath()
+    {
+        _configuration.GitPath = "/new/git/path";
+        Assert.Equal("/new/git/path", _configuration.GitPath);
+    }
+
+    [Fact]
     public void DefaultCacheDir() => Assert.Equal("/path/to/home/dir/.freshli", _configuration.CacheDir);
+
+    [Fact]
+    public void ModifyingCacheDir()
+    {
+        _configuration.CacheDir = "/new/cache/directory";
+        Assert.Equal("/new/cache/directory", _configuration.CacheDir);
+    }
 }
