fb7cb477e1466a5ae09697654d6e01a9d37cff00
M. Scott Ford <scott@corgibytes.com>
2022-09-30 13:10:37 -0400

    Adds implementation for CreateAnalysis

--- Corgibytes.Freshli.Cli/Functionality/FreshliWeb/ResultsApi.cs
+++ Corgibytes.Freshli.Cli/Functionality/FreshliWeb/ResultsApi.cs
@@ -1,12 +1,46 @@
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Corgibytes.Freshli.Cli.Functionality.FreshliWeb;
 
 public class ResultsApi : IResultsApi
 {
+    private IConfiguration _configuration;
+
+    public ResultsApi(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public string GetResultsUrl(Guid analysisId) =>
         // Not the final implementation
         "https://freshli.app/" + analysisId;
 
-    public Guid CreateAnalysis(string url) => throw new NotImplementedException();
+    public Guid CreateAnalysis(string url)
+    {
+        var client = new HttpClient();
+
+        var response = client.PostAsync(
+            _configuration.FreshliWebApiBaseUrl + "/api/v0/analysis-request",
+            JsonContent.Create(new {
+                name = "Freshli CLI User",
+                email = "info@freshli.io",
+                url = url
+            },
+            new MediaTypeHeaderValue("application/json"))
+        ).Result;
+
+        if (response.StatusCode == HttpStatusCode.Created)
+        {
+            var document = response.Content.ReadFromJsonAsync<JsonNode>().Result;
+            return document!["id"]!.GetValue<Guid>();
+        }
+
+        throw new InvalidOperationException($"Failed to create analysis with url: {url}.");
+    }
 }
