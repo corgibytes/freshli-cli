cec224e388b672bb7dd1f4968938c92315f9fcd3
M. Scott Ford <scott@corgibytes.com>
2022-09-30 15:24:35 -0400

    Adds `Moment` to `IAnalysisLocation`
    Marks the moment in time that the analysis should be completed for.

--- Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisLocation.cs
+++ Corgibytes.Freshli.Cli/Functionality/Analysis/AnalysisLocation.cs
@@ -1,3 +1,4 @@
+using System;
 using Newtonsoft.Json;
 
 namespace Corgibytes.Freshli.Cli.Functionality.Analysis;
@@ -6,15 +7,17 @@ public class AnalysisLocation : IAnalysisLocation
 {
     [JsonProperty] private readonly IConfiguration _configuration;
 
-    public AnalysisLocation(IConfiguration configuration, string repositoryId, string? commitId = null)
+    public AnalysisLocation(IConfiguration configuration, string repositoryId, string? commitId = null, DateTimeOffset? moment = null)
     {
         _configuration = configuration;
         RepositoryId = repositoryId;
         CommitId = commitId;
+        Moment = moment;
     }
 
     public string RepositoryId { get; }
     public string? CommitId { get; }
+    public DateTimeOffset? Moment { get; }
 
     public string Path
     {
