cb8b19edd570f6019b781705e6789daf351fcaad
M. Scott Ford <scott@corgibytes.com>
2022-10-02 15:56:47 -0400

    Adds activity and event for creating a history stop via the api

---
+++ Corgibytes.Freshli.Cli/Functionality/FreshliWeb/CreateApiHistoryStopActivity.cs
@@ -0,0 +1,30 @@
+using System;
+using Corgibytes.Freshli.Cli.Functionality.Analysis;
+using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Corgibytes.Freshli.Cli.Functionality.FreshliWeb;
+
+public class CreateApiHistoryStopActivity : IApplicationActivity
+{
+    public Guid CachedAnalysisId { get; set; }
+    public IHistoryStopData HistoryStopData { get; set; }
+
+    public CreateApiHistoryStopActivity(Guid cachedAnalysisId, IHistoryStopData historyStopData)
+    {
+        CachedAnalysisId = cachedAnalysisId;
+        HistoryStopData = historyStopData;
+    }
+
+    public void Handle(IApplicationEventEngine eventClient)
+    {
+        var resultsApi = eventClient.ServiceProvider.GetRequiredService<IResultsApi>();
+        var cacheManager = eventClient.ServiceProvider.GetRequiredService<ICacheManager>();
+        var cacheDb = cacheManager.GetCacheDb();
+
+        var cachedAnalysis = cacheDb.RetrieveAnalysis(CachedAnalysisId);
+        resultsApi.CreateHistoryPoint(cachedAnalysis!.ApiAnalysisId!.Value, HistoryStopData.Moment!.Value);
+
+        eventClient.Fire(new ApiHistoryStopCreatedEvent(CachedAnalysisId, HistoryStopData));
+    }
+}
