b25d51ae11e38301b48c092e5b694c49558121c0
M. Scott Ford <scott@corgibytes.com>
2022-10-02 16:03:26 -0400

    Dispatches CreateApiHistoryStopActivity when a history stop is found

--- Corgibytes.Freshli.Cli/Functionality/History/HistoryIntervalStopFoundEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/HistoryIntervalStopFoundEvent.cs
@@ -1,14 +1,23 @@
+using System;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.FreshliWeb;
 
 namespace Corgibytes.Freshli.Cli.Functionality.History;
 
 public class HistoryIntervalStopFoundEvent : IApplicationEvent
 {
-    public HistoryIntervalStopFoundEvent(IHistoryStopData historyStopData) => HistoryStopData = historyStopData;
+    public HistoryIntervalStopFoundEvent(Guid cachedAnalysisId, IHistoryStopData historyStopData)
+    {
+        CachedAnalysisId = cachedAnalysisId;
+        HistoryStopData = historyStopData;
+    }
+
+    public Guid CachedAnalysisId { get; set; }
     // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
     public IHistoryStopData HistoryStopData { get; set; }
 
+
     public void Handle(IApplicationActivityEngine eventClient) =>
-        eventClient.Dispatch(new CheckoutHistoryActivity(HistoryStopData));
+        eventClient.Dispatch(new CreateApiHistoryStopActivity(CachedAnalysisId, HistoryStopData));
 }
