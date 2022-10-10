058b19e499475d5040919685bc3e621b85e9b552
M. Scott Ford <scott@corgibytes.com>
2022-10-10 17:01:10 -0400

    Adds serialization tests

---
+++ Corgibytes.Freshli.Cli.Test/Functionality/LibYear/ComputeLibYearForPackageActivitySerializationTest.cs
@@ -0,0 +1,32 @@
+using System;
+using Corgibytes.Freshli.Cli.Functionality;
+using Corgibytes.Freshli.Cli.Functionality.Analysis;
+using Corgibytes.Freshli.Cli.Functionality.LibYear;
+using PackageUrl;
+using Xunit;
+
+namespace Corgibytes.Freshli.Cli.Test.Functionality.LibYear;
+
+[IntegrationTest]
+public class ComputeLibYearForPackageActivitySerializationTest : SerializationTest<ComputeLibYearForPackageActivity>
+{
+    protected override ComputeLibYearForPackageActivity BuildIncoming()
+    {
+        return new ComputeLibYearForPackageActivity()
+        {
+            AgentExecutablePath = "/path/to/agent",
+            AnalysisId = Guid.NewGuid(),
+            HistoryStopData = this.BuildHistoryStopData(),
+            Package = new PackageURL(
+                "pkg:maven/org.apache.xmlgraphics/batik-anim@1.9.1?repository_url=repo.spring.io%2Frelease")
+        };
+    }
+
+    protected override void AssertEqual(ComputeLibYearForPackageActivity incoming, ComputeLibYearForPackageActivity outgoing)
+    {
+        Assert.Equal(incoming.AgentExecutablePath, outgoing.AgentExecutablePath);
+        Assert.Equal(incoming.AnalysisId, outgoing.AnalysisId);
+        this.AssertHistoryStopDataEqual(incoming.HistoryStopData, outgoing.HistoryStopData);
+        Assert.Equal(incoming.Package.ToString(), outgoing.Package.ToString());
+    }
+}
