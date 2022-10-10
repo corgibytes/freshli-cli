8bce8b5ec07524b595a75747f54faa17b6d060f4
Edwin Kortman <edwin@corgibytes.com>
2022-10-10 15:59:04 +0200

    Make CachedPackage a DataModel

--- Corgibytes.Freshli.Cli/DataModel/CachedPackage.cs
+++ Corgibytes.Freshli.Cli/DataModel/CachedPackage.cs
@@ -1,16 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using PackageUrl;
 
 namespace Corgibytes.Freshli.Cli.DataModel;
 
+[Index(nameof(Id), IsUnique = true)]
 public class CachedPackage
 {
-    public CachedPackage(PackageURL packageUrl, DateTimeOffset releasedAt)
-    {
-        PackageUrl = packageUrl;
-        ReleasedAt = releasedAt;
-    }
-
-    public PackageURL PackageUrl { get; }
-    public DateTimeOffset ReleasedAt { get; }
+    [Required] public int Id { get; set; }
+    [Required] public string PackageName { get; set; }
+    [Required] public PackageURL PackageUrl { get; set; }
+    [Required] public DateTimeOffset ReleasedAt { get; set; }
 }
