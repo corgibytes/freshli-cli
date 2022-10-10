d93e42bfb8209ad0761dc11bc7797b49e2504218
Edwin Kortman <edwin@corgibytes.com>
2022-10-10 15:58:36 +0200

    Adds migration for adding cached packages to the database

---
+++ Corgibytes.Freshli.Cli/Migrations/20221010115139_AddCachedPackages.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Corgibytes.Freshli.Cli.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCachedPackages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CachedPackages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PackageName = table.Column<string>(type: "TEXT", nullable: false),
+                    PackageUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    ReleasedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CachedPackages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CachedPackages_Id",
+                table: "CachedPackages",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CachedPackages");
+        }
+    }
+}
