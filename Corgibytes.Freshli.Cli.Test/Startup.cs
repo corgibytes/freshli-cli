e4971b54d499f0bddfa5f8e109fe320865fd52f1
M. Scott Ford <scott@corgibytes.com>
2022-10-11 13:48:40 -0400

    Don't hang on to the configuration and environment instances

--- Corgibytes.Freshli.Cli.Test/Startup.cs
+++ Corgibytes.Freshli.Cli.Test/Startup.cs
@@ -7,12 +7,10 @@ namespace Corgibytes.Freshli.Cli.Test;
 // ReSharper disable once UnusedType.Global
 public class Startup
 {
-    private static IConfiguration Configuration { get; } = new Configuration(new Environment());
-
     // ReSharper disable once UnusedMember.Global
     public void ConfigureHost(IHostBuilder hostBuilder) =>
         hostBuilder.ConfigureServices((_, services) =>
         {
-            new FreshliServiceBuilder(services, Configuration).Register();
+            new FreshliServiceBuilder(services, new Configuration(new Environment())).Register();
         });
 }
