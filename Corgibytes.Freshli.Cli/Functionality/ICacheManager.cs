9e658fbd077156aef18cc5be667f05649c21f72f
Edwin Kortman <edwin@corgibytes.com>
2022-10-24 14:50:04 +0200

    Remove unused method
    It's only being used from the implementation

--- Corgibytes.Freshli.Cli/Functionality/ICacheManager.cs
+++ Corgibytes.Freshli.Cli/Functionality/ICacheManager.cs
@@ -5,7 +5,6 @@ namespace Corgibytes.Freshli.Cli.Functionality;
 public interface ICacheManager
 {
     public bool ValidateCacheDirectory();
-    public bool Prepare();
     public bool Destroy();
     public DirectoryInfo GetDirectoryInCache(string[] directoryStructure);
 
