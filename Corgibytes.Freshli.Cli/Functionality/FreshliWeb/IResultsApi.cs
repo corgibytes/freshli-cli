896071b5562e02736581139d35785f0ffaac13ca
M. Scott Ford <scott@corgibytes.com>
2022-10-26 22:49:53 -0400

    implements api call for updating analysis status
    Co-Authored-By: Edwin Kortman <7047894+edwinkortman@users.noreply.github.com>
    Co-Authored-By: Kimarie Hoot <52430349+klhoot@users.noreply.github.com>

--- Corgibytes.Freshli.Cli/Functionality/FreshliWeb/IResultsApi.cs
+++ Corgibytes.Freshli.Cli/Functionality/FreshliWeb/IResultsApi.cs
@@ -6,9 +6,9 @@ public interface IResultsApi
 {
     public string GetResultsUrl(Guid analysisId);
     public Guid CreateAnalysis(string url);
+    void UpdateAnalysis(Guid apiAnalysisId, string status);
 
     public void CreateHistoryPoint(ICacheDb cacheDb, Guid analysisId, int historyStopPointId);
 
-    void CreatePackageLibYear(ICacheDb cacheDb, Guid analysisId, int packageLibYearId
-    );
+    void CreatePackageLibYear(ICacheDb cacheDb, Guid analysisId, int packageLibYearId);
 }
