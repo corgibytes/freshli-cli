4d62bf672068bc83efa846a8186849cc3f9056c5
M. Scott Ford <scott@corgibytes.com>
2022-11-18 16:36:50 -0500

    Shifts synchronization responsibility
    Makes the activity responsible for ensuring that only one git archive operation is being run at a time for a particular commit id.

--- Corgibytes.Freshli.Cli/Functionality/History/CheckoutHistoryActivity.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/CheckoutHistoryActivity.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Threading;
 using System.Threading.Tasks;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
 using Corgibytes.Freshli.Cli.Functionality.Git;
@@ -6,13 +8,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Corgibytes.Freshli.Cli.Functionality.History;
 
-public class CheckoutHistoryActivity : IApplicationActivity
+public class CheckoutHistoryActivity : IApplicationActivity, ISynchronized
 {
     public CheckoutHistoryActivity(Guid analysisId, int historyStopPointId)
     {
         AnalysisId = analysisId;
         HistoryStopPointId = historyStopPointId;
     }
+
+    private static ConcurrentDictionary<string, SemaphoreSlim> s_semaphores = new();
 
     // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
     // ReSharper disable once MemberCanBePrivate.Global
@@ -43,5 +47,18 @@ public class CheckoutHistoryActivity : IApplicationActivity
             AnalysisId = AnalysisId,
             HistoryStopPointId = HistoryStopPointId
         });
+    }
+
+    public async ValueTask<SemaphoreSlim> GetSemaphore(IServiceProvider serviceProvider)
+    {
+        var cacheManager = serviceProvider.GetRequiredService<ICacheManager>();
+        var cacheDb = cacheManager.GetCacheDb();
+        var historyStopPoint = await cacheDb.RetrieveHistoryStopPoint(HistoryStopPointId);
+        if (historyStopPoint?.GitCommitId == null)
+        {
+            throw new InvalidOperationException("Unable to checkout history when commit id is not provided.");
+        }
+
+        return s_semaphores.GetOrAdd(historyStopPoint.GitCommitId, new SemaphoreSlim(1, 1));
     }
 }
