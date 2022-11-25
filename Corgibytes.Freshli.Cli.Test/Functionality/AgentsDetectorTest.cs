f4886e03741aa14d94727b554be19645bb2a5717
M. Scott Ford <scott@corgibytes.com>
2022-11-24 22:10:31 -0500

    Adds ExecutableFinder
    - On Windows uses PATHEXT environment variable to find executable files
    - On Mac/Linux uses the executable bit to determine if a file is an executable
    - Changes AgentDetector to use the new ExecutableFinder

--- Corgibytes.Freshli.Cli.Test/Functionality/AgentsDetectorTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/AgentsDetectorTest.cs
@@ -10,43 +10,57 @@ namespace Corgibytes.Freshli.Cli.Test.Functionality;
 public class AgentsDetectorTest
 {
     [Fact]
-    public void TestingAgentsDetect()
+    public void TestingAgentsDetectWithMacAndLinuxPaths()
     {
-        var agentsDetector = new AgentsDetector(new MockEnvironment());
+        var executableFinder = new Mock<IExecutableFinder>();
+        executableFinder
+            .Setup(mock => mock.GetExecutables())
+            .Returns(new List<string>
+            {
+                "/usr/bin/freshli-agent-java",
+                "/usr/local/bin/freshli-agent-dotnet",
+                "/home/user/bin/utility"
+            });
+
+        var environment = new Mock<IEnvironment>();
+        environment
+            .Setup(mock => mock.PathSeparator)
+            .Returns("/");
+
+        var agentsDetector = new AgentsDetector(executableFinder.Object, environment.Object);
         var expectedResults = new List<string>
         {
-            "/usr/local/bin/freshli-agent-java",
-            "/usr/local/bin/freshli-agent-javascript",
-            "/usr/local/agents/bin/freshli-agent-csharp",
-            "/home/freshli-user/bin/agents/freshli-agent-ruby"
+            "/usr/bin/freshli-agent-java",
+            "/usr/local/bin/freshli-agent-dotnet"
         };
         var results = agentsDetector.Detect();
         Assert.Equal(expectedResults, results);
     }
 
     [Fact]
-    public void DetectHandlesDuplicateAgentNames()
+    public void TestingAgentsDetectWithWindowsPaths()
     {
-        var environment = new Mock<IEnvironment>();
-
-        environment.Setup(mock => mock.DirectoriesInSearchPath).Returns(
-            new List<string>
+        var executableFinder = new Mock<IExecutableFinder>();
+        executableFinder
+            .Setup(mock => mock.GetExecutables())
+            .Returns(new List<string>
             {
-                "/usr/local/bin",
-                "/bin"
-            }
-        );
+                "C:\\Agents\\freshli-agent-java.bat",
+                "D:\\Agents\\dotnet\\bin\\freshli-agent-dotnet.exe",
+                "E:\\Utilities\\bin\\utility.com"
+            });
 
-        // Intentionally include the same file name in different directories
-        environment.Setup(mock => mock.GetListOfFiles("/bin")).Returns(new List<string?> { "freshli-agent-java" });
-        environment.Setup(mock => mock.GetListOfFiles("/usr/local/bin"))
-            .Returns(new List<string?> { "freshli-agent-java" });
+        var environment = new Mock<IEnvironment>();
+        environment
+            .Setup(mock => mock.PathSeparator)
+            .Returns("\\");
 
-        var agentsDetector = new AgentsDetector(environment.Object);
-
-        // The only `freshli-agent-java` command that should be included in the output is the directory that comes
-        // first in the list of directories in the search path.
-        var expectedResults = new List<string> { "/usr/local/bin/freshli-agent-java" };
+        var agentsDetector = new AgentsDetector(executableFinder.Object, environment.Object);
+        var expectedResults = new List<string>
+        {
+            "C:\\Agents\\freshli-agent-java.bat",
+            "D:\\Agents\\dotnet\\bin\\freshli-agent-dotnet.exe"
+        };
         var results = agentsDetector.Detect();
         Assert.Equal(expectedResults, results);
     }
