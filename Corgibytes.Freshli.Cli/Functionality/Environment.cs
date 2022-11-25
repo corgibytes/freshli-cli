b6d2497ab017b963562ea478a972776af4fbe167
M. Scott Ford <scott@corgibytes.com>
2022-11-24 22:10:31 -0500

    Adds ExecutableFinder
    - On Windows uses PATHEXT environment variable to find executable files
    - On Mac/Linux uses the executable bit to determine if a file is an executable
    - Changes AgentDetector to use the new ExecutableFinder

--- Corgibytes.Freshli.Cli/Functionality/Environment.cs
+++ Corgibytes.Freshli.Cli/Functionality/Environment.cs
@@ -1,29 +1,66 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Corgibytes.Freshli.Cli.Functionality;
 
 public class Environment : IEnvironment
 {
-    public IList<string?> GetListOfFiles(string directory)
+    public string PathSeparator => Path.DirectorySeparatorChar.ToString();
+
+    public IList<string> GetListOfFiles(string directory)
     {
         try
         {
-            var files = Directory.GetFiles(directory).Select(Path.GetFileName).ToList();
+            var files = Directory.GetFiles(directory)
+                .Select(value => Path.GetFileName(value) ?? throw new ArgumentNullException(nameof(value)))
+                .ToList();
             files.Sort();
             return files;
         }
         catch (DirectoryNotFoundException)
         {
-            return new List<string?>();
+            return new List<string>();
         }
     }
 
     public string? GetVariable(string variableName) => System.Environment.GetEnvironmentVariable(variableName);
+    public bool HasExecutableBit(string fileName)
+    {
+        if (IsWindows)
+        {
+            return false;
+        }
+
+#pragma warning disable CA1416
+        var mode = File.GetUnixFileMode(fileName);
+#pragma warning restore CA1416
+        return (
+            mode & (
+                UnixFileMode.UserExecute | UnixFileMode.GroupExecute | UnixFileMode.OtherExecute
+            )) != UnixFileMode.None;
+    }
 
     public IList<string> DirectoriesInSearchPath =>
         System.Environment.GetEnvironmentVariable("PATH")!.Split(Path.PathSeparator).ToList();
 
     public string HomeDirectory => System.Environment.GetEnvironmentVariable("HOME")!;
+
+    public bool IsWindows
+    {
+        get
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        }
+    }
+
+    public IList<string> WindowsExecutableExtensions
+    {
+        get
+        {
+            return GetVariable("PATHEXT")!.Split(Path.PathSeparator.ToString()).ToList();
+        }
+    }
 }
---
+++ Corgibytes.Freshli.Cli/Functionality/IExecutableFinder.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Corgibytes.Freshli.Cli.Functionality;
+
+public interface IExecutableFinder
+{
+    IList<string> GetExecutables();
+}
