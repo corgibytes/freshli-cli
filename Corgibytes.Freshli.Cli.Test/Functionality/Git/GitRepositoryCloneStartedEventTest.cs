4713baf85fb78922929746b1456ad51598c4aafe
M. Scott Ford <scott@corgibytes.com>
2022-11-29 15:36:32 -0500

    Signals IAnalyzeProgressReporter on git clone start

---
+++ Corgibytes.Freshli.Cli.Test/Functionality/Git/GitRepositoryCloneStartedEventTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Corgibytes.Freshli.Cli.Functionality.Analysis;
+using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.Git;
+using Moq;
+using Xunit;
+
+namespace Corgibytes.Freshli.Cli.Test.Functionality.Git;
+
+[UnitTest]
+public class GitRepositoryCloneStartedEventTest
+{
+    [Fact]
+    public async Task HandleSignalsProgressReporter()
+    {
+        var analysisId = Guid.NewGuid();
+        var activityClient = new Mock<IApplicationActivityEngine>();
+        var serviceProvider = new Mock<IServiceProvider>();
+        var progressReporter = new Mock<IAnalyzeProgressReporter>();
+
+        serviceProvider.Setup(mock => mock.GetService(typeof(IAnalyzeProgressReporter)))
+            .Returns(progressReporter.Object);
+        activityClient.Setup(mock => mock.ServiceProvider).Returns(serviceProvider.Object);
+
+        var appEvent = new GitRepositoryCloneStartedEvent { AnalysisId = analysisId };
+        await appEvent.Handle(activityClient.Object);
+
+        progressReporter.Verify(mock => mock.ReportGitOperationStarted(GitOperation.CreateNewClone));
+    }
+}
