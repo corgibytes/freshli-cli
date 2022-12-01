bb5b608ee89e5fc138139a6f7e65293a0905ba43
M. Scott Ford <scott@corgibytes.com>
2022-12-01 09:51:40 -0500

    Reports progress when history stop point is checked out

--- Corgibytes.Freshli.Cli/Functionality/History/HistoryStopCheckedOutEvent.cs
+++ Corgibytes.Freshli.Cli/Functionality/History/HistoryStopCheckedOutEvent.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
 using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Corgibytes.Freshli.Cli.Functionality.History;
 
@@ -10,6 +11,10 @@ public class HistoryStopCheckedOutEvent : IApplicationEvent
     public Guid AnalysisId { get; init; }
     public int HistoryStopPointId { get; init; }
 
-    public async ValueTask Handle(IApplicationActivityEngine eventClient) =>
+    public async ValueTask Handle(IApplicationActivityEngine eventClient)
+    {
+        var progressReporter = eventClient.ServiceProvider.GetRequiredService<IAnalyzeProgressReporter>();
+        progressReporter.ReportSingleHistoryStopPointOperationFinished(HistoryStopPointOperation.Archive);
         await eventClient.Dispatch(new DetectAgentsForDetectManifestsActivity(AnalysisId, HistoryStopPointId));
+    }
 }
