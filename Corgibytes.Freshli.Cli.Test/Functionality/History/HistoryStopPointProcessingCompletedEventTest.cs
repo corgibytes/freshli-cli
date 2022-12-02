800effb3738808b2d53c175e584a073ebccb3a05
M. Scott Ford <scott@corgibytes.com>
2022-12-02 10:49:14 -0500

    Adds `ReportHistoryStopPointProgressActivity`
    This class is responsible for checking to see if the processing for a single history stop point has been completed. If it has, then the progress reporter is signaled.

---
+++ Corgibytes.Freshli.Cli.Test/Functionality/History/HistoryStopPointProcessingCompletedEventTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Corgibytes.Freshli.Cli.Functionality.Analysis;
+using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.History;
+using Json.Schema;
+using Moq;
+using Xunit;
+
+namespace Corgibytes.Freshli.Cli.Test.Functionality.History;
+
+[UnitTest]
+public class HistoryStopPointProcessingCompletedEventTest
+{
+    [Fact]
+    public async Task Handle()
+    {
+        var activityClient = new Mock<IApplicationActivityEngine>();
+        var serviceProvider = new Mock<IServiceProvider>();
+        var progressReporter = new Mock<IAnalyzeProgressReporter>();
+
+        activityClient.Setup(mock => mock.ServiceProvider).Returns(serviceProvider.Object);
+        serviceProvider.Setup(mock => mock.GetService(typeof(IAnalyzeProgressReporter)))
+            .Returns(progressReporter.Object);
+
+        var appEvent = new HistoryStopPointProcessingCompletedEvent { HistoryStopPointId = 12 };
+
+        await appEvent.Handle(activityClient.Object);
+
+        progressReporter.Verify(mock =>
+            mock.ReportSingleHistoryStopPointOperationFinished(HistoryStopPointOperation.Process));
+    }
+
+}
---
+++ Corgibytes.Freshli.Cli.Test/Functionality/History/ReportHistoryStopPointProgressActivityTest.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Corgibytes.Freshli.Cli.Functionality.Engine;
+using Corgibytes.Freshli.Cli.Functionality.History;
+using Moq;
+using Xunit;
+
+namespace Corgibytes.Freshli.Cli.Test.Functionality.History;
+
+[UnitTest]
+public class ReportHistoryStopPointProgressActivityTest
+{
+    [Fact]
+    public async Task HandleWhenOperationIsCompleted()
+    {
+        var eventClient = new Mock<IApplicationEventEngine>();
+
+        eventClient.Setup(mock => mock.AreOperationsPending(It.IsAny<Func<IHistoryStopPointProcessingTask, bool>>()))
+            .ReturnsAsync(false);
+
+        var activity = new ReportHistoryStopPointProgressActivity { HistoryStopPointId = 12 };
+
+        await activity.Handle(eventClient.Object);
+
+        eventClient.Verify(mock => mock.Fire(It.Is<HistoryStopPointProcessingCompletedEvent>(value =>
+            value.HistoryStopPointId == activity.HistoryStopPointId)));
+    }
+
+    [Fact]
+    public async Task HandleWhenOperationIsStillInProgress()
+    {
+        var eventClient = new Mock<IApplicationEventEngine>();
+
+        eventClient.Setup(mock => mock.AreOperationsPending(It.IsAny<Func<IHistoryStopPointProcessingTask, bool>>()))
+            .ReturnsAsync(true);
+
+        var activity = new ReportHistoryStopPointProgressActivity { HistoryStopPointId = 12 };
+
+        await activity.Handle(eventClient.Object);
+
+        eventClient.Verify(mock => mock.Fire(It.Is<HistoryStopPointProcessingCompletedEvent>(value =>
+            value.HistoryStopPointId == activity.HistoryStopPointId)), Times.Never);
+    }
+}
