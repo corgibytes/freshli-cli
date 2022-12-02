a9263cab1304c045b89108a478d77cc644176c1e
M. Scott Ford <scott@corgibytes.com>
2022-12-02 10:57:07 -0500

    Uses a priority queue to track tasks
    This allows for querying to determine if there is pending work that meets a certain set of criterial, such as belonging to a history stop point.

--- Corgibytes.Freshli.Cli/Functionality/Engine/DefaultBackgroundTaskQueue.cs
+++ Corgibytes.Freshli.Cli/Functionality/Engine/DefaultBackgroundTaskQueue.cs
@@ -1,44 +1,62 @@
 // Derived from https://learn.microsoft.com/en-us/dotnet/core/extensions/queue-service
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Corgibytes.Freshli.Cli.Functionality.Engine;
 
 public sealed class DefaultBackgroundTaskQueue : IBackgroundTaskQueue
 {
-    private readonly Channel<Func<CancellationToken, ValueTask>> _queue;
+    private readonly SemaphoreSlim _queueReaderSemaphore = new(0);
+    private readonly SemaphoreSlim _queueWriterSemaphore = new(1);
+    private readonly PriorityQueue<WorkItem, int> _queue = new();
+    private readonly SemaphoreSlim _pendingWorkItemsSemaphore = new(1);
+    private readonly List<WorkItem> _pendingWorkItems = new();
 
-    private readonly object _statisticsLock = new();
+    private readonly SemaphoreSlim _statisticsSemaphore = new(1);
     private QueueStatistics _statistics;
 
-    public DefaultBackgroundTaskQueue()
+    public async ValueTask QueueBackgroundWorkItemAsync(WorkItem workItem, CancellationToken cancellationToken = default)
     {
-        _queue = Channel.CreateUnbounded<Func<CancellationToken, ValueTask>>();
-    }
+        await IncrementEnqueuedCount(cancellationToken);
 
-    public async ValueTask QueueBackgroundWorkItemAsync(
-        Func<CancellationToken, ValueTask> workItem)
-    {
-        if (workItem is null)
+        await AddToPendingWorkItems(workItem, cancellationToken);
+
+        await WithSemaphoreLock(() =>
         {
-            throw new ArgumentNullException(nameof(workItem));
-        }
+            _queue.Enqueue(WrapWorkItem(workItem), workItem.ApplicationTask.Priority);
+        }, _queueWriterSemaphore, cancellationToken);
 
-        IncrementEnqueuedCount();
-        await _queue.Writer.WriteAsync(WrapWorkItem(workItem));
+        _queueReaderSemaphore.Release();
     }
 
-    private Func<CancellationToken, ValueTask> WrapWorkItem(Func<CancellationToken, ValueTask> workItem)
+    private async ValueTask AddToPendingWorkItems(WorkItem workItem, CancellationToken cancellationToken = default)
     {
-        return async cancellationToken =>
+        await WithSemaphoreLock(() =>
+        {
+            _pendingWorkItems.Add(workItem);
+        }, _pendingWorkItemsSemaphore, cancellationToken);
+    }
+
+    private async ValueTask RemoveFromPendingWorkItems(WorkItem workItem, CancellationToken cancellationToken = default)
+    {
+        await WithSemaphoreLock(() =>
+        {
+            _pendingWorkItems.Remove(workItem);
+        }, _pendingWorkItemsSemaphore, cancellationToken);
+    }
+
+    private WorkItem WrapWorkItem(WorkItem workItem)
+    {
+        return new WorkItem(workItem.ApplicationTask, async cancellationToken =>
         {
             try
             {
-                await workItem(cancellationToken);
-                IncrementSucceededCount();
+                await workItem.Invoker(cancellationToken);
+                await IncrementSucceededCount(cancellationToken);
             }
             catch (OperationCanceledException)
             {
@@ -47,81 +65,116 @@ public sealed class DefaultBackgroundTaskQueue : IBackgroundTaskQueue
             }
             catch (Exception)
             {
-                IncrementFailedCount();
+                await IncrementFailedCount(cancellationToken);
                 throw;
             }
             finally
             {
-                DecrementProcessingCount();
+                await RemoveFromPendingWorkItems(workItem, cancellationToken);
+                await DecrementProcessingCount(cancellationToken);
             }
-        };
+        });
     }
 
-    public async ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(
-        CancellationToken cancellationToken)
+    public async ValueTask<WorkItem> DequeueAsync(CancellationToken cancellationToken)
     {
-        var workItem = await _queue.Reader.ReadAsync(cancellationToken);
+        await _queueReaderSemaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+        var dequeueResult = _queue.TryDequeue(out var workItem, out var _);
+
+        if (!dequeueResult)
+        {
+            throw new InvalidOperationException("Failed to retrieve an item from the queue");
+        }
 
         // this assumes that a work item is going to start processing after it has been removed from the queue
-        IncrementProcessingCount();
-        DecrementEnqueuedCount();
+        await IncrementProcessingCount(cancellationToken);
+        await DecrementEnqueuedCount(cancellationToken);
 
         return workItem;
     }
 
     public QueueStatistics GetStatistics()
     {
-        lock (_statisticsLock)
+        return _statistics;
+    }
+
+    public async ValueTask<bool> ContainsUnprocessedWork<T>(Func<T, bool> query, CancellationToken cancellationToken = default)
+    {
+        var result = true;
+
+        await WithSemaphoreLock(() =>
         {
-            return _statistics;
+            result = _pendingWorkItems.Any(item => item.ApplicationTask is T task && query(task));
+        }, _pendingWorkItemsSemaphore, cancellationToken);
+
+        return result;
+    }
+
+    private async ValueTask IncrementEnqueuedCount(CancellationToken cancellationToken = default)
+    {
+        await WithStatisticsLock(() =>
+        {
+            _statistics = _statistics with {Enqueued = _statistics.Enqueued + 1};
+        }, cancellationToken);
+    }
+
+    private async ValueTask DecrementEnqueuedCount(CancellationToken cancellationToken = default)
+    {
+        await WithStatisticsLock(() =>
+        {
+            _statistics = _statistics with {Enqueued = _statistics.Enqueued - 1};
+        }, cancellationToken);
+    }
+
+    private async ValueTask IncrementProcessingCount(CancellationToken cancellationToken = default)
+    {
+        await WithStatisticsLock(() =>
+        {
+            _statistics = _statistics with {Processing = _statistics.Processing + 1};
+        }, cancellationToken);
+    }
+
+    private async ValueTask DecrementProcessingCount(CancellationToken cancellationToken = default)
+    {
+        await WithStatisticsLock(() =>
+        {
+            _statistics = _statistics with {Processing = _statistics.Processing - 1};
+        }, cancellationToken);
+    }
+
+    private async ValueTask IncrementFailedCount(CancellationToken cancellationToken = default)
+    {
+        await WithStatisticsLock(() =>
+        {
+            _statistics = _statistics with {Failed = _statistics.Failed + 1};
+        }, cancellationToken);
+    }
+
+    private async ValueTask IncrementSucceededCount(CancellationToken cancellationToken = default)
+    {
+        await WithStatisticsLock(() =>
+        {
+            _statistics = _statistics with {Succeeded = _statistics.Succeeded + 1};
+        }, cancellationToken);
+    }
+
+    private async ValueTask WithStatisticsLock(Action operation, CancellationToken cancellationToken = default)
+    {
+        await WithSemaphoreLock(operation, _statisticsSemaphore, cancellationToken);
+    }
+
+    private async ValueTask WithSemaphoreLock(Action operation, SemaphoreSlim semaphore,
+        CancellationToken cancellationToken = default)
+    {
+        await semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+        try
+        {
+            operation();
+        }
+        finally
+        {
+            semaphore.Release();
         }
     }
 
-    private void IncrementEnqueuedCount()
-    {
-        lock (_statisticsLock)
-        {
-            _statistics = _statistics with { Enqueued = _statistics.Enqueued + 1 };
-        }
-    }
-
-    private void DecrementEnqueuedCount()
-    {
-        lock (_statisticsLock)
-        {
-            _statistics = _statistics with { Enqueued = _statistics.Enqueued - 1 };
-        }
-    }
-
-    private void IncrementProcessingCount()
-    {
-        lock (_statisticsLock)
-        {
-            _statistics = _statistics with { Processing = _statistics.Processing + 1 };
-        }
-    }
-
-    private void DecrementProcessingCount()
-    {
-        lock (_statisticsLock)
-        {
-            _statistics = _statistics with { Processing = _statistics.Processing - 1 };
-        }
-    }
-
-    private void IncrementFailedCount()
-    {
-        lock (_statisticsLock)
-        {
-            _statistics = _statistics with { Failed = _statistics.Failed + 1 };
-        }
-    }
-
-    private void IncrementSucceededCount()
-    {
-        lock (_statisticsLock)
-        {
-            _statistics = _statistics with { Succeeded = _statistics.Succeeded + 1 };
-        }
-    }
 }
