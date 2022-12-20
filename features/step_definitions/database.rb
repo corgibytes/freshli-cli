b4ca49e3a55063f75547f6565be3e5c27db082ef
M. Scott Ford <scott@corgibytes.com>
2022-12-20 11:45:24 -0500

    Ensures that sqlite3 database is closed after use

--- features/step_definitions/database.rb
+++ features/step_definitions/database.rb
@@ -4,9 +4,10 @@ require 'sqlite3'
 
 Then('we can open a SQLite connection to {string}') do |database|
   database = resolve_path database
-  db = SQLite3::Database.open database
-  r = db.execute('PRAGMA integrity_check')
-  expect(r[0]).to match(['ok'])
+  SQLite3::Database.new(database, readonly: true) do |db|
+    r = db.execute('PRAGMA integrity_check')
+    expect(r[0]).to match(['ok'])
+  end
 end
 
 Then('the {channel} should contain the version of {string}') do |_channel, dll|
@@ -16,27 +17,29 @@ end
 
 Then('the {string} contains history stop point at {string} {string}') do |database, commit_date, commit_id|
   database = resolve_path database
-  db = SQLite3::Database.open database
-  r = db.execute(<<-SQL)
-    SELECT AsOfDateTime, GitCommitId FROM CachedHistoryStopPoints
-    WHERE GitCommitId='#{commit_id}' AND AsOfDateTime LIKE '#{commit_date}%'
-  SQL
-  expect(r[0][0]).to match(/#{commit_date}/)
-  expect(r[0][1]).to match(commit_id)
+  SQLite3::Database.new(database, readonly: true) do |db|
+    r = db.execute(<<-SQL)
+      SELECT AsOfDateTime, GitCommitId FROM CachedHistoryStopPoints
+      WHERE GitCommitId='#{commit_id}' AND AsOfDateTime LIKE '#{commit_date}%'
+    SQL
+    expect(r[0][0]).to match(/#{commit_date}/)
+    expect(r[0][1]).to match(commit_id)
+  end
 end
 
 Then('the {string} contains lib year {double} for {string} as of {string}') do
   |database, lib_year, package, as_of_date_time|
 
   database = resolve_path database
-  db = SQLite3::Database.open database
-  r = db.execute(<<-SQL)
-    SELECT CachedPackageLibYears.PackageName, CachedPackageLibYears.LibYear, CachedHistoryStopPoints.AsOfDateTime
-    FROM CachedHistoryStopPoints
-    INNER JOIN CachedPackageLibYears ON CachedHistoryStopPoints.ID=CachedPackageLibYears.HistoryStopPointId
-    WHERE CachedHistoryStopPoints.AsOfDateTime LIKE '#{as_of_date_time}%' AND CachedPackageLibYears.PackageName='#{package}'
-  SQL
-  expect(r[0][0]).to match(/#{package}/)
-  expect(r[0][1]).to eq(lib_year)
-  expect(r[0][2]).to match(as_of_date_time.to_s)
+  SQLite3::Database.new(database, readonly: true) do |db|
+    r = db.execute(<<-SQL)
+      SELECT CachedPackageLibYears.PackageName, CachedPackageLibYears.LibYear, CachedHistoryStopPoints.AsOfDateTime
+      FROM CachedHistoryStopPoints
+      INNER JOIN CachedPackageLibYears ON CachedHistoryStopPoints.ID=CachedPackageLibYears.HistoryStopPointId
+      WHERE CachedHistoryStopPoints.AsOfDateTime LIKE '#{as_of_date_time}%' AND CachedPackageLibYears.PackageName='#{package}'
+    SQL
+    expect(r[0][0]).to match(/#{package}/)
+    expect(r[0][1]).to eq(lib_year)
+    expect(r[0][2]).to match(as_of_date_time.to_s)
+  end
 end
