c95d2601791302c88a8ee1a630308c3f095ef011
M. Scott Ford <scott@corgibytes.com>
2023-01-17 11:51:21 -0500

    Implements retry logic for Freshli Web API calls
    * Uses retry policy when interacting with `HttpClient`
    * Uses a singleton `HttpClient` instance, because this class is not meant to be short lived

--- Corgibytes.Freshli.Cli/Functionality/FreshliWeb/ResultsApi.cs
+++ Corgibytes.Freshli.Cli/Functionality/FreshliWeb/ResultsApi.cs
@@ -5,60 +5,119 @@ using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using Polly;
 
 namespace Corgibytes.Freshli.Cli.Functionality.FreshliWeb;
 
-public class ResultsApi : IResultsApi
+public class ResultsApi : IResultsApi, IDisposable
 {
     private readonly IConfiguration _configuration;
+    private readonly HttpClient _client;
 
-    public ResultsApi(IConfiguration configuration) => _configuration = configuration;
+    public ResultsApi(IConfiguration configuration, HttpClient client)
+    {
+        _configuration = configuration;
+        _client = client;
+    }
 
     // TODO: the results URL should use the base URL from the configuration
     public string GetResultsUrl(Guid analysisId) => "https://freshli.io/AnalysisRequests/" + analysisId;
 
-    public async ValueTask<Guid> CreateAnalysis(string url)
+    private class UnexpectedStatusCode : Exception
     {
-        var client = new HttpClient();
-
-        var response = await client.PostAsync(
-            _configuration.FreshliWebApiBaseUrl + "/api/v0/analysis-request",
-            JsonContent.Create(
-                new
-                {
-                    name = "Freshli CLI User",
-                    email = "info@freshli.io",
-                    url
-                },
-                new MediaTypeHeaderValue("application/json")
-            )
-        );
-
-        if (response.StatusCode == HttpStatusCode.Created)
+        public UnexpectedStatusCode(HttpStatusCode expected, HttpStatusCode actual): base(BuildMessage(expected, actual))
         {
-            var document = await response.Content.ReadFromJsonAsync<JsonNode>();
-            return document!["id"]!.GetValue<Guid>();
         }
 
-        throw new InvalidOperationException($"Failed to create analysis with url: {url}.");
+        private static string BuildMessage(HttpStatusCode expected, HttpStatusCode actual)
+        {
+            return $"Expected status code {expected} but got {actual}";
+        }
+    }
+
+    private async ValueTask<T> ApiSendAsync<T>(HttpMethod method, string url, JsonContent? content,
+        HttpStatusCode expectedStatusCode, Func<HttpResponseMessage, Task<T>>? responseProcessor = null)
+    {
+        var uri = string.IsNullOrEmpty(url) ? null : new Uri(url, UriKind.RelativeOrAbsolute);
+        var request = new HttpRequestMessage(method, uri);
+        request.Content = content;
+
+        var response = await Policy
+            .Handle<HttpRequestException>()
+            .Or<TimeoutException>()
+            .WaitAndRetryAsync(6, retryAttempt =>
+                TimeSpan.FromMilliseconds(Math.Pow(10, retryAttempt / 2.0))
+            )
+            .ExecuteAsync(async () =>
+                // TODO: pass in a cancellation token
+                await _client.SendAsync(request));
+
+        if (response.StatusCode != expectedStatusCode)
+        {
+            throw new UnexpectedStatusCode(expectedStatusCode, response.StatusCode);
+        }
+
+        return responseProcessor != null ? await responseProcessor(response) : default!;
+    }
+
+    private async ValueTask<T> ApiSendAsync<T>(HttpMethod method, string url, JsonContent body,
+        HttpStatusCode expectedStatusCode,
+        Func<HttpResponseMessage, T>? responseProcessor = null)
+    {
+        return await ApiSendAsync(method, url, body, expectedStatusCode, (response) =>
+        {
+            var processorResult = responseProcessor != null ? responseProcessor(response) : default!;
+            return Task.FromResult(processorResult);
+        });
+    }
+
+    private async ValueTask ApiSendAsync(HttpMethod method, string url, JsonContent body, HttpStatusCode expectedStatusCode)
+    {
+        await ApiSendAsync(method, url, body, expectedStatusCode, _ => true);
+    }
+
+    public async ValueTask<Guid> CreateAnalysis(string url)
+    {
+        var apiUrl = _configuration.FreshliWebApiBaseUrl + "/api/v0/analysis-request";
+        var requestBody = JsonContent.Create(new
+        {
+            name = "Freshli CLI User",
+            email = "info@freshli.io",
+            url
+        }, new MediaTypeHeaderValue("application/json"));
+
+        try
+        {
+            return await ApiSendAsync(HttpMethod.Post, apiUrl, requestBody, HttpStatusCode.Created, async (response) =>
+            {
+                var document = await response.Content.ReadFromJsonAsync<JsonNode>();
+                return document!["id"]!.GetValue<Guid>();
+            });
+        }
+        catch (Exception error)
+        {
+            throw new InvalidOperationException($"Failed to create analysis with url: {url}.", error);
+        }
     }
 
     public async ValueTask UpdateAnalysis(Guid apiAnalysisId, string status)
     {
-        var client = new HttpClient();
-
-        var response = await client.PutAsync(
-            _configuration.FreshliWebApiBaseUrl + "/api/v0/analysis-request/" + apiAnalysisId,
-            JsonContent.Create(
-                new { state = status },
-                new MediaTypeHeaderValue("application/json")
-            )
+        var apiUrl = _configuration.FreshliWebApiBaseUrl + "/api/v0/analysis-request/" + apiAnalysisId;
+        var requestBody = JsonContent.Create(
+            new { state = status },
+            new MediaTypeHeaderValue("application/json")
         );
 
-        if (response.StatusCode != HttpStatusCode.OK)
+        try
+        {
+            await ApiSendAsync(HttpMethod.Put, apiUrl, requestBody, HttpStatusCode.OK);
+        }
+        catch (Exception error)
         {
             throw new InvalidOperationException(
-                $"Failed to update analysis '{apiAnalysisId}' with state = '{status}'.");
+                $"Failed to update analysis '{apiAnalysisId}' with state = '{status}'.",
+                error
+            );
         }
     }
 
@@ -70,20 +129,22 @@ public class ResultsApi : IResultsApi
         var historyStopPoint = await cacheDb.RetrieveHistoryStopPoint(historyStopPointId);
         var asOfDateTime = historyStopPoint!.AsOfDateTime;
 
-        var client = new HttpClient();
-
-        var response = await client.PostAsync(
-            _configuration.FreshliWebApiBaseUrl + "/api/v0/analysis-request/" + apiAnalysisId,
-            JsonContent.Create(
-                new { date = asOfDateTime.ToString("o") },
-                new MediaTypeHeaderValue("application/json")
-            )
+        var apiUrl = _configuration.FreshliWebApiBaseUrl + "/api/v0/analysis-request/" + apiAnalysisId;
+        var requestBody = JsonContent.Create(
+            new { date = asOfDateTime.ToString("o") },
+            new MediaTypeHeaderValue("application/json")
         );
 
-        if (response.StatusCode != HttpStatusCode.Created)
+        try
+        {
+            await ApiSendAsync(HttpMethod.Post, apiUrl, requestBody, HttpStatusCode.Created);
+        }
+        catch (Exception error)
         {
             throw new InvalidOperationException(
-                $"Failed to create history point for analysis '{apiAnalysisId}' with '{asOfDateTime}'.");
+                $"Failed to create history point for analysis '{apiAnalysisId}' with '{asOfDateTime}'.",
+                error
+            );
         }
     }
 
@@ -98,25 +159,29 @@ public class ResultsApi : IResultsApi
         var apiAnalysisId = cachedAnalysis!.ApiAnalysisId;
         var asOfDateTime = historyStopPoint!.AsOfDateTime;
 
-        var client = new HttpClient();
-
-        var response = await client.PostAsync(
-            $"{_configuration.FreshliWebApiBaseUrl}/api/v0/analysis-request/{apiAnalysisId}/{asOfDateTime:o}",
-            JsonContent.Create(
-                new
-                {
-                    packageUrl = packageLibYear.CurrentVersion!,
-                    publicationDate = packageLibYear.ReleaseDateCurrentVersion.ToString("o"),
-                    libYear = packageLibYear.LibYear
-                },
-                new MediaTypeHeaderValue("application/json")
-            )
+        var apiUrl = $"{_configuration.FreshliWebApiBaseUrl}/api/v0/analysis-request/{apiAnalysisId}/{asOfDateTime:o}";
+        var requestContent = JsonContent.Create(
+            new
+            {
+                packageUrl = packageLibYear.CurrentVersion!,
+                publicationDate = packageLibYear.ReleaseDateCurrentVersion.ToString("o"),
+                libYear = packageLibYear.LibYear
+            },
+            new MediaTypeHeaderValue("application/json")
         );
 
-        if (response.StatusCode != HttpStatusCode.Created)
+        try
+        {
+            await ApiSendAsync(HttpMethod.Post, apiUrl, requestContent, HttpStatusCode.Created);
+        }
+        catch (Exception error)
         {
             throw new InvalidOperationException(
-                $"Failed to create package lib year for analysis '{apiAnalysisId}' and '{asOfDateTime:o}' with package URL '{packageLibYear.CurrentVersion!}' publication date '{packageLibYear.ReleaseDateCurrentVersion:o}' and LibYear '{packageLibYear.LibYear}'.");
+                $"Failed to create package lib year for analysis '{apiAnalysisId}' and '{asOfDateTime:o}' with package URL '{packageLibYear.CurrentVersion!}' publication date '{packageLibYear.ReleaseDateCurrentVersion:o}' and LibYear '{packageLibYear.LibYear}'.",
+                error
+            );
         }
     }
+
+    public void Dispose() => _client.Dispose();
 }
