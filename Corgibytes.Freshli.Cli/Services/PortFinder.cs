095a2e9163c95dfc2c568be5816d0b32befa5bd6
M. Scott Ford <scott@corgibytes.com>
2023-01-17 15:11:15 -0500

    Change mechanism for retrieving a random port number

--- Corgibytes.Freshli.Cli/Services/PortFinder.cs
+++ Corgibytes.Freshli.Cli/Services/PortFinder.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Corgibytes.Freshli.Cli.Services;
@@ -8,19 +10,24 @@ public class PortFinder
     private const int MinPort = 1;
     private const int MaxPort = 65535;
 
-    private readonly ConcurrentBag<int> _possibleValues;
+    private readonly List<int> _possibleValues;
     private readonly ConcurrentBag<int> _attemptedValues = new();
+    private readonly Random _random = new();
 
     public PortFinder(int rangeStart = MinPort, int rangeStop = MaxPort)
     {
-        _possibleValues = new ConcurrentBag<int>(Enumerable.Range(rangeStart, rangeStop));
+        _possibleValues = new List<int>(Enumerable.Range(rangeStart, rangeStop));
     }
 
     public int FindNext()
     {
-        _possibleValues.TryTake(out var port);
-        _attemptedValues.Add(port);
-
-        return port;
+        lock (_possibleValues)
+        {
+            var index = _random.Next(_possibleValues.Count);
+            var value = _possibleValues[index];
+            _possibleValues.RemoveAt(index);
+            _attemptedValues.Add(value);
+            return value;
+        }
     }
 }
