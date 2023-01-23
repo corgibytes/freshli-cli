f959358c2710f36f9836d793bd4dcebaf14cc5f3
M. Scott Ford <scott@corgibytes.com>
2023-01-23 09:38:28 -0500

    Fires NoManifestsDetectedEvent when there are no manifests

--- Corgibytes.Freshli.Cli.Test/Functionality/Analysis/DetectManifestsUsingAgentActivityTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/Analysis/DetectManifestsUsingAgentActivityTest.cs
@@ -84,4 +84,43 @@ public class DetectManifestsUsingAgentActivityTest
             appEvent.Exception == exception
         )));
     }
+
+    [Fact]
+    public async Task HandleCorrectlyFiresNoManifestsDetectedEvent()
+    {
+        const string localPath = "/path/to/repository";
+        var agentReader = new Mock<IAgentReader>();
+        agentReader.Setup(mock => mock.DetectManifests(localPath)).Returns(
+            new List<string>().ToAsyncEnumerable());
+
+        const string agentExecutablePath = "/path/to/agent";
+        var agentManager = new Mock<IAgentManager>();
+        agentManager.Setup(mock => mock.GetReader(agentExecutablePath, CancellationToken.None)).Returns(agentReader.Object);
+
+        var cacheManager = new Mock<ICacheManager>();
+        var cacheDb = new Mock<ICacheDb>();
+        var historyStopPoint = new CachedHistoryStopPoint { LocalPath = localPath };
+
+        const int historyStopPointId = 29;
+        cacheManager.Setup(mock => mock.GetCacheDb()).Returns(cacheDb.Object);
+        cacheDb.Setup(mock => mock.RetrieveHistoryStopPoint(historyStopPointId)).ReturnsAsync(historyStopPoint);
+
+        var serviceProvider = new Mock<IServiceProvider>();
+        serviceProvider.Setup(mock => mock.GetService(typeof(IAgentManager))).Returns(agentManager.Object);
+        serviceProvider.Setup(mock => mock.GetService(typeof(ICacheManager))).Returns(cacheManager.Object);
+
+        var eventEngine = new Mock<IApplicationEventEngine>();
+        eventEngine.Setup(mock => mock.ServiceProvider).Returns(serviceProvider.Object);
+
+        var analysisId = Guid.NewGuid();
+        var activity =
+            new DetectManifestsUsingAgentActivity(analysisId, historyStopPointId, agentExecutablePath);
+
+        await activity.Handle(eventEngine.Object);
+
+        eventEngine.Verify(mock => mock.Fire(It.Is<NoManifestsDetectedEvent>(appEvent =>
+            appEvent.AnalysisId == analysisId &&
+            appEvent.HistoryStopPointId == historyStopPointId
+        )));
+    }
 }
