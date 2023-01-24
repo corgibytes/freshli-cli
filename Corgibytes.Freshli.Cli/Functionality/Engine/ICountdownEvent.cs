e9799a03941327d74a3ad82a0704cc83af4cc32a
M. Scott Ford <scott@corgibytes.com>
2023-01-24 10:45:07 -0500

    Adds analogs to the `CountdownEvent` class
    * The `ICountdownEvent` matches the contract of `CountdownEvent` with one addition, it fires an event whenever an operation changes `CurrentCount`.
    * The `DefaultCountdownEvent` matches the behavior of the `CountdownEvent` with the addition of firing events and exposing virtual methods that can be overriding by children.
    * The `ChildCountdownEvent` is used to make changes to a "parent" `ICountdownEvent` instance whenever the child instance changes. This allows the creation of parent child relationships between `ICountdownEvents` where the parent is incremented and decremented each time a child instance is.

---
+++ Corgibytes.Freshli.Cli/Functionality/Engine/ICountdownEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Corgibytes.Freshli.Cli.Functionality.Engine;
+
+public interface ICountdownEvent : IDisposable
+{
+    public int CurrentCount { get; }
+    public int InitialCount { get; }
+    public bool IsSet { get; }
+    public void AddCount();
+    public void AddCount(int signalCount);
+    public void Reset();
+    public void Reset(int count);
+    public bool Signal(int signalCount);
+    public bool Signal();
+    public bool TryAddCount();
+    public bool TryAddCount(int signalCount);
+    public bool TrySignal();
+    public bool TrySignal(int signalCount);
+    public void Wait();
+    public void Wait(int millisecondsTimeout);
+    public void Wait(CancellationToken cancellationToken);
+    public void Wait(TimeSpan timeout);
+    public void Wait(int millisecondsTimeout, CancellationToken cancellationToken);
+    public void Wait(TimeSpan timeout, CancellationToken cancellationToken);
+
+    public event EventHandler<CountChangedArgs>? CountChanged;
+    public record struct CountChangedArgs(int Delta);
+}
+
