cd8012434dba407868aa9f689c9260cec7cd404a
M. Scott Ford <scott@corgibytes.com>
2023-01-27 16:11:58 -0500

    Work-around issue building on Windows ARM64
    `protoc` hasn't been released for ARM64 on Windows yet, so we need to tell the gRPC build integration to use the x64 version of `protoc` instead. This can likely be removed once `protoc` has been released for Windows on ARM.

--- bin/build.rb
+++ bin/build.rb
@@ -7,6 +7,10 @@ require_relative './support/execute'
 
 enable_dotnet_command_colors
 
+if Gem.win_platform?
+  ENV['PROTOBUF_TOOLS_CPU'] = 'x64'
+end
+
 status = execute('dotnet tool restore')
 status = execute('dotnet build -o exe') if status.success?
 
