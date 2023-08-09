66c7df4e01956dd97b695959efda5a5acf9fe694
M. Scott Ford <scott@corgibytes.com>
2023-08-09 16:38:04 -0400

    Mocks out filesystem query about whether or not file exists

---
+++ Corgibytes.Freshli.Cli.Test/Services/FileValidatorTest.cs
@@ -0,0 +1,41 @@
+using Xunit;
+
+using Corgibytes.Freshli.Cli.Services;
+using Corgibytes.Freshli.Cli.Test;
+using System.IO;
+
+[IntegrationTest]
+public class FileValidatorTest
+{
+    [Fact]
+    public void IsValidFilePathWithValidFilePath()
+    {
+        var tempFilePath = Path.GetTempFileName();
+
+        var validator = new FileValidator();
+        Assert.True(validator.IsValidFilePath(tempFilePath));
+
+        File.Delete(tempFilePath);
+    }
+
+    [Fact]
+    public void IsValidFilePathWithNull()
+    {
+        var validator = new FileValidator();
+        Assert.False(validator.IsValidFilePath(null!));
+    }
+
+    [Fact]
+    public void IsValidFilePathWithEmptyString()
+    {
+        var validator = new FileValidator();
+        Assert.False(validator.IsValidFilePath(string.Empty));
+    }
+
+    [Fact]
+    public void IsValidFilePathWithBogusFilePath()
+    {
+        var validator = new FileValidator();
+        Assert.False(validator.IsValidFilePath("bogus"));
+    }
+}
