f245c25319324c449eb74fc299f71c219b081179
M. Scott Ford <scott@corgibytes.com>
2023-08-11 00:28:26 -0400

    Increases default timeout

--- Corgibytes.Freshli.Cli.Test/Constants.cs
+++ Corgibytes.Freshli.Cli.Test/Constants.cs
@@ -2,6 +2,6 @@ namespace Corgibytes.Freshli.Cli.Test;
 
 internal static class Constants
 {
-    public const int DefaultTestTimeout = 2000;
+    public const int DefaultTestTimeout = 10000;
     public const int ExpandedTestTimeout = 30000;
 }
