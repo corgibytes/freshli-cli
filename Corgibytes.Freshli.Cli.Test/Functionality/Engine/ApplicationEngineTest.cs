954b0a4a5ac5a0e9c22c30f30e4c2522a5f8618b
M. Scott Ford <scott@corgibytes.com>
2023-10-12 17:37:40 -0400

    Addresses linter issues

--- Corgibytes.Freshli.Cli.Test/Functionality/Engine/ApplicationEngineTest.cs
+++ Corgibytes.Freshli.Cli.Test/Functionality/Engine/ApplicationEngineTest.cs
@@ -2,7 +2,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.JavaScript;
 using System.Threading;
 using System.Threading.Tasks;
 using Corgibytes.Freshli.Cli.Functionality.Analysis;
--- Corgibytes.Freshli.Cli/Migrations/20231003132118_AdditionalIndexes.cs
+++ Corgibytes.Freshli.Cli/Migrations/20231003132118_AdditionalIndexes.cs
@@ -2,36 +2,35 @@
 
 #nullable disable
 
-namespace Corgibytes.Freshli.Cli.Migrations
+namespace Corgibytes.Freshli.Cli.Migrations;
+
+/// <inheritdoc />
+public partial class AdditionalIndexes : Migration
 {
     /// <inheritdoc />
-    public partial class AdditionalIndexes : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_CachedManifests_HistoryStopPointId",
-                table: "CachedManifests");
+        migrationBuilder.DropIndex(
+            name: "IX_CachedManifests_HistoryStopPointId",
+            table: "CachedManifests");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_CachedManifests_HistoryStopPointId_ManifestFilePath",
-                table: "CachedManifests",
-                columns: new[] { "HistoryStopPointId", "ManifestFilePath" },
-                unique: true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_CachedManifests_HistoryStopPointId_ManifestFilePath",
+            table: "CachedManifests",
+            columns: new[] { "HistoryStopPointId", "ManifestFilePath" },
+            unique: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_CachedManifests_HistoryStopPointId_ManifestFilePath",
-                table: "CachedManifests");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_CachedManifests_HistoryStopPointId_ManifestFilePath",
+            table: "CachedManifests");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_CachedManifests_HistoryStopPointId",
-                table: "CachedManifests",
-                column: "HistoryStopPointId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_CachedManifests_HistoryStopPointId",
+            table: "CachedManifests",
+            column: "HistoryStopPointId");
     }
 }
