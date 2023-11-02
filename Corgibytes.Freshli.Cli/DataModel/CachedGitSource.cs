b2b0948144aff02c1e602a128bc89f5c569badc2
M. Scott Ford <scott@corgibytes.com>
2023-11-02 16:21:46 -0400

    Makes branch a required value in the cache db

--- Corgibytes.Freshli.Cli/DataModel/CachedGitSource.cs
+++ Corgibytes.Freshli.Cli/DataModel/CachedGitSource.cs
@@ -1,11 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
-// ReSharper disable MemberInitializerValueIgnored
-// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
-// ReSharper disable PropertyCanBeMadeInitOnly.Global
-// ReSharper disable UnusedAutoPropertyAccessor.Global
-
 namespace Corgibytes.Freshli.Cli.DataModel;
 
 [Index(nameof(Id), IsUnique = true)]
@@ -13,8 +8,8 @@ public class CachedGitSource : TimeStampedEntity
 {
     [Required] public string Id { get; set; } = null!;
     [Required] public string Url { get; set; } = null!;
-    // TODO: Make this field required. It should be set to the name of the default branch instead of null.
-    public string? Branch { get; set; }
+    // ReSharper disable once PropertyCanBeMadeInitOnly.Global
+    [Required] public string Branch { get; set; } = null!;
     // TODO: Add validation to ensure that this is not a relative path.
     [Required] public string LocalPath { get; set; } = null!;
 }
