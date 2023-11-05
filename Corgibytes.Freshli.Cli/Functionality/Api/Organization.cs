54dc1498a139f008e8be3be85bf2eee71dcc1717
M. Scott Ford <scott@corgibytes.com>
2023-11-05 14:18:42 -0500

    Removes Id
    These are stored as `ObjectId` instances from the C# MongoDB driver. I'd rather not need to have that library as a CLI dependency.

--- Corgibytes.Freshli.Cli/Functionality/Api/Organization.cs
+++ Corgibytes.Freshli.Cli/Functionality/Api/Organization.cs
@@ -5,7 +5,6 @@ namespace Corgibytes.Freshli.Cli.Functionality.Api;
 
 public class Organization
 {
-    public ObjectId Id { get; set; }
     public string Name { get; set; }
     public string Nickname { get; set; }
     public List<Project> Projects { get; set; }
